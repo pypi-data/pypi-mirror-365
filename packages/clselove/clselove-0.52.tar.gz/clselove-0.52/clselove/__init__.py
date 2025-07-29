8e86407b62d6fe67475c63fbdbf813d437ec0457
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:30 +0000

    Add package clselove/0.52/clselove-0.52.tar.gz

---
+++ packages/clselove/clselove-0.52.tar.gz/clselove-0.52/clselove/__init__.py
@@ -0,0 +1,17 @@
+
+from .clselove import (
+    auto,
+    get_phone,
+    get_sms,
+    scr,
+    totel_temp,
+    geetest,
+    se,
+    na_em,
+    cap_se,
+    web,
+    se_chr
+)
+
+__version__ = "0.6"
+__author__ = "CodeMaster"
