ff96f36fb53a588862a1b2eab745f1b79162400d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:18 +0000

    Add package PREDICT/3.2.0/predict-3.2.0.tar.gz

---
+++ packages/PREDICT/predict-3.2.0.tar.gz/predict-3.2.0/docs/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'PREDICT'
+copyright = '2025, Martijn P. A. Starmans, Sebastian R. van der Voort'
+author = 'Martijn P. A. Starmans, Sebastian R. van der Voort'
+release = '3.2.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
