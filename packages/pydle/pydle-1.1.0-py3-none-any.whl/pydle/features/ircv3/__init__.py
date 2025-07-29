ae70748c8b207b29fda9e20d769d932d7ab1db0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:15 +0000

    Add package pydle/1.1.0/pydle-1.1.0-py3-none-any.whl

---
+++ packages/pydle/pydle-1.1.0-py3-none-any.whl/pydle/features/ircv3/__init__.py
@@ -0,0 +1,44 @@
+__all__ = [
+    "CapabilityNegotiationSupport",
+    "IRCv3Support",
+    "IRCv3_1Support",
+    "IRCv3_2Support",
+    "IRCv3_3Support",
+    "MetadataSupport",
+    "MonitoringSupport",
+    "SASLSupport",
+    "TaggedMessageSupport",
+    "cap",
+    "ircv3_1",
+    "ircv3_2",
+    "ircv3_3",
+    "monitor",
+    "sasl",
+    "tags",
+]
+
+## IRCv3.1 support.
+from . import cap, sasl, ircv3_1
+
+from .cap import CapabilityNegotiationSupport
+from .sasl import SASLSupport
+from .ircv3_1 import IRCv3_1Support
+
+
+## IRCv3.2 support.
+from . import monitor, tags, ircv3_2
+
+from .monitor import MonitoringSupport
+from .tags import TaggedMessageSupport
+from .metadata import MetadataSupport
+from .ircv3_2 import IRCv3_2Support
+
+
+## IRCv3.3 support.
+from . import ircv3_3
+
+from .ircv3_3 import IRCv3_3Support
+
+
+class IRCv3Support(IRCv3_3Support, IRCv3_2Support, IRCv3_1Support):
+    pass
---
+++ packages/pydle/pydle-1.1.0-py3-none-any.whl/pydle/utils/run.py
@@ -0,0 +1,13 @@
+## run.py
+# Run client.
+import asyncio
+from . import _args
+
+
+def main():
+    client, connect = _args.client_from_args("pydle", description="pydle IRC library.")
+    asyncio.run(connect())
+
+
+if __name__ == "__main__":
+    main()
