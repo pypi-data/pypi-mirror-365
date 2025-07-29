8314cb411f2a1742cd23eb6a391d3517a06629fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:40 +0000

    Add package Solidity-test/0.1.0/solidity_test-0.1.0.tar.gz

---
+++ packages/Solidity-test/solidity_test-0.1.0.tar.gz/solidity_test-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Solidity_test",
+    version="0.1.0",
+    description="Octobizteam IT Developer Technical Test",
+    author="masbro",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.0.0",
+    ],
+    python_requires=">=3.6",
+)
