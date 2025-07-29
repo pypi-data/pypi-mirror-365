474026c1041a0b0662ea5f014cd8db28677e93cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:11 +0000

    Add package edcba/0.0.1/edcba-0.0.1.tar.gz

---
+++ packages/edcba/edcba-0.0.1.tar.gz/edcba-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="edcba",
+    version="0.0.1",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="EDCBA",
+    long_description="'edcba' on PyPI.",
+    long_description_content_type="text/plain",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Programming Language :: Python :: 3",
+    ],
+)
