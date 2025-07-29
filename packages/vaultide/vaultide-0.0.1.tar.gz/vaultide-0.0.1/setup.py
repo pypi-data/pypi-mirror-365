18bea66a96bb7a82e648efc5163cca6d03260f67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:45 +0000

    Add package vaultide/0.0.1/vaultide-0.0.1.tar.gz

---
+++ packages/vaultide/vaultide-0.0.1.tar.gz/vaultide-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="vaultide",
+    version="0.0.1",
+    description="Vaultide is coming soon!",
+    author="Meder Kamalov",
+    author_email="meder@vaultide.com",
+    packages=[],
+)
