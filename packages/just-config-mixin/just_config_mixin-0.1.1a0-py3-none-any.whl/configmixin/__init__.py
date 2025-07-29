3157ea94e4fdcc8eeaea311adfd081c23c098491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:20 +0000

    Add package just-config-mixin/0.1.1a0/just_config_mixin-0.1.1a0-py3-none-any.whl

---
+++ packages/just-config-mixin/just_config_mixin-0.1.1a0-py3-none-any.whl/configmixin/__init__.py
@@ -0,0 +1,13 @@
+"""
+.. include:: ../../README.md
+"""
+
+from ._core import ConfigMixin, FrozenDict, register_to_config
+
+__version__ = "0.1.1-alpha"
+
+__all__ = [
+    "ConfigMixin",
+    "FrozenDict",
+    "register_to_config",
+]
