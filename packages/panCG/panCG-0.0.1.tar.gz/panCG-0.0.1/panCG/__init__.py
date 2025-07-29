fed90515fafc5ec41197da629d8b75b7d3bf2057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:15 +0000

    Add package panCG/0.0.1/panCG-0.0.1.tar.gz

---
+++ packages/panCG/panCG-0.0.1.tar.gz/panCG-0.0.1/panCG/__init__.py
@@ -0,0 +1,5 @@
+import os
+
+PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__))
+SCIPTS_DIR = os.path.join(os.path.dirname(PACKAGE_DIR), 'Scipts')
+
---
+++ packages/panCG/panCG-0.0.1.tar.gz/panCG-0.0.1/setup.py
@@ -0,0 +1,35 @@
+import codecs
+import os
+from setuptools import setup, find_packages
+
+# these things are needed for the README.md show on pypi (if you dont need delete it)
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+# you need to change all these
+VERSION = '0.0.1'
+DESCRIPTION = 'xxx'
+LONG_DESCRIPTION = 'xxxxxx'
+
+setup(
+    name="panCG",
+    version=VERSION,
+    author="ltan",
+    author_email="leitan1127@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'panCG', 'windows','mac','linux'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
