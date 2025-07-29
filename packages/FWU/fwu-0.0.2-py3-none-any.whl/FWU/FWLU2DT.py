781e2b25ad307bd9d28e31c241ea6d75ab0208b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:37 +0000

    Add package FWU/0.0.2/fwu-0.0.2-py3-none-any.whl

---
+++ packages/FWU/fwu-0.0.2-py3-none-any.whl/FWU/FWLU2DT.py
@@ -0,0 +1,28 @@
+import torch
+import math
+
+class FWLU2DT(torch.nn.Module):
+    def __init__(self,in_channels,out_channels,kernel_size,stride=1,padding=0,output_padding=0,dilation=1,groups=1,padding_mode='zeros',bias=True,gain=1,**kwargs):
+        super(FWLU2DT, self).__init__()
+        self.ConvP=torch.nn.ConvTranspose2d(in_channels,out_channels,kernel_size,stride=stride,padding=padding,output_padding=output_padding,dilation=dilation,groups=groups,padding_mode=padding_mode,bias=False,**kwargs)
+        self.ConvN=torch.nn.ConvTranspose2d(in_channels,out_channels,kernel_size,stride=stride,padding=padding,output_padding=output_padding,dilation=dilation,groups=groups,padding_mode=padding_mode,bias=False,**kwargs)
+        
+        if bias:
+            self.bias=torch.nn.Parameter(torch.zeros((out_channels,1,1),**kwargs),requires_grad=True)
+        else:
+            self.bias=None
+              
+        with torch.no_grad():
+            torch.nn.init.orthogonal_(self.ConvP.weight,gain=gain)
+            torch.nn.init.orthogonal_(self.ConvN.weight,gain=gain)
+        
+    def forward(self,input):
+        inputP=input.clamp(min=0)
+        inputN=input.clamp(max=0)   
+        output=self.ConvP(inputP)+self.ConvN(inputN)
+            
+        if self.bias is not None:
+            output=output+self.bias
+        
+        return output
+  
---
+++ packages/FWU/fwu-0.0.2-py3-none-any.whl/FWU/FWMU2D.py
@@ -0,0 +1,32 @@
+import torch
+import math
+
+class FWMU2D(torch.nn.Module):
+    def __init__(self,in_channels,out_channels,kernel_size,stride=1,padding=0,dilation=1,groups=1,padding_mode='zeros',bias=True,gain=1,**kwargs):
+        super(FWMU2D, self).__init__()
+        self.ConvP=torch.nn.Conv2d(in_channels,out_channels,kernel_size,stride=stride,padding=padding,dilation=dilation,groups=groups,padding_mode=padding_mode,bias=True,**kwargs)
+        self.ConvN=torch.nn.Conv2d(in_channels,out_channels,kernel_size,stride=stride,padding=padding,dilation=dilation,groups=groups,padding_mode=padding_mode,bias=True,**kwargs)
+        
+        if bias:
+            self.bias=torch.nn.Parameter(torch.zeros((out_channels,1,1),**kwargs),requires_grad=True)
+        else:
+            self.bias=None
+              
+        with torch.no_grad():
+            torch.nn.init.orthogonal_(self.ConvP.weight,gain=gain)
+            torch.nn.init.orthogonal_(self.ConvN.weight,gain=gain)
+            torch.nn.init.normal_(self.ConvP.bias)
+            self.ConvP.bias.data.sign_()
+            torch.nn.init.normal_(self.ConvN.bias)
+            self.ConvN.bias.data.sign_()
+            
+    def forward(self,input):
+        inputP=input.clamp(min=0)
+        inputN=input.clamp(max=0)   
+        output=self.ConvP(inputP)*self.ConvN(inputN)-(self.ConvP.bias*self.ConvN.bias).unsqueeze(-1).unsqueeze(-1)
+            
+        if self.bias is not None:
+            output=output+self.bias
+        
+        return output
+  
