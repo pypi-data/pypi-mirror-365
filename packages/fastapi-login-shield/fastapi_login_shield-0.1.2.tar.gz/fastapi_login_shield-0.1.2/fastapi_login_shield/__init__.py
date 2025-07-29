286e6ba11de8d9c4d66e4f84f3e0950cd3385fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:44 +0000

    Add package fastapi-login-shield/0.1.2/fastapi_login_shield-0.1.2.tar.gz

---
+++ packages/fastapi-login-shield/fastapi_login_shield-0.1.2.tar.gz/fastapi_login_shield-0.1.2/fastapi_login_shield/__init__.py
@@ -0,0 +1,8 @@
+from .dependency import check_brute_force, register_failed_login, clear_login_attempts
+
+__all__ = [
+    "BruteForceProtectMiddleware",
+    "check_brute_force",
+    "register_failed_login",
+    "clear_login_attempts",
+]
