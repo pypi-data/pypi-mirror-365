c00c39bf664a31d8b269e1a86feff0c33c8aee42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:50 +0000

    Add package mimasv2configtool/2025.0.5/mimasv2configtool-2025.0.5.tar.gz

---
+++ packages/mimasv2configtool/mimasv2configtool-2025.0.5.tar.gz/mimasv2configtool-2025.0.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="mimasv2configtool",
+    version="2025.0.5",
+    author="Habib Ullah",
+    author_email="khaalidi@icloud.com",
+    description="SPI Flash configuration tool for Numato Lab Mimas V2 FPGA board",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",  
+    url="https://github.com/khaalidi/mimasv2configtool",
+    install_requires=[
+        "pyserial",
+    ],
+    entry_points={
+        "console_scripts": [
+            "mimasv2configtool=mimasv2configtool.main:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
