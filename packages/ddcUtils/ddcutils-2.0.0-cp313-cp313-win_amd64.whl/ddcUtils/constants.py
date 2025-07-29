e57a19da6ce8dd5e8dbd362fcc8b9a480ed7ecc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:36 +0000

    Add package ddcUtils/2.0.0/ddcutils-2.0.0-cp313-cp313-win_amd64.whl

---
+++ packages/ddcUtils/ddcutils-2.0.0-cp313-cp313-win_amd64.whl/ddcUtils/constants.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+DATE_TIME_FORMATTER_STR = "%a %b %m %Y %X"
+DATE_FORMATTER = "%Y-%m-%d"
+TIME_FORMATTER = "%H:%M:%S.%f"
