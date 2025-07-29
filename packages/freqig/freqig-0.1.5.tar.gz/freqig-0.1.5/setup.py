726fcd8eb0653e1ab90354e624668d85c1b77098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:04:48 +0000

    Add package freqig/0.1.5/freqig-0.1.5.tar.gz

---
+++ packages/freqig/freqig-0.1.5.tar.gz/freqig-0.1.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="freqig",
+    version="0.1.5",
+    packages=find_packages(),
+    install_requires=[
+        'torch>=2.0.0',
+        'numpy>=1.21.0',
+        'captum>=0.6.0'
+    ],
+    author="Paul Gräve",
+    description="Frequency-domain model explanation (IG) package",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://gitlab.com/paulabraham.graeve/flex",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    license="MIT",
+    license_files=["LICENSE", "CITATION.cff"],
+    extras_require={
+        'academic': ['howfairis>=0.20.0']
+    }
+)
