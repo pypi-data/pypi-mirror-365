05d828a38366008820f1f7ff4430f15bfb388411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:04:44 +0000

    Add package cachecleaner/0.6/cachecleaner-0.6.tar.gz

---
+++ packages/cachecleaner/cachecleaner-0.6.tar.gz/cachecleaner-0.6/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(
+    name='cachecleaner',
+    version='0.6',
+    description='Keeps dir size in given capacity.',
+    author='Alexander Karpinsky',
+    author_email='homm86@gmail.com',
+    url='https://github.com/homm/cachecleaner',
+    py_modules=['cachecleaner'],
+    entry_points={
+        'console_scripts': [
+            'cachecleaner = cachecleaner:main',
+        ],
+    },
+    install_requires=[
+        'tqdm>=4.20.0',
+    ]
+)
