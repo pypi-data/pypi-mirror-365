187848149b16a4f203f7c5ec6dfcd3f03d5096d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:38 +0000

    Add package ramifice/0.8.14/ramifice-0.8.14-py3-none-any.whl

---
+++ packages/ramifice/ramifice-0.8.14-py3-none-any.whl/ramifice/utils/mixins/__init__.py
@@ -0,0 +1,6 @@
+"""Ramifice - Set of mixins for Models and Fields."""
+
+from ramifice.utils.mixins.add_valid import AddValidMixin
+from ramifice.utils.mixins.hooks import HooksMixin
+from ramifice.utils.mixins.indexing import IndexMixin
+from ramifice.utils.mixins.json_converter import JsonMixin
