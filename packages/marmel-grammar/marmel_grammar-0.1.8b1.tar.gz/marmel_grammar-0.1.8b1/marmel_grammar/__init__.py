f39b7231a87c03e07c53ba38e45a149af3a373c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:15 +0000

    Add package marmel-grammar/0.1.8b1/marmel_grammar-0.1.8b1.tar.gz

---
+++ packages/marmel-grammar/marmel_grammar-0.1.8b1.tar.gz/marmel_grammar-0.1.8b1/marmel_grammar/__init__.py
@@ -0,0 +1,12 @@
+from .grammar import MarmelGrammar
+from . import dataset
+
+__version__ = "0.1.8b1"
+__author__ = "Dev-Marmel"
+__email__ = "marmelgpt@gmail.com"
+__description__ = "Библиотека русской морфологии и транслитерации для Python"
+
+__all__ = [
+    "MarmelGrammar",
+    "dataset",
+]
