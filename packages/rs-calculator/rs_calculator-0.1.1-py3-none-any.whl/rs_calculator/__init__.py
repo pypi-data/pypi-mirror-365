f375b4f4d9babfde88c5c5f611576b058ebca440
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:44 +0000

    Add package rs-calculator/0.1.1/rs_calculator-0.1.1-py3-none-any.whl

---
+++ packages/rs-calculator/rs_calculator-0.1.1-py3-none-any.whl/rs_calculator/__init__.py
@@ -0,0 +1,40 @@
+# rs_calculator/__init__.py
+
+def calculate(num1, num2, operator):
+    if operator == "x":
+        operator = "*"
+    elif operator == "^":
+        operator = "**"
+
+    if operator == "+":
+        return num1 + num2
+    elif operator == "-":
+        return num1 - num2
+    elif operator == "*":
+        return num1 * num2
+    elif operator == "/":
+        if num2 == 0:
+            return "Error: Division by zero"
+        return num1 / num2
+    elif operator == "**":
+        return num1 ** num2
+    elif operator == "%":
+        if num2 == 0:
+            return "Error: Modulo by zero"
+        return num1 % num2
+    elif operator == "//":
+        if num2 == 0:
+            return "Error: Integer division by zero"
+        return num1 // num2
+    else:
+        return "Invalid operator"
+
+def show_help():
+    print(
+        "How to use (In order):\n"
+        " number 1, number 2, operator\n"
+        "Operators supported: +, -, *, x, /, //, %, **, ^\n"
+        "RS Calculator - simplest calculator library in Python!"
+    )
+
+__version__ = "0.1.1"
