604b5e3233a1dc0c919f51b62099b189ee1e1776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:07 +0000

    Add package withpi/1.55.0/withpi-1.55.0.tar.gz

---
+++ packages/withpi/withpi-1.55.0.tar.gz/withpi-1.55.0/src/withpi/types/__init__.py
@@ -0,0 +1,32 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .shared import (
+    Example as Example,
+    Question as Question,
+    ExplorationMode as ExplorationMode,
+    SyntheticDataStatus as SyntheticDataStatus,
+    DataGenerationStatus as DataGenerationStatus,
+    ScoringSystemMetrics as ScoringSystemMetrics,
+    QueryClassifierResult as QueryClassifierResult,
+    ScoringSpecCalibrationStatus as ScoringSpecCalibrationStatus,
+)
+from .scoring_system_score_params import ScoringSystemScoreParams as ScoringSystemScoreParams
+from .scoring_system_generate_params import ScoringSystemGenerateParams as ScoringSystemGenerateParams
+from .scoring_system_list_jobs_params import ScoringSystemListJobsParams as ScoringSystemListJobsParams
+from .scoring_system_generate_response import ScoringSystemGenerateResponse as ScoringSystemGenerateResponse
+from .scoring_system_import_spec_params import ScoringSystemImportSpecParams as ScoringSystemImportSpecParams
+from .scoring_system_list_jobs_response import ScoringSystemListJobsResponse as ScoringSystemListJobsResponse
+from .scoring_system_cancel_job_response import ScoringSystemCancelJobResponse as ScoringSystemCancelJobResponse
+from .scoring_system_import_spec_response import ScoringSystemImportSpecResponse as ScoringSystemImportSpecResponse
+from .scoring_system_retrieve_job_response import ScoringSystemRetrieveJobResponse as ScoringSystemRetrieveJobResponse
+from .scoring_system_stream_job_messages_response import (
+    ScoringSystemStreamJobMessagesResponse as ScoringSystemStreamJobMessagesResponse,
+)
+from .scoring_system_upload_to_huggingface_params import (
+    ScoringSystemUploadToHuggingfaceParams as ScoringSystemUploadToHuggingfaceParams,
+)
+from .scoring_system_upload_to_huggingface_response import (
+    ScoringSystemUploadToHuggingfaceResponse as ScoringSystemUploadToHuggingfaceResponse,
+)
