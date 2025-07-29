fb555d4d0623a45763a1ba955b751e2fc5937e1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:32 +0000

    Add package xmodules/1.0.1/xmodules-1.0.1.tar.gz

---
+++ packages/xmodules/xmodules-1.0.1.tar.gz/xmodules-1.0.1/xmodules/runner.py
@@ -0,0 +1,20 @@
+# xmodules/runner.py
+
+import asyncio
+from xmodules.session import create_or_load_session
+from xmodules.handlers.save import register_save_handler
+
+
+def run(api_id: int, api_hash: str, admin_id: int):
+    """
+    XModulesni ishga tushirish.
+    """
+
+    async def main():
+        client = await create_or_load_session(api_id, api_hash)
+        register_save_handler(client, admin_id=admin_id)
+        await client.start()
+        print(f"✅ XModules ishga tushdi.")
+        await client.run_until_disconnected()
+
+    asyncio.run(main())
