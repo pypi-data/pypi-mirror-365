9a56a876f82fb7e9f39ea3d3c2a1ce3469c56092
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:47 +0000

    Add package requirementsengineering/0.1.2/requirementsengineering-0.1.2.tar.gz

---
+++ packages/requirementsengineering/requirementsengineering-0.1.2.tar.gz/requirementsengineering-0.1.2/src/requirementsengineering/__init__.py
@@ -0,0 +1,7 @@
+# __init__.py
+from .main import main
+
+__version__ = "0.1.2"
+
+if __name__ == "__main__":
+    main()
---
+++ packages/requirementsengineering/requirementsengineering-0.1.2.tar.gz/requirementsengineering-0.1.2/src/requirementsengineering/__main__.py
@@ -0,0 +1,9 @@
+# requirementsengineering/__main__.py
+
+from .main import main as requirements_main
+
+def main():
+    requirements_main()
+
+if __name__ == "__main__":
+    main()
