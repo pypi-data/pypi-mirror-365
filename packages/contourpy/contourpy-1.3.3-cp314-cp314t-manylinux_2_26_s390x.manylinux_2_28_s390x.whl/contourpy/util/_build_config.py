64880f3fda93aec2e71d5d788366853e908b9cd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:51 +0000

    Add package contourpy/1.3.3/contourpy-1.3.3-cp314-cp314t-manylinux_2_26_s390x.manylinux_2_28_s390x.whl

---
+++ packages/contourpy/contourpy-1.3.3-cp314-cp314t-manylinux_2_26_s390x.manylinux_2_28_s390x.whl/contourpy/util/_build_config.py
@@ -0,0 +1,60 @@
+# _build_config.py.in is converted into _build_config.py during the meson build process.
+
+from __future__ import annotations
+
+
+def build_config() -> dict[str, str]:
+    """
+    Return a dictionary containing build configuration settings.
+
+    All dictionary keys and values are strings, for example ``False`` is
+    returned as ``"False"``.
+
+        .. versionadded:: 1.1.0
+    """
+    return dict(
+        # Python settings
+        python_version="3.14",
+        python_install_dir=r"/usr/local/lib/python3.14t/site-packages/",
+        python_path=r"/tmp/build-env-uhkzjphj/bin/python",
+
+        # Package versions
+        contourpy_version="1.3.3",
+        meson_version="1.8.2",
+        mesonpy_version="0.18.0",
+        pybind11_version="3.0.0",
+
+        # Misc meson settings
+        meson_backend="ninja",
+        build_dir=r"/project/.mesonpy-j3h5fce5/lib/contourpy/util",
+        source_dir=r"/project/lib/contourpy/util",
+        cross_build="False",
+
+        # Build options
+        build_options=r"-Dbuildtype=release -Db_ndebug=if-release -Db_vscrt=md -Dvsenv=True --native-file=/project/.mesonpy-j3h5fce5/meson-python-native-file.ini",
+        buildtype="release",
+        cpp_std="c++17",
+        debug="False",
+        optimization="3",
+        vsenv="True",
+        b_ndebug="if-release",
+        b_vscrt="from_buildtype",
+
+        # C++ compiler
+        compiler_name="gcc",
+        compiler_version="14.2.1",
+        linker_id="ld.bfd",
+        compile_command="c++",
+
+        # Host machine
+        host_cpu="s390x",
+        host_cpu_family="s390x",
+        host_cpu_endian="big",
+        host_cpu_system="linux",
+
+        # Build machine, same as host machine if not a cross_build
+        build_cpu="s390x",
+        build_cpu_family="s390x",
+        build_cpu_endian="big",
+        build_cpu_system="linux",
+    )
