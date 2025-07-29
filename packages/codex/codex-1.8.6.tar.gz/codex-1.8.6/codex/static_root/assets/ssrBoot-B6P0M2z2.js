57c59000c211960511c54d0f2885b376f76d42fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:15 +0000

    Add package codex/1.8.6/codex-1.8.6.tar.gz

---
+++ packages/codex/codex-1.8.6.tar.gz/codex-1.8.6/codex/static_root/assets/ssrBoot-B6P0M2z2.js
@@ -0,0 +1,1 @@
+import{aT as t,bk as e,t as s,bp as n}from"./main-Bmi4BPiO.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
