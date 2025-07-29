01c501ae74d469cef8337e155d71cbab72d79e78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:04:59 +0000

    Add package django-computedfields/0.3.3/django_computedfields-0.3.3.tar.gz

---
+++ packages/django-computedfields/django_computedfields-0.3.3.tar.gz/django_computedfields-0.3.3/docs/reference.rst
@@ -0,0 +1,48 @@
+API Reference
+=============
+
+models.py
+---------
+
+.. automodule:: computedfields.models
+   :members:
+   :show-inheritance:
+
+resolver.py
+-----------
+
+.. automodule:: computedfields.resolver
+   :members:
+   :show-inheritance:
+
+
+graph.py
+--------
+
+.. automodule:: computedfields.graph
+   :members:
+   :show-inheritance:
+
+
+handlers.py
+-----------
+
+.. automodule:: computedfields.handlers
+   :members:
+   :show-inheritance:
+
+
+admin.py
+--------
+
+.. automodule:: computedfields.admin
+   :members:
+   :show-inheritance:
+
+
+signals.py
+----------
+
+.. automodule:: computedfields.signals
+   :members:
+   :show-inheritance:
---
+++ packages/django-computedfields/django_computedfields-0.3.3.tar.gz/django_computedfields-0.3.3/setup.py
@@ -0,0 +1,50 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+def get_version():
+    import computedfields
+    return computedfields.__version__
+
+
+setup(
+    name='django-computedfields',
+    packages=find_packages(exclude=['example']),
+    include_package_data=True,
+    install_requires=[
+        'Django>=4.2,<6.0',
+        'typing_extensions>=4.1',
+        'django-fast-update'
+    ],
+    version=get_version(),
+    license='MIT',
+    description='autoupdated database fields for model methods',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='netzkolchose',
+    author_email='j.breitbart@netzkolchose.de',
+    url='https://github.com/netzkolchose/django-computedfields',
+    download_url=f'https://github.com/netzkolchose/django-computedfields/archive/{get_version()}.tar.gz',
+    keywords=['django', 'denormalization', 'method', 'decorator',
+              'autoupdate', 'persistent', 'field'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Database',
+        'Topic :: Database :: Front-Ends',
+        'Topic :: Software Development :: Libraries',
+        'Framework :: Django',
+        'Framework :: Django :: 4.2',
+        'Framework :: Django :: 5.1',
+        'Framework :: Django :: 5.2',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13'
+    ],
+)
