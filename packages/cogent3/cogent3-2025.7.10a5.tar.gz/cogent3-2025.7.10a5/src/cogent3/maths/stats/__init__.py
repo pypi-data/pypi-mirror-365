1555e90f97e117c49071fccb1f00a34a5cca1ae8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:23 +0000

    Add package cogent3/2025.7.10a5/cogent3-2025.7.10a5.tar.gz

---
+++ packages/cogent3/cogent3-2025.7.10a5.tar.gz/cogent3-2025.7.10a5/src/cogent3/maths/stats/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""Provides statistical tests and distributions.
+
+Also provides NumberList and FrequencyDistribution, two classes for
+working with statistical data.
+"""
+
+__all__ = [
+    "contingency",
+    "distribution",
+    "information_criteria",
+    "kendall",
+    "ks",
+    "special",
+    "test",
+]
