b02a03b1559b8709ce230949ec48090eb9797e8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:21 +0000

    Add package utils4plans/0.1.1/utils4plans-0.1.1-py3-none-any.whl

---
+++ packages/utils4plans/utils4plans-0.1.1-py3-none-any.whl/utils4plans/printing.py
@@ -0,0 +1,12 @@
+from rich.theme import Theme
+from rich.console import Console
+
+custom_theme = Theme(
+    {
+        "info": "bold cyan",
+        "success": "bold green",
+        "warning": "bold #ff8700",
+        "error": "bold red",
+    }
+)
+StyledConsole = Console(theme=custom_theme)
