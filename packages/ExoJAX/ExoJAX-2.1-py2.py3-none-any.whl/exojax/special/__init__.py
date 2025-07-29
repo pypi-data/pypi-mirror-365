c117d597cec9b38ba66debdc6628ecd30b21bb33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:43 +0000

    Add package ExoJAX/2.1/ExoJAX-2.1-py2.py3-none-any.whl

---
+++ packages/ExoJAX/ExoJAX-2.1-py2.py3-none-any.whl/exojax/special/__init__.py
@@ -0,0 +1,22 @@
+__all__ = []
+
+__version__ = '1.6'
+__uri__ = ''
+__author__ = 'ExoJAX collaborators'
+__email__ = 'divrot@gmail.com'
+__license__ = ''
+__description__ = ''
+
+from exojax.special.erfcx import (
+    erfcx,
+)
+
+from exojax.special.j0 import (
+    j0,
+)
+
+from exojax.special.faddeeva import (
+    rewofz,
+    imwofz,
+    asymptotic_wofz,
+)
