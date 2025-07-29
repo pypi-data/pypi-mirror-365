400aa3960104c85fe4a64e81cf56dfe54828814a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:59 +0000

    Add package islam-sdk/0.0.1/islam_sdk-0.0.1.tar.gz

---
+++ packages/islam-sdk/islam_sdk-0.0.1.tar.gz/islam_sdk-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="islam-sdk",
+    version="0.0.1",
+    packages=find_packages(),
+    description="Official Python SDK for Islamic APIs",
+    author="Mohammed Raad",
+    author_email="you@example.com",
+    license="MIT",
+    keywords="islam quran sdk prayer hadith",
+)
