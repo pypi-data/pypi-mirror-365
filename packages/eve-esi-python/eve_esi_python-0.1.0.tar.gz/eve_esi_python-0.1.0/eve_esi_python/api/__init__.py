674f02f726c0f9cd997a70ce1d3acab2b9b9f9db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:38 +0000

    Add package eve-esi-python/0.1.0/eve_esi_python-0.1.0.tar.gz

---
+++ packages/eve-esi-python/eve_esi_python-0.1.0.tar.gz/eve_esi_python-0.1.0/eve_esi_python/api/__init__.py
@@ -0,0 +1,34 @@
+# flake8: noqa
+
+# import apis into api package
+from eve_esi_python.api.alliance_api import AllianceApi
+from eve_esi_python.api.assets_api import AssetsApi
+from eve_esi_python.api.calendar_api import CalendarApi
+from eve_esi_python.api.character_api import CharacterApi
+from eve_esi_python.api.clones_api import ClonesApi
+from eve_esi_python.api.contacts_api import ContactsApi
+from eve_esi_python.api.contracts_api import ContractsApi
+from eve_esi_python.api.corporation_api import CorporationApi
+from eve_esi_python.api.dogma_api import DogmaApi
+from eve_esi_python.api.faction_warfare_api import FactionWarfareApi
+from eve_esi_python.api.fittings_api import FittingsApi
+from eve_esi_python.api.fleets_api import FleetsApi
+from eve_esi_python.api.incursions_api import IncursionsApi
+from eve_esi_python.api.industry_api import IndustryApi
+from eve_esi_python.api.insurance_api import InsuranceApi
+from eve_esi_python.api.killmails_api import KillmailsApi
+from eve_esi_python.api.location_api import LocationApi
+from eve_esi_python.api.loyalty_api import LoyaltyApi
+from eve_esi_python.api.mail_api import MailApi
+from eve_esi_python.api.market_api import MarketApi
+from eve_esi_python.api.planetary_interaction_api import PlanetaryInteractionApi
+from eve_esi_python.api.routes_api import RoutesApi
+from eve_esi_python.api.search_api import SearchApi
+from eve_esi_python.api.skills_api import SkillsApi
+from eve_esi_python.api.sovereignty_api import SovereigntyApi
+from eve_esi_python.api.status_api import StatusApi
+from eve_esi_python.api.universe_api import UniverseApi
+from eve_esi_python.api.user_interface_api import UserInterfaceApi
+from eve_esi_python.api.wallet_api import WalletApi
+from eve_esi_python.api.wars_api import WarsApi
+
