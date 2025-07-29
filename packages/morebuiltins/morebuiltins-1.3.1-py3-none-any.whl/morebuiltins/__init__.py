8b5fd5bee6ce235d231f0782c711285ec4fa8e95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:20 +0000

    Add package morebuiltins/1.3.1/morebuiltins-1.3.1-py3-none-any.whl

---
+++ packages/morebuiltins/morebuiltins-1.3.1-py3-none-any.whl/morebuiltins/__init__.py
@@ -0,0 +1,21 @@
+__version__ = "1.3.1"
+__all__ = [
+    "morebuiltins.utils",
+    "morebuiltins.date",
+    "morebuiltins.funcs",
+    "morebuiltins.ipc",
+    "morebuiltins.request",
+    "morebuiltins.download_python",
+    "morebuiltins.tk",
+    "morebuiltins.emails",
+    "morebuiltins.cmd.log_server",
+    "morebuiltins.cmd.proxy_checker",
+    "morebuiltins.cmd.ui",
+    "morebuiltins.cmd.parse_deps",
+    "morebuiltins.snippets.event",
+    "morebuiltins.snippets.sql",
+    "morebuiltins.cmd.systemd.service",
+    "morebuiltins.cmd.systemd.timer",
+    "morebuiltins.sqlite",
+    "morebuiltins.shared_memory",
+]
