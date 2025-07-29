4cb0c294ad64a7704ddc64af05bdecf5e439cd50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:25 +0000

    Add package panCG/0.0.1/panCG-0.0.1-py3-none-any.whl

---
+++ packages/panCG/panCG-0.0.1-py3-none-any.whl/panCG/lib/info.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '0.0.1'
+__author__ = "Lei Tan"
+__email__ = "lei.tan.bio@outlook.com"
+__doc__ = ("panCG is a toolkit for construction and analysis of cross-species pan-Gene and "
+           "pan-CNS (conserved non-coding sequences)")
