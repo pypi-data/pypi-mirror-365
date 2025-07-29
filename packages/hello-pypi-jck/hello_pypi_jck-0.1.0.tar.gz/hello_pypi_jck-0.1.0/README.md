5244b2f284bc46ccedc336b6683e6567bf9b137b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:05 +0000

    Add package hello-pypi-jck/0.1.0/hello_pypi_jck-0.1.0.tar.gz

---
+++ packages/hello-pypi-jck/hello_pypi_jck-0.1.0.tar.gz/hello_pypi_jck-0.1.0/README.md
@@ -0,0 +1,3 @@
+# hello_pypi
+
+A simple dummy package to say hello!
---
+++ packages/hello-pypi-jck/hello_pypi_jck-0.1.0.tar.gz/hello_pypi_jck-0.1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hello_pypi_jck',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+)
