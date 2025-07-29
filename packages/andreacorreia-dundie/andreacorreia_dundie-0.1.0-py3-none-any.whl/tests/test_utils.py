6a6b2842bbeb97f5d1d35cf838a299025c8a97fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:47 +0000

    Add package andreacorreia-dundie/0.1.0/andreacorreia_dundie-0.1.0-py3-none-any.whl

---
+++ packages/andreacorreia-dundie/andreacorreia_dundie-0.1.0-py3-none-any.whl/tests/test_utils.py
@@ -0,0 +1,33 @@
+import pytest
+
+from dundie.utils.email import check_valid_email
+from dundie.utils.user import generate_simple_password
+
+
+@pytest.mark.unit
+@pytest.mark.parametrize(
+    "address", ["andrea@hotmail.com", "joe@doe.com", "a@b.pt"]
+)
+def test_positive_check_valid_email(address):
+    """Ensure email is valide."""
+    assert check_valid_email(address) is True
+
+
+@pytest.mark.unit
+@pytest.mark.unit
+@pytest.mark.parametrize("address", ["andrea@.com", "@doe.com", "a@b"])
+def test_positive_check_valid_email(address):
+    """Ensure email is invalid."""
+    assert check_valid_email(address) is False
+
+
+@pytest.mark.unit
+def test_generate_simple_password():
+    """Test generation of random simple passwords
+    TODO: Generate hashed complex passwords, encrypit it
+    """
+    passwords = []
+    for _ in range(100):
+        passwords.append(generate_simple_password(8))
+
+    assert len(set(passwords)) == 100
