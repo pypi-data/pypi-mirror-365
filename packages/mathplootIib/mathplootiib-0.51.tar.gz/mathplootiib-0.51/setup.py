40e0d98d0090f3efed1b70558d81dea471016952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:33 +0000

    Add package mathplootIib/0.51/mathplootiib-0.51.tar.gz

---
+++ packages/mathplootIib/mathplootiib-0.51.tar.gz/mathplootiib-0.51/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mathplootIib',  # your package name
+    version='0.51',
+    description='hi',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Ayush Chadha',
+    author_email='elegantShock3307@gmail.com',
+    url='https://github.com/elegantShock2258/mathplotlib',
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
