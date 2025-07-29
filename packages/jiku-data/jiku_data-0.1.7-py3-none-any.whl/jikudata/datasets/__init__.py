e064dc90ac33d2c879f519da503ecc424cfdb92f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:01 +0000

    Add package jiku-data/0.1.7/jiku_data-0.1.7-py3-none-any.whl

---
+++ packages/jiku-data/jiku_data-0.1.7-py3-none-any.whl/jikudata/datasets/__init__.py
@@ -0,0 +1,58 @@
+
+# from . Besier2009muscleforces import Besier2009muscleforces
+
+
+import os
+__all__ = []
+for s in sorted( os.listdir( os.path.dirname(__file__) ) ):
+    if s[0].isupper():
+        exec(  f'from . {s} import {s}' )
+        __all__.append( s )
+del os, s
+
+
+_skip = ['SpeedPP2DS']
+
+
+def iter_all():
+    for s in __all__:
+        if s not in _skip:
+            dataset = eval( f'{s}()' )
+            yield dataset
+
+
+def iter_by_testname(name, dim=None):
+    for d in iter_all():
+        if d.params.testname == name:
+            if dim is None:
+                yield( d )
+            elif d.dim == dim:
+                yield( d )
+
+
+def get_datasetnames_by_testname(name, dim=None):
+    dsnames = []
+    for d in iter_by_testname(name, dim):
+        dsnames.append( d.name )
+    return dsnames
+    
+def get_datasetnames(testname=None, dim=None):
+    datasets = get_datasets( testname, dim )
+    names    = [d.name for d in datasets]
+    return names
+
+def get_datasets(testname=None, dim=None):
+    datasets = []
+    for d in iter_all():
+        if (testname is None) and (dim is None):
+            datasets.append( d )
+        elif (testname is None) and (dim is not None):
+            if d.dim == dim:
+                datasets.append( d )
+        elif (testname is not None) and (dim is None):
+            if (d.params is not None) and (d.params.testname == testname):
+                datasets.append( d )
+        elif (testname is not None) and (dim is not None):
+            if (d.params is not None) and (d.params.testname == testname) and (d.dim==dim):
+                datasets.append( d )
+    return datasets
