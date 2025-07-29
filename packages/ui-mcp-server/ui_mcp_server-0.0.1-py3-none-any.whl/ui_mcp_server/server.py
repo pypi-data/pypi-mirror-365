42a9ce1df092b6db7dfd65e506f617e464483fc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:11 +0000

    Add package ui-mcp-server/0.0.1/ui_mcp_server-0.0.1-py3-none-any.whl

---
+++ packages/ui-mcp-server/ui_mcp_server-0.0.1-py3-none-any.whl/ui_mcp_server/server.py
@@ -0,0 +1,43 @@
+"""Tools for UI components."""
+
+from mcp.server.fastmcp import FastMCP
+from .models import Choice, NumberInput, TableOutput
+
+
+server = FastMCP("ui-mcp-server")
+
+
+@server.prompt()
+def ui_component_prompt() -> str:  # pragma: no cover
+    """Predefined prompt for UI component generation."""
+    return "Use the tools from the ui-mcp-server to generate a UI components, which will be used in a frontend application."  # noqa: E501
+
+
+@server.tool()
+def number_input(params: NumberInput) -> NumberInput:
+    """Generate a number input component.
+
+    Args:
+        params: Parameters for the number input component.
+    """
+    return params
+
+
+@server.tool()
+def choice(params: Choice) -> Choice:
+    """Generate a choice input component.
+
+    Args:
+        params: Parameters for the choice input component.
+    """
+    return params
+
+
+@server.tool()
+def table_output(params: TableOutput) -> TableOutput:
+    """Generate a table output component.
+
+    Args:
+        params: Parameters for the table output component.
+    """
+    return params
