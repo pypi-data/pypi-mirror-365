ecefca934c41538aea66cc2ec3ff70bb950ff07e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:12 +0000

    Add package liblaf-tangerine/0.1.0/liblaf_tangerine-0.1.0-py3-none-any.whl

---
+++ packages/liblaf-tangerine/liblaf_tangerine-0.1.0-py3-none-any.whl/liblaf/tangerine/core/environment.py
@@ -0,0 +1,84 @@
+import itertools
+from pathlib import Path
+
+import attrs
+import cytoolz as toolz
+import git
+import jinja2
+
+from liblaf import grapes
+
+from .constants import TANGERINE_END, TANGERINE_START
+from .template import Template
+
+type Segment = str | Template
+
+
+def load_copier_answers() -> dict[str, str]:
+    repo = git.Repo(search_parent_directories=True)
+    cwd = Path(repo.working_dir)
+    answers: dict[str, str] = {}
+    for file in itertools.chain(
+        cwd.rglob(".copier-answers.*.yaml"),
+        cwd.rglob(".copier-answers.*.yml"),
+        cwd.rglob(".copier-answers.yaml"),
+        cwd.rglob(".copier-answers.yml"),
+    ):
+        answers.update(grapes.load(file))
+    answers = toolz.keyfilter(lambda k: not k.startswith("_"), answers)
+    return answers
+
+
+def _default_environment() -> jinja2.Environment:
+    return jinja2.Environment(
+        undefined=jinja2.StrictUndefined,
+        autoescape=jinja2.select_autoescape(),
+        loader=jinja2.PackageLoader("liblaf.tangerine"),
+    )
+
+
+@attrs.define
+class Environment:
+    context: dict[str, str] = attrs.field(factory=load_copier_answers)
+    jinja: jinja2.Environment = attrs.field(factory=_default_environment)
+
+    def parse_text(self, text: str) -> list[Segment]:
+        lines: list[str] = text.splitlines()
+        segments: list[Segment] = []
+        in_template: bool = False
+        template_lines: list[str] = []
+        for line in lines:
+            if in_template:
+                template_lines.append(line)
+                if TANGERINE_END in line:
+                    segments.append(Template.from_lines(template_lines))
+                    in_template = False
+            elif TANGERINE_START in line:
+                in_template = True
+                template_lines.append(line)
+            else:
+                segments.append(line)
+        return segments
+
+    def render(self, segments: list[Segment], **kwargs: str) -> str:
+        lines: list[str] = []
+        for segment in segments:
+            if isinstance(segment, Template):
+                lines.append(self.render_template(segment, **kwargs))
+            else:
+                lines.append(segment)
+        text: str = "\n".join(lines)
+        if not text.endswith("\n"):
+            text += "\n"
+        return text
+
+    def render_template(self, template: Template, **kwargs: str) -> str:
+        template_jinja: jinja2.Template = self.jinja.get_template(template.name)
+        kwargs = toolz.merge(self.context, template.context, kwargs)
+        rendered: str = template_jinja.render(kwargs).strip()
+        lines: list[str] = rendered.splitlines()
+        if "-*-" in lines[0]:
+            lines = lines[1:]
+        rendered = "\n".join(lines).strip()
+        rendered = template.start_line + "\n" + rendered + "\n" + template.end_line
+        return rendered
