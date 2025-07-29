4beb3b4841097c8707c402c675e46e7246c18d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:09:01 +0000

    Add package devops-automation-cli/1.1/devops_automation_cli-1.1.tar.gz

---
+++ packages/devops-automation-cli/devops_automation_cli-1.1.tar.gz/devops_automation_cli-1.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+# Read README.md content for long_description
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name='devops_automation_cli',
+    version='1.1',
+    description='AI-powered CLI tool for automating DevOps workflows like setup, Git, CI/CD, Docker, and documentation.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Dinesh',
+    author_email='dinesh121104@gmail.com',
+    url='https://github.com/Dinesh-4/Devops-Automation',
+    packages=find_packages(),
+    install_requires=[
+        'click',
+        'rich',
+        'sh',
+        'loguru',
+        'python-dotenv',
+        'openai'
+    ],
+    entry_points={
+        'console_scripts': [
+            'autodevops=devops_automation_cli.cli:autodevops',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+    ],
+    python_requires='>=3.6',
+)
