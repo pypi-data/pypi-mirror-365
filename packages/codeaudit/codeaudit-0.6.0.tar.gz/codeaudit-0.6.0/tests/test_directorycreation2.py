40f1566dc4e0bcab590c49fbe3ecc996629f279b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:03 +0000

    Add package codeaudit/0.6.0/codeaudit-0.6.0.tar.gz

---
+++ packages/codeaudit/codeaudit-0.6.0.tar.gz/codeaudit-0.6.0/tests/test_directorycreation2.py
@@ -0,0 +1,95 @@
+import pytest
+from pathlib import Path
+
+from codeaudit.filehelpfunctions import read_in_source_file
+from codeaudit.issuevalidations import find_constructs
+
+def test_os_makedirs():
+    current_file_directory = Path(__file__).parent
+    # validation1.py is in a subfolder:
+    validation_file_path = current_file_directory / "validationfiles" / "directorycreation2.py"
+    source = read_in_source_file(validation_file_path)
+    constructs = {'os.makedirs'}
+        
+    actual_data = find_constructs(source, constructs) 
+
+    # This is the expected dictionary
+    expected_data = {'os.makedirs': [10, 79]}
+
+    # Assert that the actual data matches the expected data
+    assert actual_data == expected_data
+
+def test_os_mkdir():
+    current_file_directory = Path(__file__).parent
+    # validation1.py is in a subfolder:
+    validation_file_path = current_file_directory / "validationfiles" / "directorycreation2.py"
+    source = read_in_source_file(validation_file_path)
+    constructs = {'os.mkdir'}
+     
+    actual_data = find_constructs(source, constructs) 
+
+    # This is the expected dictionary
+    expected_data = {'os.mkdir': [26]}
+
+    # Assert that the actual data matches the expected data
+    assert actual_data == expected_data
+
+def test_os_mkfifo():
+    current_file_directory = Path(__file__).parent
+    # validation1.py is in a subfolder:
+    validation_file_path = current_file_directory / "validationfiles" / "directorycreation2.py"
+    source = read_in_source_file(validation_file_path)
+    constructs = {'os.mkfifo'}
+     
+    actual_data = find_constructs(source, constructs) 
+
+    # This is the expected dictionary
+    expected_data = {'os.mkfifo': [48]}
+
+    # Assert that the actual data matches the expected data
+    assert actual_data == expected_data
+
+def test_os_mknod():
+    current_file_directory = Path(__file__).parent
+    # validation1.py is in a subfolder:
+    validation_file_path = current_file_directory / "validationfiles" / "directorycreation2.py"
+    source = read_in_source_file(validation_file_path)
+    constructs = {'os.mknod'}
+     
+    actual_data = find_constructs(source, constructs) 
+
+    # This is the expected dictionary
+    expected_data = {'os.mknod': [65]}
+
+    # Assert that the actual data matches the expected data
+    assert actual_data == expected_data
+
+def test_os_makedirs():
+    current_file_directory = Path(__file__).parent
+    # validation1.py is in a subfolder:
+    validation_file_path = current_file_directory / "validationfiles" / "directorycreation2.py"
+    source = read_in_source_file(validation_file_path)
+    constructs = {'os.makedirs'}
+     
+    actual_data = find_constructs(source, constructs) 
+
+    # This is the expected dictionary
+    expected_data = {'os.makedirs': [13,82]}
+
+    # Assert that the actual data matches the expected data
+    assert actual_data == expected_data
+
+def test_os_makedev():
+    current_file_directory = Path(__file__).parent
+    # validation1.py is in a subfolder:
+    validation_file_path = current_file_directory / "validationfiles" / "directorycreation2.py"
+    source = read_in_source_file(validation_file_path)
+    constructs = {'os.makedev'}
+     
+    actual_data = find_constructs(source, constructs) 
+
+    # This is the expected dictionary
+    expected_data = {'os.makedev': [94]}
+
+    # Assert that the actual data matches the expected data
+    assert actual_data == expected_data
