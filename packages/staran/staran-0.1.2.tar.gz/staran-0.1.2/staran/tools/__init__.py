a3ef88f66acc6eb8e4d6ed0035445e4e752d573e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:17 +0000

    Add package staran/0.1.2/staran-0.1.2.tar.gz

---
+++ packages/staran/staran-0.1.2.tar.gz/staran-0.1.2/staran/tools/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Staran Tools - 简化的工具集合
+提供日期处理等实用功能
+"""
+
+# 导入date模块的主要类和函数
+from .date import Date
+
+# 主要导出
+__all__ = [
+    'Date'
+]
+
+# 模块信息
+__version__ = '1.0.0'
+__author__ = 'Staran Team'
+__description__ = 'Staran simple tools collection'
