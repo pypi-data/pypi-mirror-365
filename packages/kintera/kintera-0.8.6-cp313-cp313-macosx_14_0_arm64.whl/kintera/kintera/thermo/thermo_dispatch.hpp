e0f11b01007aba931a6e334c95808988ed4c8d1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:22 +0000

    Add package kintera/0.8.6/kintera-0.8.6-cp313-cp313-macosx_14_0_arm64.whl

---
+++ packages/kintera/kintera-0.8.6-cp313-cp313-macosx_14_0_arm64.whl/kintera/kintera/thermo/thermo_dispatch.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+// torch
+#include <ATen/TensorIterator.h>
+#include <ATen/native/DispatchStub.h>
+
+using user_func1 = double (*)(double);
+using user_func2 = double (*)(double, double);
+
+namespace at::native {
+
+using equilibrate_tp_fn = void (*)(at::TensorIterator &iter, int ngas,
+                                   user_func1 const *logsvp_func,
+                                   float logsvp_eps, int max_iter);
+
+using equilibrate_uv_fn = void (*)(at::TensorIterator &iter,
+                                   user_func1 const *logsvp_func,
+                                   user_func1 const *logsvp_func_ddT,
+                                   user_func2 const *intEng_extra,
+                                   user_func2 const *intEng_extra_ddT,
+                                   float logsvp_eps, int max_iter);
+
+DECLARE_DISPATCH(equilibrate_tp_fn, call_equilibrate_tp);
+DECLARE_DISPATCH(equilibrate_uv_fn, call_equilibrate_uv);
+
+}  // namespace at::native
