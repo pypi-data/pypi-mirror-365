830c8484e5ee3331759e4dc5e207ccb242787dd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:26 +0000

    Add package nats-fasterapi/0.1.0/nats-fasterapi-0.1.0.tar.gz

---
+++ packages/nats-fasterapi/nats-fasterapi-0.1.0.tar.gz/nats-fasterapi-0.1.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nats-fasterapi",
+    version="0.1.0",
+    description="A CLI tool to scaffold FastAPI projects with CRUD and schema support",
+    author="Nathaniel Uriri",
+    author_email="nat@uriri.com.ng",
+
+    packages=find_packages(),
+    include_package_data=True,
+
+    install_requires=[
+        "click",
+        "pymongo",
+        "motor",
+        "redis",
+        "requests",
+        "pyjwt",
+        "python-dotenv",
+    ],
+
+    entry_points={
+        "console_scripts": [
+            "fasterapi=fasterapi.cli:cli",
+        ],
+    },
+
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+    python_requires=">=3.8",
+)
