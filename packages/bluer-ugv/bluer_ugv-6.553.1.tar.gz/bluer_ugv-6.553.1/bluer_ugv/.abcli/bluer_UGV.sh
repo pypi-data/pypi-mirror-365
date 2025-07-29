7383be7614d329971f1a4d7534b2d1700ebcb561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:06:47 +0000

    Add package bluer-ugv/6.553.1/bluer_ugv-6.553.1.tar.gz

---
+++ packages/bluer-ugv/bluer_ugv-6.553.1.tar.gz/bluer_ugv-6.553.1/bluer_ugv/.abcli/bluer_UGV.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+function bluer_ugv() {
+    local task=${1:-version}
+
+    bluer_ai_generic_task \
+        plugin=bluer_ugv,task=$task \
+        "${@:2}"
+}
+
+bluer_ai_log $(bluer_ugv version --show_icon 1)
