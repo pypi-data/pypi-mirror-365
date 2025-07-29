192a067dc04bf5d4136379f68399db7e0f4af4df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:11 +0000

    Add package abstractrepo/0.8.0/abstractrepo-0.8.0-py3-none-any.whl

---
+++ packages/abstractrepo/abstractrepo-0.8.0-py3-none-any.whl/abstractrepo/order.py
@@ -0,0 +1,63 @@
+import abc
+from enum import Enum
+from typing import List, Tuple, Union
+
+
+class OrderDirection(Enum):
+    ASC = 'ASC'
+    DESC = 'DESC'
+
+
+class NonesOrder(Enum):
+    FIRST = 'FIRST'
+    LAST = 'LAST'
+
+
+OrderOptionsTuple = Union[Tuple[str], Tuple[str, OrderDirection], Tuple[str, OrderDirection, NonesOrder]]
+
+
+class OrderOption:
+    attribute: str
+    direction: OrderDirection
+    nones_order: NonesOrder
+
+    def __init__(self, attribute: str, direction: OrderDirection, nones_order: NonesOrder = NonesOrder.FIRST):
+        self.attribute = attribute
+        self.direction = direction
+        self.nones_order = nones_order
+
+
+class OrderOptions:
+    _options: List[OrderOption]
+
+    def __init__(self, *options: OrderOption):
+        self._options = list(options)
+
+    @property
+    def options(self):
+        return self._options
+
+
+class OrderOptionsBuilder:
+    _params: List[OrderOption]
+
+    def __init__(self):
+        self._params = []
+
+    def add(self, attribute: str, direction: OrderDirection = OrderDirection.ASC, nones_order: NonesOrder = NonesOrder.LAST) -> "OrderOptionsBuilder":
+        self._params.append(OrderOption(attribute, direction, nones_order))
+        return self
+
+    def add_mass(self, *items: OrderOptionsTuple) -> "OrderOptionsBuilder":
+        for item in items:
+            self.add(*item)
+        return self
+
+    def build(self) -> OrderOptions:
+        return OrderOptions(*self._params)
+
+
+class OrderOptionsConverterInterface(abc.ABC):
+    @abc.abstractmethod
+    def convert(self, order: OrderOptions) -> OrderOptions:
+        raise NotImplementedError()
