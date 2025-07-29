9087599497778f973d6f96900ab80214a0f66d2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:04:55 +0000

    Add package flask-apm-watchlog/1.0.1/flask_apm_watchlog-1.0.1.tar.gz

---
+++ packages/flask-apm-watchlog/flask_apm_watchlog-1.0.1.tar.gz/flask_apm_watchlog-1.0.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="flask-apm-watchlog",
+    license="MIT",
+    version="1.0.1",
+    description="Flask instrumentation for Watchlog APM with JSON OTLP export",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Mohammadreza",
+    author_email="mohammadnajm75@gmail.com",
+    url="https://github.com/Watchlog-monitoring/flask_watchlog_apm",
+    packages=find_packages(),
+    install_requires=[
+        # Core API & SDK
+        "opentelemetry-api>=1.9.0",
+        "opentelemetry-sdk>=1.9.0",
+
+        # OTLP HTTP exporter
+        "opentelemetry-exporter-otlp-proto-http>=1.9.0",
+
+        # Instrumentation for Flask & outgoing HTTP
+        "opentelemetry-instrumentation-flask>=0.34b0",
+        "opentelemetry-instrumentation-requests>=0.34b0",
+
+        # برای شناسایی Kubernetes DNS
+        "dnspython>=2.0.0",
+    ],
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Framework :: Flask",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
