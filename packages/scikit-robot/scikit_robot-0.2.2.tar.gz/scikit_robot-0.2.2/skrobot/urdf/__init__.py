07fd4c556f437b7886828cc26385c5d1dfe6f01f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:39 +0000

    Add package scikit-robot/0.2.2/scikit_robot-0.2.2.tar.gz

---
+++ packages/scikit-robot/scikit_robot-0.2.2.tar.gz/scikit_robot-0.2.2/skrobot/urdf/__init__.py
@@ -0,0 +1,12 @@
+from .modularize_urdf import find_root_link
+from .modularize_urdf import transform_urdf_to_macro
+from .xml_root_link_changer import change_urdf_root_link
+from .xml_root_link_changer import URDFXMLRootLinkChanger
+
+
+__all__ = [
+    'change_urdf_root_link',
+    'URDFXMLRootLinkChanger',
+    'find_root_link',
+    'transform_urdf_to_macro',
+]
