d2f70cc871b3191f27180d561e9fdcfe2e6afa99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:41 +0000

    Add package mycli/1.37.0/mycli-1.37.0-py3-none-any.whl

---
+++ packages/mycli/mycli-1.37.0-py3-none-any.whl/mycli/packages/paramiko_stub/__init__.py
@@ -0,0 +1,32 @@
+"""A module to import instead of paramiko when it is not available (to avoid
+checking for paramiko all over the place).
+
+When paramiko is first envoked, it simply shuts down mycli, telling
+user they either have to install paramiko or should not use SSH
+features.
+
+"""
+
+
+class Paramiko:
+    def __getattr__(self, name: str) -> None:
+        import sys
+        from textwrap import dedent
+
+        print(
+            dedent("""
+            To enable certain SSH features you need to install paramiko and sshtunnel:
+
+               pip install paramiko sshtunnel
+
+            It is required for the following configuration options:
+                --list-ssh-config
+                --ssh-config-host
+                --ssh-host
+            """),
+            file=sys.stderr,
+        )
+        sys.exit(1)
+
+
+paramiko = Paramiko()
