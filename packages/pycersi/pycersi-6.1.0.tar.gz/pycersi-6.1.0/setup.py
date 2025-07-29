357ba2221b455037bd82e6eb425f30bdaf1d667f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:47 +0000

    Add package pycersi/6.1.0/pycersi-6.1.0.tar.gz

---
+++ packages/pycersi/pycersi-6.1.0.tar.gz/pycersi-6.1.0/setup.py
@@ -0,0 +1,24 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="pycersi",
+    version="6.1.0",
+    install_requires=[],
+    description="PyCersi is a lightweight Python library for number-related utilities. It offers mathematical functions, checkers, and stack operations, simplifying common tasks. Easy to install via pip , it's designed for efficient integration into various Python projects.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/subhrachakraborti/PyCersi",
+    author="Subhra Chakraborti",
+    author_email="mail@subhrachakraborti.com",
+    license="MIT",
+    keywords="PyCersi",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">3.5",
+)
+
