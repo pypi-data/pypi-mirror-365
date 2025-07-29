aefce6ef6a2456d17a04ab93294273ab8dbb139b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:22 +0000

    Add package async_garlandtools/0.1.3.dev0/async_garlandtools-0.1.3.dev0-py3-none-any.whl

---
+++ packages/async_garlandtools/async_garlandtools-0.1.3.dev0-py3-none-any.whl/async_garlandtools/__init__.py
@@ -0,0 +1,49 @@
+"""Copyright (C) 2021-2025 Katelynn Cadwallader.
+
+This file is part of GarlandToolsAPI_wrapper.
+
+GarlandToolsAPI_wrapper is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+GarlandToolsAPI_wrapper is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GarlandToolsAPI_wrapper; see the file COPYING.  If not, write to the Free
+Software Foundation, 51 Franklin Street - Fifth Floor, Boston, MA
+02110-1301, USA.
+"""
+
+from __future__ import annotations
+
+__title__ = "GarlandToolsAPI_wrapper"
+__author__ = "k8thekat"
+__license__ = "GNU"
+__version__ = "0.1.3-dev"
+__credits__ = "Universalis, GarlandTools, GarlandTools-PIP and SquareEnix"
+
+from typing import Literal, NamedTuple
+
+from ._enums import *
+from .modules import *
+
+
+class VersionInfo(NamedTuple):  # noqa: D101
+    major: int
+    minor: int
+    revision: int
+    release_level: Literal["release", "development"]
+
+
+version_info: VersionInfo = VersionInfo(  # noqa: F821
+    major=0,
+    minor=1,
+    revision=3,
+    release_level="development",
+)
+
+del VersionInfo
