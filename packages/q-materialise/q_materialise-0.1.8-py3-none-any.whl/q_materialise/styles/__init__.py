deefcc4e4a551dac838bb7dde14880fe45842f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:36 +0000

    Add package q-materialise/0.1.8/q_materialise-0.1.8-py3-none-any.whl

---
+++ packages/q-materialise/q_materialise-0.1.8-py3-none-any.whl/q_materialise/styles/__init__.py
@@ -0,0 +1,13 @@
+"""
+Built-in style package.
+
+This package contains JSON files defining a selection of default styles.
+Additional styles can be added by dropping a `*.json` file into this directory.
+Each file must contain a mapping compatible with the
+`q_materialise.style.Style` constructor.
+
+Note:
+    You should not import anything from this module directly. Instead, use
+    `q_materialise.list_styles` and `q_materialise.get_style` to discover and
+    load styles.
+"""
