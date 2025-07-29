f004c5c8a104d768683ad1bef2fb74bf6ba8c901
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:04:18 +0000

    Add package model-logger-dp/1.0.3.post2/model_logger_dp-1.0.3.post2.tar.gz

---
+++ packages/model-logger-dp/model_logger_dp-1.0.3.post2.tar.gz/model_logger_dp-1.0.3.post2/model_logger_dp/constant.py
@@ -0,0 +1,10 @@
+text_colors = {
+    "logs": "\033[34m",  # 033 is the escape code and 34 is the color code
+    "info": "\033[32m",
+    "warning": "\033[33m",
+    "debug": "\033[93m",
+    "error": "\033[31m",
+    "bold": "\033[1m",
+    "end_color": "\033[0m",
+    "light_red": "\033[36m",
+}
