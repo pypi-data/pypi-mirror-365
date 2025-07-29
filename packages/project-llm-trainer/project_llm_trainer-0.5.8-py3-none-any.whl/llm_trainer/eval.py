4cf4b33fac943344b6e59a812a1edea0a3890072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:55 +0000

    Add package project-llm-trainer/0.5.8/project_llm_trainer-0.5.8-py3-none-any.whl

---
+++ packages/project-llm-trainer/project_llm_trainer-0.5.8-py3-none-any.whl/llm_trainer/eval.py
@@ -0,0 +1,33 @@
+import torch
+
+from .generate_utils import generate
+from .log import get_log_dir
+from .tools import TrainerTools
+from .train_configs import EvalConfig
+
+def submit_gen_task(
+        eval_model: torch.nn.Module,
+        eval_config: EvalConfig,
+        tag,
+        prompt,
+        pixel_values,
+        max_position_embeddings,
+        tokens_per_image
+):
+    log_dir = get_log_dir()
+
+    gen_result = generate(
+        eval_model,
+        prompt=prompt,
+        max_position_embeddings=max_position_embeddings,
+        max_new_tokens=eval_config.max_new_tokens,
+        temperature=eval_config.temperature,
+        k=eval_config.top_k,
+        p=eval_config.top_p,
+        pixel_values=pixel_values,
+        tokens_per_image=tokens_per_image,
+        device=TrainerTools().parallel.device
+    )
+
+    with open(f'{log_dir}gen.txt', 'a') as f:
+        f.write(f"{tag}, gen->{gen_result}\n")
