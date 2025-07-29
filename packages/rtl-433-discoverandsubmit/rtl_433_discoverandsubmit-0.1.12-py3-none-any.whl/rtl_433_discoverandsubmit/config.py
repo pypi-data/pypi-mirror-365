eb0af92b56a30b136f7bf75de7c2cca3364a6782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:43 +0000

    Add package rtl-433-discoverandsubmit/0.1.12/rtl_433_discoverandsubmit-0.1.12-py3-none-any.whl

---
+++ packages/rtl-433-discoverandsubmit/rtl_433_discoverandsubmit-0.1.12-py3-none-any.whl/rtl_433_discoverandsubmit/config.py
@@ -0,0 +1,11 @@
+# config.py
+configuration = {
+    'mqtt_server': None,
+    'mqtt_port': None,
+    'mqtt_username': None,
+    'mqtt_password': None,
+    'topic': None,
+    'current_sort_criteria': "last_detected_time",
+    'log_filename': 'rtl_433_discoverandsubmit.log',
+    'log_level': 'ERROR'
+}
---
+++ packages/rtl-433-discoverandsubmit/rtl_433_discoverandsubmit-0.1.12-py3-none-any.whl/rtl_433_discoverandsubmit/modules/config_loader.py
@@ -0,0 +1,37 @@
+import json
+import os
+import pkg_resources
+import logging
+
+config_path = pkg_resources.resource_filename('rtl_433_discoverandsubmit', 'config/cli_config.json')
+try:
+    device_mappings_path = pkg_resources.resource_filename(
+        'custom_components.rtl_433_discoverandsubmit',
+        'config/device_mappings.json'
+    )
+except ModuleNotFoundError:
+    device_mappings_path = os.path.join(
+        os.path.dirname(__file__),
+        '../custom_components/rtl_433_discoverandsubmit/config/device_mappings.json'
+    )
+
+
+# Define Configuration Paths
+CLI_CONFIG_PATH = os.path.join(os.path.dirname(__file__), '../config/cli_config.json')
+DEVICE_MAPPINGS_PATH = os.path.join(
+    os.path.dirname(__file__),
+    '../custom_components/rtl_433_discoverandsubmit/config/device_mappings.json'
+)
+
+
+
+def load_device_mappings(file_path=device_mappings_path):
+    """
+    Load device mappings from a JSON file.
+
+    :param file_path: Path to the JSON file containing the device mappings.
+    :return: Loaded device mappings.
+    """
+    logging.info(f"devicemappings path: {file_path}")
+    with open(file_path, "rb") as file:
+        return json.load(file)
