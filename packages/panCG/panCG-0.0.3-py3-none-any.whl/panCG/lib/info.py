1bd4cb3df1806ee577bd446354491d9ac645690e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:31 +0000

    Add package panCG/0.0.3/panCG-0.0.3-py3-none-any.whl

---
+++ packages/panCG/panCG-0.0.3-py3-none-any.whl/panCG/lib/info.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '0.0.3'
+__author__ = "Lei Tan"
+__email__ = "lei.tan.bio@outlook.com"
+__doc__ = """
+    an integrative pipeline for family-level super-pangenome analysis across coding and noncoding sequences. 
+"""
