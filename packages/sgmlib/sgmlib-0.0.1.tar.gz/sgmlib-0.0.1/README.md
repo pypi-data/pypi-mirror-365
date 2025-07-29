28eaec091fc88e8ac9040587792e91dc27159842
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:06:52 +0000

    Add package sgmlib/0.0.1/sgmlib-0.0.1.tar.gz

---
+++ packages/sgmlib/sgmlib-0.0.1.tar.gz/sgmlib-0.0.1/README.md
@@ -0,0 +1,7 @@
+# Sentence Gestalt Model Library (SGMLIB)
+
+This repository contains a Python implementation of the Sentence Gestalt Model Library (SGMLIB) introduced in St. John & McClelland (1990).
+
+More information to be added.
+
+
---
+++ packages/sgmlib/sgmlib-0.0.1.tar.gz/sgmlib-0.0.1/sgmlib/__init__.py
@@ -0,0 +1,19 @@
+"""
+Sentence Gestalt Model Library (SGMLIB) - A PyTorch implementation of St. John & McClelland (1990).
+
+This package provides a faithful implementation of the Sentence Gestalt Model 
+for learning and applying contextual constraints in sentence comprehension.
+"""
+
+__version__ = "0.0.1"
+__author__ = "Adrian Brasoveanu"
+__email__ = "abrsvn@gmail.com"
+
+# Placeholder for future imports when full implementation is ready
+# from .model import SentenceGestaltModel
+# from .training import SGTrainer
+# from .evaluation import SGEvaluator
+
+def hello_sgmlib():
+    """Simple function to verify package installation."""
+    return "SGMLIB package successfully installed!"
