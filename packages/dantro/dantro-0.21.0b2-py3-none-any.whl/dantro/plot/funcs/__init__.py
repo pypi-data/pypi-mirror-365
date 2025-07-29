a3939caf00069ed298c689ec0656a8610465e2d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:04:33 +0000

    Add package dantro/0.21.0b2/dantro-0.21.0b2-py3-none-any.whl

---
+++ packages/dantro/dantro-0.21.0b2-py3-none-any.whl/dantro/plot/funcs/__init__.py
@@ -0,0 +1,8 @@
+"""Plotting functions that can be used by the
+:py:class:`~dantro.plot.creators.pyplot.PyPlotCreator` and derived plot
+creators.
+"""
+
+from .basic import lineplot
+from .generic import errorbars, facet_grid, scatter3d
+from .multiplot import multiplot
