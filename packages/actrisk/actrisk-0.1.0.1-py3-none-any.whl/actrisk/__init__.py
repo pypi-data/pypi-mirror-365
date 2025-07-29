c3c2a155d29c234fa1e4913d78c9027acd1eef90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:18 +0000

    Add package actrisk/0.1.0.1/actrisk-0.1.0.1-py3-none-any.whl

---
+++ packages/actrisk/actrisk-0.1.0.1-py3-none-any.whl/actrisk/__init__.py
@@ -0,0 +1,17 @@
+from .core.actfitter import DistributionFitter
+from .core.actsimulator import StochasticSimulator 
+from .utils.utils import Config
+
+def load_config(file_path: str = None):
+    """
+    unction to load the default config or a custom config file.
+    
+    Args:
+        file_path: Path to a custom config file. If None, uses the default config.
+    
+    Returns:
+        Config object with the loaded configuration.
+    """
+    return Config(file_path)
+
+__all__ = ["DistributionFitter", "StochasticSimulator", "Config", "load_config"]
