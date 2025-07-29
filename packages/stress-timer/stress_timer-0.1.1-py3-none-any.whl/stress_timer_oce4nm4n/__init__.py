6d738f6e6d795f92c6e09272c80a6aa0b714f033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:53 +0000

    Add package stress-timer/0.1.1/stress_timer-0.1.1-py3-none-any.whl

---
+++ packages/stress-timer/stress_timer-0.1.1-py3-none-any.whl/stress_timer_oce4nm4n/__init__.py
@@ -0,0 +1,7 @@
+# __init__.py
+from .timer import main
+
+__version__ = "0.1.1"
+
+if __name__ == "__main__":
+    main()
---
+++ packages/stress-timer/stress_timer-0.1.1-py3-none-any.whl/stress_timer_oce4nm4n/main.py
@@ -0,0 +1,6 @@
+# stress-timer_oce4nm4n/main.py
+
+from .timer import main as timer_main
+
+def main():
+    timer_main()
