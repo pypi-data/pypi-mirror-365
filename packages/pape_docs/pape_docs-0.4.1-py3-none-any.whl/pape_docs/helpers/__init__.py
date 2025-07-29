d46c328492ff45d4d6a54d0e47ca3de8f5af1a26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:53 +0000

    Add package pape_docs/0.4.1/pape_docs-0.4.1-py3-none-any.whl

---
+++ packages/pape_docs/pape_docs-0.4.1-py3-none-any.whl/pape_docs/helpers/__init__.py
@@ -0,0 +1,8 @@
+# # # # # # # # # # # # # # # # # # # #
+# Pape Docs
+# Copyright 2025 Carter Pape
+#
+# See file LICENSE for licensing terms.
+# # # # # # # # # # # # # # # # # # # #
+
+"""A set of helper functions for the main pape-docs CLI."""
