af121f72342276b0c4fbc577193c12ac5c77f412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:34 +0000

    Add package relation/1.0/relation-1.0-py3-none-any.whl

---
+++ packages/relation/relation-1.0-py3-none-any.whl/relation/__init__.py
@@ -0,0 +1,123 @@
+class Relator:
+    def __init__(self, name, relations=None, children=None, parents=None):
+        self.name = name
+        self.relations = relations if relations is not None else []
+        self.children = children if children is not None else []
+        self.parents = parents if parents is not None else []
+        for i in self.relations:
+            if self not in i.relations:
+                i.relations.append(self)
+        for i in self.children:
+            if self not in i.parents:
+                i.parents.append(self)
+        for i in self.parents:
+            if self not in i.children:
+                i.children.append(self)
+    
+    def __mul__(self, other):
+        if other not in self.relations:
+            self.relations.append(other)
+        if isinstance(other, Relator) and self not in other.relations:
+            other.relations.append(self)
+    
+    def __add__(self, other):
+        if not isinstance(other,type([])):
+            yes = [other]
+        else:
+            yes = other
+        for i in yes:
+            if i not in self.children:
+                self.children.append(i)
+            if isinstance(i, Relator) and self not in i.parents:
+                i.parents.append(self)
+    def __radd__(self, other):
+        if not isinstance(other,type([])):
+            yes = [other]
+        else:
+            yes = other
+        for i in yes:
+            if i not in self.parents:
+                self.parents.append(i)
+            if isinstance(i, Relator) and self not in i.children:
+                i.children.append(self)
+
+    def __ior__(self, other):
+        self.children += other.children
+        self.relations += other.relations
+        self.parents += other.parents
+        return self
+    
+    def __or__(self, other):
+        self.children += other.children
+        self.relations += other.relations
+        self.parents += other.parents
+        other.children += self.children
+        other.relations += self.relations
+        other.parents += self.parents
+
+        
+    def __str__(self):
+        return self.name
+    
+    def relations_names(self):
+        return [str(i) for i in self.relations]
+    
+    def children_names(self):
+        return [str(i) for i in self.children]
+    
+    def parents_names(self):
+        return [str(i) for i in self.parents]
+    def relation_is_type_names(self, type):
+        return [str(i) for i in self.relations if isinstance(i,type)]
+    def children_is_type_names(self, type):
+        return [str(i) for i in self.children if isinstance(i,type)]
+    def parents_is_type_names(self, type):
+        return [str(i) for i in self.parents if isinstance(i,type)]
+    def relation_is_type(self, type):
+        return [i for i in self.relations if isinstance(i,type)]
+    def children_is_type(self, type):
+        return [i for i in self.children if isinstance(i,type)]
+    def parents_is_type(self, type):
+        return [i for i in self.parents if isinstance(i,type)]
+        
+    def color(self):
+        has_relations = len(self.relations) > 0
+        has_children = len(self.children) > 0
+        has_parents = len(self.parents) > 0
+        if has_relations and has_children and has_parents:
+            return "SkyBlue"
+        elif has_relations and has_children:
+            return "Khaki"
+        elif has_relations and has_parents:
+            return "LightGreen"
+        elif has_children and has_parents:
+            return "LightGrey"
+        elif has_relations:
+            return "DodgerBlue"
+        elif has_children:
+            return "Yellow"
+        elif has_parents:
+            return "LimeGreen"
+        else:
+            return "Grey"
+# deprecated, use issubclass(obj, Relator) instead
+
+# def is_relator(cls):
+#     class_type = type(cls)
+#     if class_type is Relator:
+#         return True
+#     if Relator in class_type.__bases__:
+#         return True
+#     for base in class_type.__bases__:
+#         if type_is_relator(base):
+#             return True
+#     return False
+
+# def type_is_relator(class_type):
+#     if class_type is Relator:
+#         return True
+#     if Relator in class_type.__bases__:
+#         return True
+#     for base in class_type.__bases__:
+#         if type_is_relator(base):
+#             return True
