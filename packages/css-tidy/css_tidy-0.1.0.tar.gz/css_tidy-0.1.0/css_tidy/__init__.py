82cc02c6cfc2926cb0c89b111bbcee4d0b19d043
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:09 +0000

    Add package css-tidy/0.1.0/css_tidy-0.1.0.tar.gz

---
+++ packages/css-tidy/css_tidy-0.1.0.tar.gz/css_tidy-0.1.0/css_tidy/__init__.py
@@ -0,0 +1,20 @@
+"""
+CSS Tidy - A Python tool to tidy and format CSS files.
+
+This module provides functionality to format, minify, and validate CSS code.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Henry Lok"
+__email__ = "henry.lok@example.com"
+
+from .tidy import CSSFormatter, CSSMinifier, CSSValidator
+
+__all__ = [
+    "CSSFormatter",
+    "CSSMinifier", 
+    "CSSValidator",
+    "__version__",
+    "__author__",
+    "__email__",
+] 
