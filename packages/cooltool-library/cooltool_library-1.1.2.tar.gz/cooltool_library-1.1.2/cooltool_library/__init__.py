3d4beb96dc9eaa1ec12f06d3ba3884a6f41b7097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:36 +0000

    Add package cooltool-library/1.1.2/cooltool_library-1.1.2.tar.gz

---
+++ packages/cooltool-library/cooltool_library-1.1.2.tar.gz/cooltool_library-1.1.2/cooltool_library/__init__.py
@@ -0,0 +1,9 @@
+"""
+CoolTool Library
+
+A library with many functionality to became easly the code writing end reading
+
+The principal Class is CTl (Cool Tool library) and it contains the main functions
+"""
+
+from .cooltool_library import *
---
+++ packages/cooltool-library/cooltool_library-1.1.2.tar.gz/cooltool_library-1.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name="cooltool_library",
+    version='1.1.2',
+    packages=['cooltool_library'],
+    author='Quadzeco',
+    author_email='example@example.com',
+    description='Simple library with many function to help you with consol project',
+    long_description=long_description,
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
