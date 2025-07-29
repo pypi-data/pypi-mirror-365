86cf6aa8f90aba5c825697fc2e76e930ff31e183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:16 +0000

    Add package Susmita-Pattern-printing/0.1.0/susmita_pattern_printing-0.1.0.tar.gz

---
+++ packages/Susmita-Pattern-printing/susmita_pattern_printing-0.1.0.tar.gz/susmita_pattern_printing-0.1.0/pattern_pack/pattern1.py
@@ -0,0 +1,23 @@
+def pyramid(rows):
+        pattern=""
+        for i in range(1,rows+1):
+             pattern+=" " *(rows-i)+"* " * i +"\n"
+        return pattern    
+
+
+def right_angle(rows):
+    pattern=""
+    for i in range(1,rows+1):
+        pattern+="*" * i+"\n"
+    return pattern 
+
+
+def left_angle(rows):
+    pattern=""
+    for i in range(1,rows+1):
+         pattern+=" " * (rows-i) + "*" * i+"\n"
+    return pattern 
+
+
+p=right_angle(3)
+print(p)
