ade496a0924bda391bd8c6124f9ea09792b2f29d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:14 +0000

    Add package canadatestgen/0.1.0/canadatestgen-0.1.0.tar.gz

---
+++ packages/canadatestgen/canadatestgen-0.1.0.tar.gz/canadatestgen-0.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='canada-testgen',
+    version='0.1.0',
+    description='Generate realistic Canadian test data from schema',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Jeremie Nombro',
+    author_email='guillaumenombro@gmail.com',
+    url='https://github.com/your-username/canada-testgen',
+    packages=find_packages(),
+    install_requires=[
+        'faker',
+        'click'
+    ],
+    entry_points={
+        'console_scripts': [
+            'canada-testgen=canada_testgen.cli:main'
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
