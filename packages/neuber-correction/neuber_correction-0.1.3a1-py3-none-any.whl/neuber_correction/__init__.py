0145557ff1a71d801d87f69def4604aa78e27edf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:06:48 +0000

    Add package neuber-correction/0.1.3a1/neuber_correction-0.1.3a1-py3-none-any.whl

---
+++ packages/neuber-correction/neuber_correction-0.1.3a1-py3-none-any.whl/neuber_correction/__init__.py
@@ -0,0 +1,8 @@
+from neuber_correction.neuber_correction import (
+    NeuberCorrection,
+    MaterialForNeuberCorrection,
+    NeuberSolverSettings,
+)
+
+__version__ = "0.1.3-alpha1"
+__all__ = ["NeuberCorrection", "MaterialForNeuberCorrection", "NeuberSolverSettings"]
