1e0dd414e2be9cf27b456ad00bdba780d96a1c43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:42 +0000

    Add package general-calculator-zsd/1.5.0/general_calculator_zsd-1.5.0.tar.gz

---
+++ packages/general-calculator-zsd/general_calculator_zsd-1.5.0.tar.gz/general_calculator_zsd-1.5.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='general_calculator_zsd',
+    version='1.4.9.5',
+    author='colin zhang',
+    author_email='zsd0830@163.com',
+    description='hupu添加keyword',
+    long_description_content_type="""text/markdown""",
+    url='',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
