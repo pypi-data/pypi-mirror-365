bcf8282f0f4d2d69e47fc9c3b4f63ef80a480853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:02 +0000

    Add package nats-fasterapi/0.2.3/nats-fasterapi-0.2.3.tar.gz

---
+++ packages/nats-fasterapi/nats-fasterapi-0.2.3.tar.gz/nats-fasterapi-0.2.3/fasterapi/scaffolder/templates/project_template/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello from FasterAPI!"}
+
+@app.get("/health")
+async def health_check():
+    return {"status": "healthy"}
+    
