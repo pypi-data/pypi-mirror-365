6bd7654c552938f4c7867512a073f37535d4ad7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:55 +0000

    Add package notata/0.2.0/notata-0.2.0.tar.gz

---
+++ packages/notata/notata-0.2.0.tar.gz/notata-0.2.0/notata/__init__.py
@@ -0,0 +1,13 @@
+from notata.logbook import Logbook
+from notata.experiment import Experiment
+from notata.reader import LogReader, ExperimentReader
+
+__version__ = "0.2.0"
+
+__all__ = [
+    "Logbook",
+    "Experiment",
+    "LogReader",
+    "ExperimentReader",
+    "__version__",
+      ]
