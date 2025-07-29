941e26d79cd6b69f829fab1fb1d702adb8dbdfd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:25 +0000

    Add package ygt-menu/0.1.0/ygt_menu-0.1.0-py3-none-any.whl

---
+++ packages/ygt-menu/ygt_menu-0.1.0-py3-none-any.whl/ygt_menu/run_menu.py
@@ -0,0 +1,12 @@
+from menu import menu
+
+def func_a():
+    return "A"
+
+def func_b():
+    return "B"
+
+
+if __name__ == '__main__':
+    return_value = menu(a=func_a, b=func_b)
+    print(f'Result is {return_value}')
