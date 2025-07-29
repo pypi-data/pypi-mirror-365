ba816965c1738f7102cd7d962b88f6f325ba55d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:29 +0000

    Add package coldpack/0.1.1a1/coldpack-0.1.1a1-py3-none-any.whl

---
+++ packages/coldpack/coldpack-0.1.1a1-py3-none-any.whl/coldpack/utils/__init__.py
@@ -0,0 +1,34 @@
+"""Utility modules for coldpack operations."""
+
+from .compression import ZstdCompressor, ZstdDecompressor
+from .filesystem import (
+    check_disk_space,
+    cleanup_temp_directory,
+    create_temp_directory,
+    safe_file_operations,
+    validate_paths,
+)
+from .hashing import DualHasher, HashVerifier
+from .par2 import PAR2Manager
+from .progress import ProgressTracker, create_progress_callback
+from .windows_compat import (
+    check_par2_related_paths_compatibility,
+    check_windows_par2_unicode_compatibility,
+)
+
+__all__ = [
+    "create_temp_directory",
+    "cleanup_temp_directory",
+    "check_disk_space",
+    "validate_paths",
+    "safe_file_operations",
+    "ZstdCompressor",
+    "ZstdDecompressor",
+    "DualHasher",
+    "HashVerifier",
+    "PAR2Manager",
+    "ProgressTracker",
+    "create_progress_callback",
+    "check_par2_related_paths_compatibility",
+    "check_windows_par2_unicode_compatibility",
+]
