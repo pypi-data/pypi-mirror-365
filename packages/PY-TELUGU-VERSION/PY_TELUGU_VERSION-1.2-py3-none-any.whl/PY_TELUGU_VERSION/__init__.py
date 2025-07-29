a3a7cb79650e8e5d1432b5baeda6defc5fe1f9e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:44 +0000

    Add package PY-TELUGU-VERSION/1.2/PY_TELUGU_VERSION-1.2-py3-none-any.whl

---
+++ packages/PY-TELUGU-VERSION/PY_TELUGU_VERSION-1.2-py3-none-any.whl/PY_TELUGU_VERSION/__init__.py
@@ -0,0 +1,10 @@
+from .auto_update import eng2tel, tel2eng
+from .auto_update import _watch as watch
+from .main import run_telugu_file as run
+
+__all__ = [
+    "eng2tel",
+    "tel2eng",
+    "watch",
+    "run",
+]
