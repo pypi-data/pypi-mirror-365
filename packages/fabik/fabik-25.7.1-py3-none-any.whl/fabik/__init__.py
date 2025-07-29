c9388c87a34e1b73dc390486326a8fa573da0843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:53 +0000

    Add package fabik/25.7.1/fabik-25.7.1-py3-none-any.whl

---
+++ packages/fabik/fabik-25.7.1-py3-none-any.whl/fabik/__init__.py
@@ -0,0 +1,26 @@
+"""
+fabik is a library for Python command line application.
+-----------------------
+"""
+
+__version__ = '25.7.1'
+__author__ = 'zrong'
+__name__ = 'fabik'
+__email__ = 'zrongzrong@gmail.com'
+__url__ = 'https://github.com/zrong/fabik'
+__license__ = 'MIT'
+__description__ = 'fabik is a library for Python command line application.'
+
+import datetime
+__now__ = datetime.datetime.now()
+
+__all__ = [
+    '__version__',
+    '__author__',
+    '__email__',
+    '__url__',
+    '__license__',
+    '__description__',
+    '__now__',
+]
+
