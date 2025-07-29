61231d25a99fbf672888a121d078bf073f74c428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:01 +0000

    Add package umlshapes/0.8.5/umlshapes-0.8.5-py3-none-any.whl

---
+++ packages/umlshapes/umlshapes-0.8.5-py3-none-any.whl/umlshapes/shapes/PositionReporter.py
@@ -0,0 +1,20 @@
+
+from typing_extensions import Protocol
+
+
+class PositionReporter(Protocol):
+    """
+    My one and only use of a protocol class
+    """
+    def GetLabelPosition(self, idx: int):
+        """
+
+        Args:
+            idx:    One of
+                            NAME_IDX
+                            SOURCE_CARDINALITY_IDX
+                            DESTINATION_CARDINALITY_IDX
+
+        Returns:    An x,y tuple
+        """
+        pass
