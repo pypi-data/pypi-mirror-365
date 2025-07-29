ca7c62c6ac2be5539884d43b81a997ac1d40b3d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:02 +0000

    Add package nerdd-module/0.3.39/nerdd_module-0.3.39.tar.gz

---
+++ packages/nerdd-module/nerdd_module-0.3.39.tar.gz/nerdd_module-0.3.39/nerdd_module/preprocessing/remove_stereochemistry.py
@@ -0,0 +1,24 @@
+from typing import List, Tuple
+
+from rdkit.Chem import Mol
+from rdkit.Chem import RemoveStereochemistry as remove_stereochemistry
+
+from ..problem import Problem
+from .preprocessing_step import PreprocessingStep
+
+
+class RemoveStereochemistry(PreprocessingStep):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def _preprocess(self, mol: Mol) -> Tuple[Mol, List[Problem]]:
+        problems = []
+
+        try:
+            remove_stereochemistry(mol)
+        except Exception:
+            problems.append(
+                Problem("remove_stereochemistry_failed", "Cannot remove stereochemistry")
+            )
+
+        return mol, problems
