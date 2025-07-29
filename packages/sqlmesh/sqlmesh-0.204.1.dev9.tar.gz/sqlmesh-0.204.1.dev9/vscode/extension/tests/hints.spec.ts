2199d5b56764dd2cd285eb2f0bbd081a41c420aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:04:45 +0000

    Add package sqlmesh/0.204.1.dev9/sqlmesh-0.204.1.dev9.tar.gz

---
+++ packages/sqlmesh/sqlmesh-0.204.1.dev9.tar.gz/sqlmesh-0.204.1.dev9/vscode/extension/tests/hints.spec.ts
@@ -0,0 +1,44 @@
+import { test, expect } from './fixtures'
+import path from 'path'
+import fs from 'fs-extra'
+import os from 'os'
+import {
+  openServerPage,
+  SUSHI_SOURCE_PATH,
+  waitForLoadedSQLMesh,
+} from './utils'
+import { createPythonInterpreterSettingsSpecifier } from './utils_code_server'
+
+test('Model type hinting', async ({ page, sharedCodeServer }) => {
+  const tempDir = await fs.mkdtemp(path.join(os.tmpdir(), 'vscode-test-sushi-'))
+  await fs.copy(SUSHI_SOURCE_PATH, tempDir)
+  await createPythonInterpreterSettingsSpecifier(tempDir)
+  await openServerPage(page, tempDir, sharedCodeServer)
+
+  // Wait for the models folder to be visible
+  await page.waitForSelector('text=models')
+
+  // Click on the models folder
+  await page
+    .getByRole('treeitem', { name: 'models', exact: true })
+    .locator('a')
+    .click()
+
+  // Open the customers_revenue_by_day model
+  await page
+    .getByRole('treeitem', {
+      name: 'customer_revenue_by_day.sql',
+      exact: true,
+    })
+    .locator('a')
+    .click()
+
+  await page.waitForSelector('text=grain')
+  await waitForLoadedSQLMesh(page)
+
+  // Wait a moment for hints to appear
+  await page.waitForTimeout(500)
+
+  // Check if the hint is visible
+  expect(await page.locator('text="country code"::INT').count()).toBe(1)
+})
