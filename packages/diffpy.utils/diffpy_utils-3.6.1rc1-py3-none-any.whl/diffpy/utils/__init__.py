5058cf456ed6373681ed204c6cc5230b767316f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:53 +0000

    Add package diffpy.utils/3.6.1rc1/diffpy_utils-3.6.1rc1-py3-none-any.whl

---
+++ packages/diffpy.utils/diffpy_utils-3.6.1rc1-py3-none-any.whl/diffpy/utils/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# (c) 2025 The Trustees of Columbia University in the City of New York.
+# All rights reserved.
+#
+# File coded by: Simon Billinge, Billinge Group members.
+#
+# See GitHub contributions for a more detailed list of contributors.
+# https://github.com/diffpy/diffpy.utils/graphs/contributors
+#
+# See LICENSE.rst for license information.
+#
+##############################################################################
+"""Shared utilities for diffpy packages."""
+
+# package version
+from diffpy.utils.version import __version__
+
+# silence the pyflakes syntax checker
+assert __version__ or True
+
+# End of file
