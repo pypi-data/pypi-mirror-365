d33ac8ae730a24b9677b496837c0f2964f285ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:13 +0000

    Add package pybaseutils/2.3.2/pybaseutils-2.3.2.tar.gz

---
+++ packages/pybaseutils/pybaseutils-2.3.2.tar.gz/pybaseutils-2.3.2/pybaseutils/setup_config.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+提供工具函数的模块
+"""
+
+import logging
+import yaml
+import easydict
+import json
+from pybaseutils.config_utils import *
+
+if __name__ == '__main__':
+    pass
