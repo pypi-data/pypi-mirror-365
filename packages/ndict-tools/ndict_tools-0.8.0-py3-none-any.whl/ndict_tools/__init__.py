3251bab3405bad5a976bd0cc9b0ff699ca1e8671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:04:33 +0000

    Add package ndict-tools/0.8.0/ndict_tools-0.8.0-py3-none-any.whl

---
+++ packages/ndict-tools/ndict_tools-0.8.0-py3-none-any.whl/ndict_tools/__init__.py
@@ -0,0 +1,26 @@
+from .core import NestedDictionary, SmoothNestedDictionary, StrictNestedDictionary
+from .exception import (
+    NestedDictionaryException,
+    StackedAttributeError,
+    StackedDictionaryError,
+    StackedIndexError,
+    StackedKeyError,
+    StackedTypeError,
+    StackedValueError,
+)
+
+__version__ = "0.8.0"
+__author__ = "biface"
+
+__all__ = [
+    "NestedDictionary",
+    "SmoothNestedDictionary",
+    "StrictNestedDictionary",
+    "NestedDictionaryException",
+    "StackedDictionaryError",
+    "StackedKeyError",
+    "StackedAttributeError",
+    "StackedTypeError",
+    "StackedValueError",
+    "StackedIndexError",
+]
