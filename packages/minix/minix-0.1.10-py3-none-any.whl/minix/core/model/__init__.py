9d9709f23faa749f7ae851ec3041a2e69d27b5c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:47 +0000

    Add package minix/0.1.10/minix-0.1.10-py3-none-any.whl

---
+++ packages/minix/minix-0.1.10-py3-none-any.whl/minix/core/model/__init__.py
@@ -0,0 +1,4 @@
+from .model import Model
+from .mlflow_model import MlflowModel
+from .model_registry import ModelRegistry
+from .embedding_model import EmbeddingModel
---
+++ packages/minix/minix-0.1.10-py3-none-any.whl/minix/core/service/__init__.py
@@ -0,0 +1,4 @@
+from .service import Service
+from minix.core.service.sql.sql_service import SqlService
+from minix.core.service.redis.redis_service import RedisService
+from minix.core.service.qdrant.qdrant_service import QdrantService
