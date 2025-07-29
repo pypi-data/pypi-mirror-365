79dfeae512ab87cf60dd8450233ef0f71b8275d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:43 +0000

    Add package flet-audio/0.2.0.dev58/flet_audio-0.2.0.dev58-py3-none-any.whl

---
+++ packages/flet-audio/flet_audio-0.2.0.dev58-py3-none-any.whl/flet_audio/__init__.py
@@ -0,0 +1,17 @@
+from .audio import Audio
+from .types import (
+    AudioDurationChangeEvent,
+    AudioPositionChangeEvent,
+    AudioState,
+    AudioStateChangeEvent,
+    ReleaseMode,
+)
+
+__all__ = [
+    "Audio",
+    "AudioDurationChangeEvent",
+    "AudioPositionChangeEvent",
+    "AudioState",
+    "AudioStateChangeEvent",
+    "ReleaseMode",
+]
