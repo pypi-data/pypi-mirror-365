89462eb866b997e250339ca381ff4883c777e016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:16 +0000

    Add package just-config-mixin/0.1.0/just_config_mixin-0.1.0.tar.gz

---
+++ packages/just-config-mixin/just_config_mixin-0.1.0.tar.gz/just_config_mixin-0.1.0/src/configmixin/__init__.py
@@ -0,0 +1,13 @@
+"""
+.. include:: ../../README.md
+"""
+
+from ._core import ConfigMixin, FrozenDict, register_to_config
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "ConfigMixin",
+    "FrozenDict",
+    "register_to_config",
+]
