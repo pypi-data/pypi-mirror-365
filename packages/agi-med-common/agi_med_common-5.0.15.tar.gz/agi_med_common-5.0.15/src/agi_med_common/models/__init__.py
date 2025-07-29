c8193f4ba4aeb83ff8b7336b4204d96fa8fac6e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:24 +0000

    Add package agi-med-common/5.0.15/agi_med_common-5.0.15.tar.gz

---
+++ packages/agi-med-common/agi_med_common-5.0.15.tar.gz/agi_med_common-5.0.15/src/agi_med_common/models/__init__.py
@@ -0,0 +1,6 @@
+from ._base import _Base
+
+from .enums import MTRSLabelEnum, DiagnosticsXMLTagEnum, MTRSXMLTagEnum, DoctorChoiceXMLTagEnum
+from .chat_item import ChatItem, OuterContextItem, InnerContextItem, ReplicaItem
+from .base_config_models import GigaChatConfig
+from .tracks import TrackInfo, DomainInfo
