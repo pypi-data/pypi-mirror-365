db9179eeaa694c0e7f79b6d4290b944d7a1659e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:54 +0000

    Add package pod5-random-access/0.6.0/pod5_random_access-0.6.0-cp313-cp313-win_amd64.whl

---
+++ packages/pod5-random-access/pod5_random_access-0.6.0-cp313-cp313-win_amd64.whl/pod5_random_access/__init__.py
@@ -0,0 +1,5 @@
+from .reader import Pod5RandomAccessReader
+from .settings import IndexSettings
+from .build import build_pod5_index
+
+__all__ = ["Pod5RandomAccessReader", "IndexSettings", "build_pod5_index"]
