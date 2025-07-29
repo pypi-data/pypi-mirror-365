28b9c80a47e1a390c11e05d6b0351eacad541a40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:46 +0000

    Add package flet-permission-handler/0.2.0.dev55/flet_permission_handler-0.2.0.dev55-py3-none-any.whl

---
+++ packages/flet-permission-handler/flet_permission_handler-0.2.0.dev55-py3-none-any.whl/flet_permission_handler/__init__.py
@@ -0,0 +1,8 @@
+from .permission_handler import PermissionHandler
+from .types import Permission, PermissionStatus
+
+__all__ = [
+    "Permission",
+    "PermissionHandler",
+    "PermissionStatus",
+]
