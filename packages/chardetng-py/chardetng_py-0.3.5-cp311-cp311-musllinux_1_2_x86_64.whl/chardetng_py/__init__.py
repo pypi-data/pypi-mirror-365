1ae4c9dd5d12095b781dca137b79c465eca9a679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:06:58 +0000

    Add package chardetng-py/0.3.5/chardetng_py-0.3.5-cp311-cp311-musllinux_1_2_x86_64.whl

---
+++ packages/chardetng-py/chardetng_py-0.3.5-cp311-cp311-musllinux_1_2_x86_64.whl/chardetng_py/__init__.py
@@ -0,0 +1,20 @@
+"""Functions for dealing with byte strings of unknown encoding.".
+
+:code:`detect` is imported from :code:`chardetng_py.api` and so therefore may
+be imported using :code:`from chardetng_py import detect`.
+"""
+
+from __future__ import annotations
+
+from typing import Final
+
+from chardetng_py.detector import EncodingDetector
+from chardetng_py.shortcuts import detect
+
+# Documentation for the detect function is on the rust function
+# in lib.rs
+
+__all__: Final[list[str]] = [
+    "EncodingDetector",
+    "detect",
+]
