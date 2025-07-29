ff524ca1fc0cc99b266afcba673b6296b58787c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:12 +0000

    Add package isaacus/0.8.0/isaacus-0.8.0.tar.gz

---
+++ packages/isaacus/isaacus-0.8.0.tar.gz/isaacus-0.8.0/api.md
@@ -0,0 +1,39 @@
+# Classifications
+
+## Universal
+
+Types:
+
+```python
+from isaacus.types.classifications import UniversalClassification
+```
+
+Methods:
+
+- <code title="post /classifications/universal">client.classifications.universal.<a href="./src/isaacus/resources/classifications/universal.py">create</a>(\*\*<a href="src/isaacus/types/classifications/universal_create_params.py">params</a>) -> <a href="./src/isaacus/types/classifications/universal_classification.py">UniversalClassification</a></code>
+
+# Rerankings
+
+Types:
+
+```python
+from isaacus.types import Reranking
+```
+
+Methods:
+
+- <code title="post /rerankings">client.rerankings.<a href="./src/isaacus/resources/rerankings.py">create</a>(\*\*<a href="src/isaacus/types/reranking_create_params.py">params</a>) -> <a href="./src/isaacus/types/reranking.py">Reranking</a></code>
+
+# Extractions
+
+## Qa
+
+Types:
+
+```python
+from isaacus.types.extractions import AnswerExtraction
+```
+
+Methods:
+
+- <code title="post /extractions/qa">client.extractions.qa.<a href="./src/isaacus/resources/extractions/qa.py">create</a>(\*\*<a href="src/isaacus/types/extractions/qa_create_params.py">params</a>) -> <a href="./src/isaacus/types/extractions/answer_extraction.py">AnswerExtraction</a></code>
---
+++ packages/isaacus/isaacus-0.8.0.tar.gz/isaacus-0.8.0/src/isaacus/resources/__init__.py
@@ -0,0 +1,47 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .rerankings import (
+    RerankingsResource,
+    AsyncRerankingsResource,
+    RerankingsResourceWithRawResponse,
+    AsyncRerankingsResourceWithRawResponse,
+    RerankingsResourceWithStreamingResponse,
+    AsyncRerankingsResourceWithStreamingResponse,
+)
+from .extractions import (
+    ExtractionsResource,
+    AsyncExtractionsResource,
+    ExtractionsResourceWithRawResponse,
+    AsyncExtractionsResourceWithRawResponse,
+    ExtractionsResourceWithStreamingResponse,
+    AsyncExtractionsResourceWithStreamingResponse,
+)
+from .classifications import (
+    ClassificationsResource,
+    AsyncClassificationsResource,
+    ClassificationsResourceWithRawResponse,
+    AsyncClassificationsResourceWithRawResponse,
+    ClassificationsResourceWithStreamingResponse,
+    AsyncClassificationsResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "ClassificationsResource",
+    "AsyncClassificationsResource",
+    "ClassificationsResourceWithRawResponse",
+    "AsyncClassificationsResourceWithRawResponse",
+    "ClassificationsResourceWithStreamingResponse",
+    "AsyncClassificationsResourceWithStreamingResponse",
+    "RerankingsResource",
+    "AsyncRerankingsResource",
+    "RerankingsResourceWithRawResponse",
+    "AsyncRerankingsResourceWithRawResponse",
+    "RerankingsResourceWithStreamingResponse",
+    "AsyncRerankingsResourceWithStreamingResponse",
+    "ExtractionsResource",
+    "AsyncExtractionsResource",
+    "ExtractionsResourceWithRawResponse",
+    "AsyncExtractionsResourceWithRawResponse",
+    "ExtractionsResourceWithStreamingResponse",
+    "AsyncExtractionsResourceWithStreamingResponse",
+]
