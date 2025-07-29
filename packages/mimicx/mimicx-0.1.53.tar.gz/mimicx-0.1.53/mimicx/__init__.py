ba7e0b11472181f7285695cde9a5df8c6c514bca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:06:47 +0000

    Add package mimicx/0.1.53/mimicx-0.1.53.tar.gz

---
+++ packages/mimicx/mimicx-0.1.53.tar.gz/mimicx-0.1.53/mimicx/__init__.py
@@ -0,0 +1,16 @@
+"""
+MimicX AI  
+Human-like AI for everyone.
+"""
+
+__version__ = "0.1.52"
+__author__ = "MimicX"
+__credits__ = "AI Researcher at MimicX"
+
+
+from .main_with_p import Model
+from .main_with_p import Model as Utils
+from .main_with_p import Model as Pipeline
+
+
+__all__ = ["Model","Utils","Pipeline"]
