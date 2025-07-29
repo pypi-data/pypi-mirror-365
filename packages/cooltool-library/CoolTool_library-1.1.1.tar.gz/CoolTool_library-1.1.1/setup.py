2484f367639fed2adbbaaca79c1c6dcc4f6a2296
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:29 +0000

    Add package cooltool-library/1.1.1/CoolTool_library-1.1.1.tar.gz

---
+++ packages/cooltool-library/CoolTool_library-1.1.1.tar.gz/CoolTool_library-1.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='CoolTool_library',
+    version='1.1.1',
+    packages=['cooltool_library'],
+    author='Quadzeco',
+    author_email='example@example.com',
+    description='Simple library with many function to help you with consol project',
+    long_description=long_description,
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
