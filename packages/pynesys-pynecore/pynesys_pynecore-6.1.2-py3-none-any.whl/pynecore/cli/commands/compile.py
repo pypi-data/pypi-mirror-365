cc90a2f1c1a28dc175f736d74a372132c14ca927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:33 +0000

    Add package pynesys-pynecore/6.1.2/pynesys_pynecore-6.1.2-py3-none-any.whl

---
+++ packages/pynesys-pynecore/pynesys_pynecore-6.1.2-py3-none-any.whl/pynecore/cli/commands/compile.py
@@ -0,0 +1,14 @@
+from ..app import app, app_state
+
+__all__ = []
+
+
+# noinspection PyShadowingBuiltins
+@app.command()
+def compile(
+
+):
+    """
+    Compile Pine Script to Python through pynesys.com
+    """
+    # TODO: Implement the compile command when API is ready
