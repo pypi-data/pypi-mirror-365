5204bb84b5a6ad58faeea3a9c32c544a20683f18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:04:29 +0000

    Add package dlts/0.0.1a1/dlts-0.0.1a1.tar.gz

---
+++ packages/dlts/dlts-0.0.1a1.tar.gz/dlts-0.0.1a1/dlts/__init__.py
@@ -0,0 +1,2 @@
+from version import __version__ as __version__
+from .models import *
---
+++ packages/dlts/dlts-0.0.1a1.tar.gz/dlts-0.0.1a1/dlts/models/__init__.py
@@ -0,0 +1,24 @@
+from typing import Dict
+
+import torch.nn as nn
+
+import model_logger_dp as logger
+
+from ._registry import Registry
+
+
+def create_model(
+        model_name: str,
+        registry: Dict[str, Registry],
+) -> nn.Module:
+
+    create_fn = registry['model'].get(model_name)
+    if 'config' in registry.keys():
+        cfg: Dict = registry['config'].get(model_name)
+    else:
+        cfg: Dict = {}
+    model = create_fn(**cfg)
+    logger.info(f'Creating model: {model_name} with config: {cfg}')
+
+    return model
+
