cdf56daf47e9efb1dfe0508adaeb795798c78c2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:25 +0000

    Add package xianb/2.0.7/xianb-2.0.7.tar.gz

---
+++ packages/xianb/xianb-2.0.7.tar.gz/xianb-2.0.7/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xianb',
+    version='2.0.7',
+    author='jxjiang',
+    author_email='723137901@qq.com',
+    description='很大的修改，改到颠了，不想干了',
+    python_requires='>=3.8',
+    install_requires=[],
+    packages=find_packages()
+)
