7b97c8a240aecdd7397174663f9740d4f424ae15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:43 +0000

    Add package pyverto/0.1.9/pyverto-0.1.9-py3-none-any.whl

---
+++ packages/pyverto/pyverto-0.1.9-py3-none-any.whl/pyverto/__about__.py
@@ -0,0 +1,5 @@
+# SPDX-FileCopyrightText: 2025-present phdenzel <phdenzel@gmail.com>
+# SPDX-FileNotice: Part of pyverto. Distributed as-is with no warranty.
+# SPDX-License-Identifier: MIT
+"""Dynamic versioning."""
+__version__ = "0.1.9"
