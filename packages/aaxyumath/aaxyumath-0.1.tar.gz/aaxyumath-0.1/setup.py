15e905ab628af52f190528d95ea2f6eae6ba56ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:20 +0000

    Add package aaxyumath/0.1/aaxyumath-0.1.tar.gz

---
+++ packages/aaxyumath/aaxyumath-0.1.tar.gz/aaxyumath-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aaxyumath',
+    version='0.1',
+    description='A cool math library by AaxYu',
+    author='AaxYu',
+    author_email='legendarykartik777@example.com',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+)
