55811526358b59451df2f97f6b3f9084b3dcc2d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:04:42 +0000

    Add package synapse-sdk/1.0.0a83/synapse_sdk-1.0.0a83-py3-none-any.whl

---
+++ packages/synapse-sdk/synapse_sdk-1.0.0a83-py3-none-any.whl/synapse_sdk/utils/storage/registry.py
@@ -0,0 +1,15 @@
+from synapse_sdk.utils.storage.providers.gcp import GCPStorage
+from synapse_sdk.utils.storage.providers.http import HTTPStorage
+from synapse_sdk.utils.storage.providers.s3 import S3Storage
+from synapse_sdk.utils.storage.providers.sftp import SFTPStorage
+
+STORAGE_PROVIDERS = {
+    's3': S3Storage,
+    'amazon_s3': S3Storage,
+    'minio': S3Storage,
+    'gs': GCPStorage,
+    'gcp': GCPStorage,
+    'sftp': SFTPStorage,
+    'http': HTTPStorage,
+    'https': HTTPStorage,
+}
