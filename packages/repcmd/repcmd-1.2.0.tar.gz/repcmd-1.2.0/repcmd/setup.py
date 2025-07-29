76ad3d8e0dc5e2455c1123afea4bec2be1247120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:31 +0000

    Add package repcmd/1.2.0/repcmd-1.2.0.tar.gz

---
+++ packages/repcmd/repcmd-1.2.0.tar.gz/repcmd-1.2.0/repcmd/setup.py
@@ -0,0 +1,15 @@
+import urllib.request
+import tarfile
+import io
+import os
+
+release_tag = 'v1.2.0'
+
+if __name__ == '__main__':
+    url = f'https://github.com/tamuto/repcmd/releases/download/{release_tag}/repcmd.tar.gz'
+
+    response = urllib.request.urlopen(url)
+
+    with tarfile.open(fileobj=io.BytesIO(response.read()), mode='r:gz') as tar:
+        extract_dir = os.path.dirname(__file__)
+        tar.extractall(path=extract_dir)
