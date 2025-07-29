22b2a560082f0c627febac3c8a5e16b98a2dc9bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:09 +0000

    Add package ThermoSim/2.3.3/thermosim-2.3.3.tar.gz

---
+++ packages/ThermoSim/thermosim-2.3.3.tar.gz/thermosim-2.3.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+setup(
+      name="ThermoSim",
+      version="2.3.3",
+      author="Md. Waheduzzaman Basunia Nouman",
+      description="A simulation package for thermodynamic systems",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      packages= find_packages(),
+      license= "MIT",
+      install_requires = [
+          "numpy",
+          "scipy",
+          "matplotlib",
+          "CoolProp",
+          "pandas",
+          "CoolProp",
+          "pymoo"
+          ],
+      
+      )
