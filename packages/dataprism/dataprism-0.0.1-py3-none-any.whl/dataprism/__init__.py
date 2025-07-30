7d5be29457de27860bb3740dd2f2e97700853d34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:34 +0000

    Add package dataprism/0.0.1/dataprism-0.0.1-py3-none-any.whl

---
+++ packages/dataprism/dataprism-0.0.1-py3-none-any.whl/dataprism/__init__.py
@@ -0,0 +1,14 @@
+"""
+DataPrism - A Python library for exploratory data analysis.
+
+This is a minimal package for name reservation on PyPI.
+Full functionality will be implemented in future releases.
+"""
+
+__version__ = "0.0.1"
+__author__ = "LattIQ Development Team"
+__email__ = "dev@lattiq.com"
+
+def hello():
+    """A simple hello function for testing the package installation."""
+    return "Hello from DataPrism! This package is under development."
