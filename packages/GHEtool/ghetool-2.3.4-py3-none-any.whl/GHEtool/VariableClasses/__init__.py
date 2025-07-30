49da1393666b46795f83707bb6eb8f4e0c090f59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:04 +0000

    Add package GHEtool/2.3.4/ghetool-2.3.4-py3-none-any.whl

---
+++ packages/GHEtool/ghetool-2.3.4-py3-none-any.whl/GHEtool/VariableClasses/__init__.py
@@ -0,0 +1,11 @@
+from .FluidData import *
+from .FlowData import *
+from .GroundData import *
+from .LoadData import *
+from .PipeData import *
+from .Efficiency import *
+from .CustomGFunction import CustomGFunction, load_custom_gfunction, _time_values
+from .GFunction import GFunction, FIFO
+from .CalculationSetup import CalculationSetup
+from .Borehole import Borehole
+from .Result import ResultsMonthly, ResultsHourly, _Results
