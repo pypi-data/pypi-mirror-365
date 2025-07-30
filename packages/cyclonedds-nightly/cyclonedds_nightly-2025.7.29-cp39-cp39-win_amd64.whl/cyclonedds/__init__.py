d2d3660555a82faa636ad76a0e2b93826c8d826e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:15 +0000

    Add package cyclonedds-nightly/2025.7.29/cyclonedds_nightly-2025.7.29-cp39-cp39-win_amd64.whl

---
+++ packages/cyclonedds-nightly/cyclonedds_nightly-2025.7.29-cp39-cp39-win_amd64.whl/cyclonedds/__init__.py
@@ -0,0 +1,58 @@
+"""
+ * Copyright(c) 2021 to 2022 ZettaScale Technology and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Eclipse Distribution License
+ * v. 1.0 which is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
+"""
+
+
+# start delvewheel patch
+def _delvewheel_patch_1_10_1():
+    import ctypes
+    import os
+    import platform
+    import sys
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'cyclonedds_nightly.libs'))
+    is_conda_cpython = platform.python_implementation() == 'CPython' and (hasattr(ctypes.pythonapi, 'Anaconda_GetVersion') or 'packaged by conda-forge' in sys.version)
+    if sys.version_info[:2] >= (3, 8) and not is_conda_cpython or sys.version_info[:2] >= (3, 10):
+        if os.path.isdir(libs_dir):
+            os.add_dll_directory(libs_dir)
+    else:
+        load_order_filepath = os.path.join(libs_dir, '.load-order-cyclonedds_nightly-2025.7.29')
+        if os.path.isfile(load_order_filepath):
+            import ctypes.wintypes
+            with open(os.path.join(libs_dir, '.load-order-cyclonedds_nightly-2025.7.29')) as file:
+                load_order = file.read().split()
+            kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
+            kernel32.LoadLibraryExW.restype = ctypes.wintypes.HMODULE
+            kernel32.LoadLibraryExW.argtypes = ctypes.wintypes.LPCWSTR, ctypes.wintypes.HANDLE, ctypes.wintypes.DWORD
+            for lib in load_order:
+                lib_path = os.path.join(os.path.join(libs_dir, lib))
+                if os.path.isfile(lib_path) and not kernel32.LoadLibraryExW(lib_path, None, 8):
+                    raise OSError('Error loading {}; {}'.format(lib, ctypes.FormatError(ctypes.get_last_error())))
+
+
+_delvewheel_patch_1_10_1()
+del _delvewheel_patch_1_10_1
+# end delvewheel patch
+
+from . import internal, util, qos, core, domain, topic, pub, sub, builtin, dynamic, idl
+
+__all__ = [
+    "internal",
+    "util",
+    "qos",
+    "core",
+    "domain",
+    "topic",
+    "pub",
+    "sub",
+    "builtin",
+    "dynamic",
+    "idl",
+]
