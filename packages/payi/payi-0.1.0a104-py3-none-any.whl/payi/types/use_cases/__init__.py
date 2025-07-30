b5b85a5019e799fbf26401a062052a0d55cb7975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:05 +0000

    Add package payi/0.1.0a104/payi-0.1.0a104-py3-none-any.whl

---
+++ packages/payi/payi-0.1.0a104-py3-none-any.whl/payi/types/use_cases/__init__.py
@@ -0,0 +1,13 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .kpi_list_params import KpiListParams as KpiListParams
+from .kpi_list_response import KpiListResponse as KpiListResponse
+from .kpi_update_params import KpiUpdateParams as KpiUpdateParams
+from .use_case_definition import UseCaseDefinition as UseCaseDefinition
+from .definition_list_params import DefinitionListParams as DefinitionListParams
+from .property_create_params import PropertyCreateParams as PropertyCreateParams
+from .definition_create_params import DefinitionCreateParams as DefinitionCreateParams
+from .definition_update_params import DefinitionUpdateParams as DefinitionUpdateParams
+from .property_create_response import PropertyCreateResponse as PropertyCreateResponse
---
+++ packages/payi/payi-0.1.0a104-py3-none-any.whl/payi/types/use_cases/kpi_update_params.py
@@ -0,0 +1,13 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing_extensions import Required, TypedDict
+
+__all__ = ["KpiUpdateParams"]
+
+
+class KpiUpdateParams(TypedDict, total=False):
+    use_case_id: Required[str]
+
+    score: float
