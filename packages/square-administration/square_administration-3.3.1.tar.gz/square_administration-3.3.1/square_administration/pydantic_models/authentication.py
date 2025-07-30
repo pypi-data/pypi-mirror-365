4910bc6e54435406a012c6a4da7b8fc3e89a41ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:11:58 +0000

    Add package square-administration/3.3.1/square_administration-3.3.1.tar.gz

---
+++ packages/square-administration/square_administration-3.3.1.tar.gz/square_administration-3.3.1/square_administration/pydantic_models/authentication.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel
+
+
+class RegisterUsernameV0(BaseModel):
+    username: str
+    password: str
+    admin_password: str
+
+
+class LoginUsernameV0(BaseModel):
+    username: str
+    password: str
+
+
+class RemoveAppForSelfV0(BaseModel):
+    password: str
+
+
+class RegisterLoginGoogleV0(BaseModel):
+    google_id: str
+
+
+class ResetPasswordAndLoginUsingBackupCodeV0(BaseModel):
+    backup_code: str
+    username: str
+    new_password: str
+    logout_other_sessions: bool = False
+
+
+class ResetPasswordAndLoginUsingResetEmailCodeV0(BaseModel):
+    reset_email_code: str
+    username: str
+    new_password: str
+    logout_other_sessions: bool = False
