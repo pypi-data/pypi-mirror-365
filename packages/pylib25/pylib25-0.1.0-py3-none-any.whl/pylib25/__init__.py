43ba081baa0e0016594650ea116b00d4e5f9f7a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:06:55 +0000

    Add package pylib25/0.1.0/pylib25-0.1.0-py3-none-any.whl

---
+++ packages/pylib25/pylib25-0.1.0-py3-none-any.whl/pylib25/__init__.py
@@ -0,0 +1,15 @@
+"""
+`pylib25` is a simple library to illustrate how to deploy a Python library.
+"""
+
+def sqr(x: float) -> float:
+    """
+    Get square of the given `x`.
+
+    ```
+    from mylib25 import sqr
+
+    assert sqr(5.0) == 25.0
+    ```
+    """
+    return x ** 2
