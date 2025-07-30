f39a07be0ea5c60231ac3f49135cd23ecfa5006b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:05:49 +0000

    Add package dfss/1.10.4/dfss-1.10.4.tar.gz

---
+++ packages/dfss/dfss-1.10.4.tar.gz/dfss-1.10.4/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+import os
+import shutil
+
+current_folder = os.path.dirname(os.path.abspath(__file__))
+filehandle = open(os.path.join(current_folder,"../git_version"),"r")
+version_info = filehandle.readline().rstrip("\n").rstrip("\r")
+
+shutil.copy(os.path.join(current_folder,"../git_version"), os.path.join(current_folder,"dfss/output"))
+shutil.copytree(os.path.join(current_folder,"../output"), os.path.join(current_folder,"dfss/output"), dirs_exist_ok=True)
+
+setup(
+    name='dfss',
+    version=version_info,
+    author='zetao.zhang',
+    author_email='zetao.zhang@sophgo.com',
+    description='download_from_sophon_sftp',
+    packages=find_packages(),
+	package_data={"dfss":["output/*"]},
+    include_package_data=True,
+)
