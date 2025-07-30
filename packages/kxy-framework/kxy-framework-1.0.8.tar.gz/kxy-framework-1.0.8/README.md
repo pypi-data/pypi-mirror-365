432ba666c1135ede45372e1383c9ff329a2fe629
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:07 +0000

    Add package kxy-framework/1.0.8/kxy-framework-1.0.8.tar.gz

---
+++ packages/kxy-framework/kxy-framework-1.0.8.tar.gz/kxy-framework-1.0.8/README.md
@@ -0,0 +1,13 @@
+## 使用方法：
+pip install kxy-framework
+```python
+from kxy.framework import SUtil
+
+# 比较两个列表，返回删除的列表和新增的列表
+
+
+
+
+
+
+
---
+++ packages/kxy-framework/kxy-framework-1.0.8.tar.gz/kxy-framework-1.0.8/kxy/__init__.py
@@ -0,0 +1,6 @@
+from kxy.framework.base_config import BaseConfig as BaseConfig
+from kxy.framework.base_dal import BaseDal as BaseDal
+from kxy.framework.base_entity import BaseEntity as BaseEntity
+from kxy.framework.base_service import BaseService as BaseService
+from kxy.framework.friendly_exception import FriendlyException as FriendlyException
+from kxy.framework.mapper import Mapper as Mapper
