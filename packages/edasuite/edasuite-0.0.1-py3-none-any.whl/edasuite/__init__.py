bf8982890a302376d47c912aba174c5573e45364
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:14 +0000

    Add package edasuite/0.0.1/edasuite-0.0.1-py3-none-any.whl

---
+++ packages/edasuite/edasuite-0.0.1-py3-none-any.whl/edasuite/__init__.py
@@ -0,0 +1,14 @@
+"""
+EDASuite - A Python library for exploratory data analysis.
+
+This is a minimal package for name reservation on PyPI.
+Full functionality will be implemented in future releases.
+"""
+
+__version__ = "0.0.1"
+__author__ = "LattIQ Development Team"
+__email__ = "dev@lattiq.com"
+
+def hello():
+    """A simple hello function for testing the package installation."""
+    return "Hello from EDASuite! This package is under development."
