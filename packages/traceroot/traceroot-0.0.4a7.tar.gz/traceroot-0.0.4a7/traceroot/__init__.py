49e01f98dca3fb7a5cac1226d69efc8ee54367e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:00 +0000

    Add package traceroot/0.0.4a7/traceroot-0.0.4a7.tar.gz

---
+++ packages/traceroot/traceroot-0.0.4a7.tar.gz/traceroot-0.0.4a7/traceroot/__init__.py
@@ -0,0 +1,13 @@
+from traceroot.logger import get_logger
+from traceroot.tracer import _initialize_tracing as init
+from traceroot.tracer import trace
+
+__version__ = '0.0.4a6'
+
+init()
+
+__all__ = [
+    'init',
+    'trace',
+    'get_logger',
+]
