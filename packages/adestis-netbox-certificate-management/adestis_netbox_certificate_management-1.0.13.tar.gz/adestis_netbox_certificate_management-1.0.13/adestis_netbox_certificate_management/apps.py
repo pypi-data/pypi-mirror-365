01e003752a334b8dc80f57a92985626e172debc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 22:05:37 +0000

    Add package adestis-netbox-certificate-management/1.0.13/adestis_netbox_certificate_management-1.0.13.tar.gz

---
+++ packages/adestis-netbox-certificate-management/adestis_netbox_certificate_management-1.0.13.tar.gz/adestis_netbox_certificate_management-1.0.13/adestis_netbox_certificate_management/apps.py
@@ -0,0 +1,12 @@
+from django.apps import AppConfig
+
+class AdestisCertificateManagementAppConfig(AppConfig):
+    name = 'adestis_netbox_certificate_management'
+
+    def ready(self):
+        from adestis_netbox_certificate_management.jobs import CertificateMetadataExtractorJob
+
+        CertificateMetadataExtractorJob.schedule(
+            name="certificate_metadata_extractor",
+            interval=15  
+        )
