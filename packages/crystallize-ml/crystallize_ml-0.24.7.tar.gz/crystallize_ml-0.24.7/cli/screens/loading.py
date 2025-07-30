82543628367cf48fe1575e0ca3e644af65fb271e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:07:59 +0000

    Add package crystallize-ml/0.24.7/crystallize_ml-0.24.7.tar.gz

---
+++ packages/crystallize-ml/crystallize_ml-0.24.7.tar.gz/crystallize_ml-0.24.7/cli/screens/loading.py
@@ -0,0 +1,25 @@
+from textual.screen import ModalScreen
+from textual.app import ComposeResult
+from textual.containers import Container
+from textual.widgets import Static
+
+
+class LoadingScreen(ModalScreen):
+    CSS = """
+    #loading-container {
+        layout: vertical;
+        width: 40%;
+        height: 20%;
+        border: round $primary;
+        background: $panel;
+        text-align: center;
+        content-align: center middle;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        with Container(id="loading-container"):
+            yield Static("Loading...")
+
+    def on_mount(self) -> None:
+        self.set_timer(1, self.dismiss)
