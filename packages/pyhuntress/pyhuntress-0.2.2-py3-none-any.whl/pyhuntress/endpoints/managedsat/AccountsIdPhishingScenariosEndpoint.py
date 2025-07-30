4e5b88b137b74f6f0bfad56353188de16de0656f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:36 +0000

    Add package pyhuntress/0.2.2/pyhuntress-0.2.2-py3-none-any.whl

---
+++ packages/pyhuntress/pyhuntress-0.2.2-py3-none-any.whl/pyhuntress/endpoints/managedsat/AccountsIdPhishingScenariosEndpoint.py
@@ -0,0 +1,38 @@
+from pyhuntress.endpoints.base.huntress_endpoint import HuntressEndpoint
+from pyhuntress.interfaces import (
+    IGettable,
+)
+from pyhuntress.models.managedsat import SATPhishingScenarios
+from pyhuntress.types import (
+    JSON,
+    HuntressSATRequestParams,
+)
+
+
+class AccountsIdPhishingScenariosEndpoint(
+    HuntressEndpoint,
+    IGettable[SATPhishingScenarios, HuntressSATRequestParams],
+):
+    def __init__(self, client, parent_endpoint=None) -> None:
+        HuntressEndpoint.__init__(self, client, "phishing_scenarios", parent_endpoint=parent_endpoint)
+        IGettable.__init__(self, SATPhishingScenarios)
+
+    def get(
+        self,
+        data: JSON | None = None,
+        params: HuntressSATRequestParams | None = None,
+    ) -> SATPhishingScenarios:
+        
+        """
+        Performs a GET request against the /accounts/{id}/phishing-scenarios endpoint.
+
+        Parameters:
+            data (dict[str, Any]): The data to send in the request body.
+            params (dict[str, int | str]): The parameters to send in the request query string.
+        Returns:
+            SATPhishingScenarios: The parsed response data.
+        """
+        return self._parse_many(
+            SATPhishingScenarios,
+            super()._make_request("GET", data=data, params=params).json().get('data', {}),
+        )
