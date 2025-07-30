22bc307a504a6dbd49b5a8a838623607c5ebb6f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:21 +0000

    Add package logsy-core/0.1.1/logsy_core-0.1.1.tar.gz

---
+++ packages/logsy-core/logsy_core-0.1.1.tar.gz/logsy_core-0.1.1/test/test.py
@@ -0,0 +1,11 @@
+import sys
+import os
+sys.path.append('/home/kimyh/library/logsy')
+import logsy as lo
+
+def main():
+    log = lo.get_logger()
+    pass
+
+if __name__ == '__main__':
+    main()
