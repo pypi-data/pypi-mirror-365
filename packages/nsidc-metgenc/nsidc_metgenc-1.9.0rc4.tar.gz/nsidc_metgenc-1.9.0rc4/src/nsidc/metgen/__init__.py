5401f126e7be75d28eda65d878c1ce827472ceb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:46 +0000

    Add package nsidc-metgenc/1.9.0rc4/nsidc_metgenc-1.9.0rc4.tar.gz

---
+++ packages/nsidc-metgenc/nsidc_metgenc-1.9.0rc4.tar.gz/nsidc_metgenc-1.9.0rc4/src/nsidc/metgen/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "v1.9.0rc4"
+
+
+__all__ = [
+    "__version__",
+    "aws",
+    "cli",
+    "config",
+    "constants",
+    "metgen",
+]
+
+from . import aws, cli, config, constants, metgen
