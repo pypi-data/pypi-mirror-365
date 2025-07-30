32f1d9008e0cc21b390c9aa2cfdefa559bb38066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 22:05:26 +0000

    Add package pyfastmath/1.0.0/pyfastmath-1.0.0.tar.gz

---
+++ packages/pyfastmath/pyfastmath-1.0.0.tar.gz/pyfastmath-1.0.0/pyfastmath/pyfastmath.h
@@ -0,0 +1,9 @@
+#ifndef FASTMATH_H
+#define FASTMATH_H
+#include<stdbool.h>
+
+long long int gcd(long long int a, long long int b);
+bool is_prime(unsigned long long int a);
+long long mod_exp(long long base, long long exp, long long mod);
+
+#endif
