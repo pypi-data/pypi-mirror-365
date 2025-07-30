6821eb8eba02b3dd0e0d1fd980fc66fbe83a09c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:23 +0000

    Add package africanjokes/0.0.4/africanjokes-0.0.4.tar.gz

---
+++ packages/africanjokes/africanjokes-0.0.4.tar.gz/africanjokes-0.0.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name='africanjokes',
+    version='0.0.4', 
+    description='A library for African jokes',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Morris D. Toclo',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.8',
+)
