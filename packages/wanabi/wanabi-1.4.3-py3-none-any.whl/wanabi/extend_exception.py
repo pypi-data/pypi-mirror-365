34ad63a81b3990b501c414d7e76ff1b95d3fe336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:03 +0000

    Add package wanabi/1.4.3/wanabi-1.4.3-py3-none-any.whl

---
+++ packages/wanabi/wanabi-1.4.3-py3-none-any.whl/wanabi/extend_exception.py
@@ -0,0 +1,34 @@
+from tkinter import messagebox
+
+
+class NotOpenPathException(Exception):
+    """
+    ファイルを開くのに失敗した時の例外
+    基本的に握りつぶしてデフォルトを適用して続行する目的
+    """
+
+    pass
+
+class CannotWriteFileException(Exception):
+    """ファイルに書き込めなかったときのエラー"""
+    pass
+
+class CannotMakedirsException(Exception):
+    """
+    ディレクトリの作成エラー
+    """
+    pass
+
+
+class FatalError(Exception):
+    """
+    復旧不能なエラーの発生時に投げる
+    """
+    pass
+
+
+class PathPermissionException(Exception):
+    """
+    パーミッションエラーを無視
+    """
+    pass
