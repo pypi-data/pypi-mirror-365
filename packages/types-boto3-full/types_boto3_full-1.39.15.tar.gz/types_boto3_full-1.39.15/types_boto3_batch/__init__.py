a95cbd62b4850af1b867e15dd9ea40fecd3941d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:13:12 +0000

    Add package types-boto3-full/1.39.15/types_boto3_full-1.39.15.tar.gz

---
+++ packages/types-boto3-full/types_boto3_full-1.39.15.tar.gz/types_boto3_full-1.39.15/types_boto3_batch/__init__.py
@@ -0,0 +1,61 @@
+"""
+Main interface for batch service.
+
+[Documentation](https://youtype.github.io/types_boto3_docs/types_boto3_batch/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from types_boto3_batch import (
+        BatchClient,
+        Client,
+        DescribeComputeEnvironmentsPaginator,
+        DescribeJobDefinitionsPaginator,
+        DescribeJobQueuesPaginator,
+        ListConsumableResourcesPaginator,
+        ListJobsByConsumableResourcePaginator,
+        ListJobsPaginator,
+        ListSchedulingPoliciesPaginator,
+    )
+
+    session = Session()
+    client: BatchClient = session.client("batch")
+
+    describe_compute_environments_paginator: DescribeComputeEnvironmentsPaginator = client.get_paginator("describe_compute_environments")
+    describe_job_definitions_paginator: DescribeJobDefinitionsPaginator = client.get_paginator("describe_job_definitions")
+    describe_job_queues_paginator: DescribeJobQueuesPaginator = client.get_paginator("describe_job_queues")
+    list_consumable_resources_paginator: ListConsumableResourcesPaginator = client.get_paginator("list_consumable_resources")
+    list_jobs_by_consumable_resource_paginator: ListJobsByConsumableResourcePaginator = client.get_paginator("list_jobs_by_consumable_resource")
+    list_jobs_paginator: ListJobsPaginator = client.get_paginator("list_jobs")
+    list_scheduling_policies_paginator: ListSchedulingPoliciesPaginator = client.get_paginator("list_scheduling_policies")
+    ```
+"""
+
+from .client import BatchClient
+from .paginator import (
+    DescribeComputeEnvironmentsPaginator,
+    DescribeJobDefinitionsPaginator,
+    DescribeJobQueuesPaginator,
+    ListConsumableResourcesPaginator,
+    ListJobsByConsumableResourcePaginator,
+    ListJobsPaginator,
+    ListSchedulingPoliciesPaginator,
+)
+
+Client = BatchClient
+
+
+__all__ = (
+    "BatchClient",
+    "Client",
+    "DescribeComputeEnvironmentsPaginator",
+    "DescribeJobDefinitionsPaginator",
+    "DescribeJobQueuesPaginator",
+    "ListConsumableResourcesPaginator",
+    "ListJobsByConsumableResourcePaginator",
+    "ListJobsPaginator",
+    "ListSchedulingPoliciesPaginator",
+)
