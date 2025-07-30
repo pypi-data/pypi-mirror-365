98f763475f5f388df6374f69a0ee2f9cd27d6720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:05:15 +0000

    Add package valve-parsers/1.0.0/valve_parsers-1.0.0.tar.gz

---
+++ packages/valve-parsers/valve_parsers-1.0.0.tar.gz/valve_parsers-1.0.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="valve-parsers",
+    version="1.0.0",
+    author="Madison Lovett",
+    description="Python library for parsing Valve game files (VPK and PCF)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cueki/valve-parsers",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Games/Entertainment",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Archiving"
+    ],
+    python_requires=">=3.8",
+    keywords="valve vpk pcf particle steam source engine team fortress tf2 half-life parser archive modding",
+    project_urls={
+        "Bug Reports": "https://github.com/cueki/valve-parsers/issues",
+        "Source": "https://github.com/cueki/valve-parsers",
+    },
+)
