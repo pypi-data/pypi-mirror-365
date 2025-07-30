25ce935451e64ec7bec4f6f075b3cd4468bfddf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:05:30 +0000

    Add package openlifu/0.8.1/openlifu-0.8.1.tar.gz

---
+++ packages/openlifu/openlifu-0.8.1.tar.gz/openlifu-0.8.1/src/openlifu/xdc/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from . import element, transducer
+from .element import Element
+from .transducer import Transducer
+
+__all__ = [
+    "element",
+    "transducer",
+    "Element",
+    "Transducer",
+]
