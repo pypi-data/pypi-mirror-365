82b1dea286c6db8fe6179d83a52546489d30f249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:05:47 +0000

    Add package canonicalwebteam.image-template/1.7.0/canonicalwebteam_image_template-1.7.0.tar.gz

---
+++ packages/canonicalwebteam.image-template/canonicalwebteam_image_template-1.7.0.tar.gz/canonicalwebteam_image_template-1.7.0/canonicalwebteam/image_template/__init__.py
@@ -0,0 +1,111 @@
+# Standard library
+import os
+import sys
+from urllib.parse import quote, unquote, urlparse
+
+# Packages
+from jinja2 import Environment, FileSystemLoader
+
+parent_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+env = Environment(loader=FileSystemLoader(parent_dir + "/templates"))
+template = env.get_template("image_template.html")
+cloudinary_url_base = "https://res.cloudinary.com/canonical/image/fetch"
+
+
+def image_template(
+    url,
+    alt,
+    hi_def,
+    width,
+    height=None,
+    fill=False,
+    e_sharpen=False,
+    loading="lazy",
+    fmt="auto",
+    attrs={},
+    output_mode="html",
+):
+    """
+    Generate image markup
+    """
+
+    url_parts = urlparse(url)
+
+    # Default cloudinary optimisations
+    # https://cloudinary.com/documentation/image_transformations
+
+    cloudinary_options = [
+        "f_" + str(fmt),
+        "q_auto",  # Auto optimise quality
+        "fl_sanitize",  # Sanitize SVG content
+    ]
+
+    if e_sharpen:
+        cloudinary_options.append("e_sharpen")
+
+    # If the original image does not match the requested
+    # ratio set crop and fill see
+    # https://cloudinary.com/documentation/image_transformation_reference#crop_parameter
+    if fill:
+        cloudinary_options.append("c_fill")
+
+    if not url_parts.netloc:
+        raise Exception("url must contain a hostname")
+
+    std_def_cloudinary_options = cloudinary_options.copy()
+    hi_def_cloudinary_options = cloudinary_options.copy()
+
+    std_def_cloudinary_options.append("w_" + str(width))
+
+    if height is not None:
+        std_def_cloudinary_options.append("h_" + str(height))
+
+    if hi_def:
+        hi_def_cloudinary_options.append("w_" + str(int(width) * 2))
+
+        if height is not None:
+            hi_def_cloudinary_options.append("h_" + str(int(height) * 2))
+
+    std_def_cloudinary_attrs = ",".join(std_def_cloudinary_options)
+    hi_def_cloudinary_attrs = ",".join(hi_def_cloudinary_options)
+
+    # Decode the URL first to prevent double encoding
+    decoded_url = unquote(url)
+    encoded_url = quote(decoded_url, safe="")
+
+    image_src = (
+        f"{cloudinary_url_base}/"
+        f"{std_def_cloudinary_attrs}/"
+        f"{encoded_url}"
+    )
+
+    image_srcset = (
+        f"{cloudinary_url_base}/c_limit,"
+        f"{hi_def_cloudinary_attrs}/{encoded_url} 2x"
+    )
+
+    image_attrs = {
+        "src": image_src,
+        "srcset": image_srcset,
+        "alt": alt,
+        "width": int(width),
+        "height": height,
+        "hi_def": hi_def,
+        "loading": loading,
+        "attrs": attrs,
+    }
+
+    if not hi_def:
+        del image_attrs["srcset"]
+
+    if output_mode == "html":
+        return template.render(**image_attrs)
+    elif output_mode == "attrs":
+        merged_attrs = {**image_attrs, **attrs}
+        del merged_attrs["attrs"]
+        return merged_attrs
+    else:
+        raise ValueError("output_mode must be 'html' or 'attrs'")
+
+
+sys.modules[__name__] = image_template
---
+++ packages/canonicalwebteam.image-template/canonicalwebteam_image_template-1.7.0.tar.gz/canonicalwebteam_image_template-1.7.0/tests/test_image_template.py
@@ -0,0 +1,168 @@
+# Standard library
+import unittest
+
+# Local
+from canonicalwebteam import image_template
+from urllib.parse import quote, unquote
+
+
+asset_url = (
+    "https://assets.ubuntu.com/" "v1/479958ed-vivid-hero-takeover-kylin.jpg"
+)
+non_asset_url = (
+    "https://dashboard.snapcraft.io/site_media/appmedia/"
+    "2018/10/Screenshot_from_2018-10-26_14-20-14.png"
+)
+non_hostname_url = "/static/images/Screenshot_from_2018-10-26_14-20-14.png"
+cloudinary_url_base = "https://res.cloudinary.com/canonical/image/fetch"
+
+
+class TestImageTemplate(unittest.TestCase):
+    def test_returns_string(self):
+        markup = image_template(
+            url=asset_url,
+            alt="test",
+            width="1920",
+            height="1080",
+            hi_def=False,
+        )
+        self.assertTrue(isinstance(markup, str))
+
+    def test_attributes(self):
+        markup = image_template(
+            url=asset_url,
+            alt="test",
+            width="1920",
+            height="1080",
+            hi_def=False,
+            attrs={"id": "test", "title": "test title"},
+        )
+        self.assertIn('id="test"', markup)
+        self.assertIn('title="test title"', markup)
+
+    def test_classes(self):
+        markup = image_template(
+            url=asset_url,
+            alt="test",
+            width="1920",
+            height="1080",
+            hi_def=False,
+            attrs={"class": "test-title"},
+        )
+        # Check custom class exists
+        self.assertIn('class="test-title"', markup)
+
+    def test_optional_lazy(self):
+        markup = image_template(
+            url=asset_url,
+            alt="test",
+            width="1920",
+            height="1080",
+            loading="auto",
+            attrs={"class": "test-title"},
+            hi_def=False,
+        )
+        # Check lazyload class is not present
+        self.assertIn('class="test-title"', markup)
+
+    def test_optional_fill(self):
+        markup = image_template(
+            url=asset_url,
+            alt="test",
+            width="1920",
+            height="1080",
+            loading="auto",
+            fill=True,
+            hi_def=False,
+        )
+        # Check c_fill is present
+        self.assertIn("c_fill", markup)
+
+    def test_e_sharpen(self):
+        markup = image_template(
+            url=asset_url,
+            alt="test",
+            width="1920",
+            height="1080",
+            loading="auto",
+            fill=True,
+            e_sharpen=True,
+            hi_def=False,
+        )
+        # Check e_sharpen is present
+        self.assertIn("e_sharpen", markup)
+
+    def test_hi_def(self):
+        markup = image_template(
+            url=non_asset_url,
+            alt="test",
+            width="1920",
+            height="1080",
+            hi_def=True,
+        )
+        markup_asset = image_template(
+            url=asset_url, alt="test", width="1920", height="1080", hi_def=True
+        )
+
+        # Check the markup includes srcset
+        self.assertTrue(markup.find("srcset="))
+        self.assertTrue(markup.find("data-srcset"))
+        # Check x2 is present
+        self.assertTrue(markup.find("x2"))
+        # Check width and height are double
+        self.assertTrue(markup.find("3840"))
+        self.assertTrue(markup.find("2160"))
+
+        self.assertTrue(markup_asset.find("srcset="))
+        self.assertTrue(markup_asset.find("data-srcset"))
+        self.assertTrue(markup_asset.find("x2"))
+        self.assertTrue(markup_asset.find("w%3D3840%26h%3D2160"))
+
+    def test_height_is_optional(self):
+        image = image_template(
+            url=non_asset_url,
+            alt="test",
+            width="1920",
+            hi_def=True,
+        )
+
+        self.assertNotIn("height=", image)
+        self.assertNotIn("h_auto", image)
+
+    def test_attrs_return(self):
+        image_attrs = {
+            "url": asset_url,
+            "alt": "test",
+            "width": 1920,
+            "height": 1080,
+            "loading": "lazy",
+            "hi_def": True,
+            "attrs": {},
+        }
+
+        decoded_asset_url = unquote(asset_url)
+        encoded_asset_url = quote(decoded_asset_url, safe="")
+
+        expected_attrs = image_attrs.copy()
+        returned_attrs = image_template(**image_attrs, output_mode="attrs")
+
+        # consumed by src and srcset
+        del expected_attrs["url"]
+        # spread onto the rest of the attributes
+        del expected_attrs["attrs"]
+
+        expected_attrs["src"] = (
+            f"{cloudinary_url_base}/f_auto,q_auto,fl_sanitize,w_1920,h_1080"
+            f"/{encoded_asset_url}"
+        )
+
+        expected_attrs["srcset"] = (
+            f"{cloudinary_url_base}/c_limit,f_auto,q_auto,fl_sanitize,"
+            f"w_3840,h_2160/{encoded_asset_url} 2x"
+        )
+
+        self.assertEqual(expected_attrs, returned_attrs)
+
+
+if __name__ == "__main__":
+    unittest.main()
