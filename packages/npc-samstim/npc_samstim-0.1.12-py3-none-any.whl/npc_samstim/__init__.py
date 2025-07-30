ed02e1ab2a9a927bb30e58304302d2e8fd8eeea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:11 +0000

    Add package npc-samstim/0.1.12/npc_samstim-0.1.12-py3-none-any.whl

---
+++ packages/npc-samstim/npc_samstim-0.1.12-py3-none-any.whl/npc_samstim/__init__.py
@@ -0,0 +1,41 @@
+"""
+    Tools for processing stim data from Sam's DynamicRoutingTask, compatible with data in the cloud.
+"""
+
+import doctest
+import importlib.metadata
+import logging
+
+from npc_samstim.stim import *
+
+# import functions from submodules here:
+from npc_samstim.waveforms import *
+
+logger = logging.getLogger(__name__)
+
+__version__ = importlib.metadata.version("npc_samstim")
+logger.debug(f"{__name__}.{__version__ = }")
+
+
+def testmod(**testmod_kwargs) -> doctest.TestResults:
+    """
+    Run doctests for the module, configured to ignore exception details and
+    normalize whitespace.
+
+    Accepts kwargs to pass to doctest.testmod().
+
+    Add to modules to run doctests when run as a script:
+    .. code-block:: text
+        if __name__ == "__main__":
+            from npc_io import testmod
+            testmod()
+
+    """
+    _ = testmod_kwargs.setdefault(
+        "optionflags", doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS
+    )
+    return doctest.testmod(**testmod_kwargs)
+
+
+if __name__ == "__main__":
+    testmod()
