7ea53f8a5c3ee86d9d657ef513445cb846a429fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:04:57 +0000

    Add package torch-npu/2.1.0.post13/torch_npu-2.1.0.post13-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/torch-npu/torch_npu-2.1.0.post13-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/torch_npu/utils/__init__.py
@@ -0,0 +1,27 @@
+__all__ = ["save_async", "npu_combine_tensors", "get_part_combined_tensor", "is_combined_tensor_valid", "FlopsCounter",
+           "set_thread_affinity", "reset_thread_affinity"]
+
+from torch_npu import _C
+from ._module import _apply_module_patch
+from .tensor_methods import _add_tensor_methods
+from .storage import _add_storage_methods
+from .combine_tensors import npu_combine_tensors, get_part_combined_tensor, is_combined_tensor_valid
+from ._device import apply_device_patch
+from .serialization import _add_serialization_methods, save_async
+from .npu_intercept import _cann_package_check, _add_intercept_methods
+from .dtensor import _register_ops_under_dtensor_rules
+from .collect_env import _add_collect_env_methods
+from ._dynamo import add_dynamo_methods
+from ._optim import add_optim_method
+from .asd_detector import set_asd_loss_scale, register_asd_hook
+from .utils import _print_error_log, _print_warn_log, _print_info_log, _should_print_warning, _apply_npu_show_warning
+from .clip_grad_norm_ import _apply_clip_grad_norm_patch
+from ._step import add_perf_dump_patch
+from .flops_count import _FlopsCounter as FlopsCounter
+from .affinity import _set_thread_affinity as set_thread_affinity
+from .affinity import _reset_thread_affinity as reset_thread_affinity
+
+
+# init flopcount
+if not _C._flops_count_init():
+    raise RuntimeError("flopcount initialization failed" + prof_error(ErrCode.UNAVAIL))
