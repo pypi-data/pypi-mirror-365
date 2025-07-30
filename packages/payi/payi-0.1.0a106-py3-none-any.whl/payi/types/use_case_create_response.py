9945f468c2aa52efc1529c864e8c5621247b9310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:06:09 +0000

    Add package payi/0.1.0a106/payi-0.1.0a106-py3-none-any.whl

---
+++ packages/payi/payi-0.1.0a106-py3-none-any.whl/payi/types/use_case_create_response.py
@@ -0,0 +1,15 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Optional
+
+from .._models import BaseModel
+
+__all__ = ["UseCaseCreateResponse"]
+
+
+class UseCaseCreateResponse(BaseModel):
+    use_case_id: str
+
+    limit_id: Optional[str] = None
+
+    type_version: Optional[int] = None
