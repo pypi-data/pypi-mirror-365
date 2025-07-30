e8b1ec47d6c39de0ce9b08a9dd3585b301abf2e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 21:05:53 +0000

    Add package pulseaudio-device-control/0.4/pulseaudio_device_control-0.4.tar.gz

---
+++ packages/pulseaudio-device-control/pulseaudio_device_control-0.4.tar.gz/pulseaudio_device_control-0.4/README.md
@@ -0,0 +1,50 @@
+# pulseaudio_device_control
+
+A simple script to control PulseAudio devices.
+Mainly this is to easily cycle the default sink or source.
+
+## Installation
+
+pulseaudio\_device\_control depends on [pulsectl](https://github.com/mk-fg/python-pulse-control).
+
+This package can be installed using [pipx](https://pipx.pypa.io/stable/installation/):
+
+```bash
+pipx install pulseaudio_device_control
+```
+
+## Usage
+
+```bash
+pulseaudio_device_control OPERATION DEVICE_TYPE [AMOUNT]
+```
+
+Where:
+
+- `OPERATION` is `raise`, `lower`, `mute`, or `next`
+- `DEVICE_TYPE` is `sink` or `source`
+- `AMOUNT` is a percentage value for the raise and lower operations (optional, defaults to 2)
+
+When using `next` any device name ending with `monitor` is skipped.
+
+## Configuration
+
+pulseaudio_device_control can be configured with a file located at `~/.config/pulseaudio_device_control/config.ini`,
+e.g.
+
+```ini
+[next]
+# Ignore devices from the next command (devices ending in "monitor" are always ignored)
+sink_ignore = ["sink_name1", "sink_name2"]
+source_ignore = ["source_name1", "source_name2"]
+```
+
+The names of PulseAudio sink/source devices can be printed with:
+
+```python
+import pulsectl
+
+pulse = pulsectl.Pulse()
+print([sink.name for sink in pulse.sink_list()])
+print([source.name for source in pulse.source_list()])
+```
---
+++ packages/pulseaudio-device-control/pulseaudio_device_control-0.4.tar.gz/pulseaudio_device_control-0.4/src/pulseaudio_device_control/pulseaudio_device_control.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python3
+
+
+import sys
+from enum import Enum
+import pulsectl
+
+from . import config
+
+
+class Operation(Enum):
+    RAISE = 1
+    LOWER = 2
+    MUTE = 3
+    NEXT = 4
+
+
+class DeviceType(Enum):
+    SINK = 1
+    SOURCE = 2
+
+
+def print_usage():
+    print("Usage: pulseaudio_device_control OPERATION DEVICE_TYPE [AMOUNT]")
+    print("    OPERATION: \"raise\", \"lower\", \"mute\", or \"next\"")
+    print("    DEVICE_TYPE: \"sink\" or \"source\"")
+    print("    AMOUNT: Optional integer % change for raise and lower\n")
+
+
+# Parse arguments and return "operation" and "device type"
+def parse_arguments():
+    if len(sys.argv) != 3 and len(sys.argv) != 4:
+        print("Incorrect number of arguments\n")
+        print_usage()
+        sys.exit(1)
+
+    operation = None
+
+    match sys.argv[1]:
+        case "raise":
+            operation = Operation.RAISE
+        case "lower":
+            operation = Operation.LOWER
+        case "mute":
+            operation = Operation.MUTE
+        case "next":
+            operation = Operation.NEXT
+        case _:
+            print("Unknown operation " + sys.argv[1] + "\n")
+            print_usage()
+            sys.exit(1)
+
+    device_type = None
+
+    match sys.argv[2]:
+        case "sink":
+            device_type = DeviceType.SINK
+        case "source":
+            device_type = DeviceType.SOURCE
+        case _:
+            print("Unknown device type " + sys.argv[2] + "\n")
+            print_usage()
+            sys.exit(1)
+
+    amount = 0.02
+
+    if len(sys.argv) == 4:
+        amount = float(sys.argv[3]) * 0.01
+
+    return operation, device_type, amount
+
+
+def raise_volume(pulse, device, amount):
+    if device.mute:
+        pulse.mute(device, False)
+
+        if device.volume.value_flat != 0.:
+            return
+
+    pulse.volume_change_all_chans(device, amount)
+
+
+def lower_volume(pulse, device, amount):
+    pulse.volume_change_all_chans(device, -amount)
+
+    if device.volume.value_flat == 0.:
+        pulse.mute(device)
+
+
+def toggle_mute(pulse, device):
+    pulse.mute(device, not device.mute)
+
+
+def next_device(pulse, device_type: DeviceType, device_list, default_device_name):
+    if len(device_list) == 1:
+        return
+
+    # Load config here as it's only used by next
+    config.load()
+    ignore_list = config.next_sink_ignore if device_type == DeviceType.SINK else config.next_source_ignore
+
+    current_idx = -1
+
+    # Find the default device
+    for idx, device in enumerate(device_list):
+        if device.name == default_device_name:
+            # Save the index of the next device
+            current_idx = idx
+            break
+
+    if current_idx < 0:
+        # Default device not found
+        return
+
+    next_idx = current_idx + 1
+
+    # Find the next device while respecting the ignore list
+    while True:
+        # No next device
+        if next_idx == current_idx:
+            return
+
+        # Wrap around
+        if next_idx >= len(device_list):
+            next_idx = 0
+
+        # Check that the device name doesn't end in ".monitor" and isn't in the ignore list
+        next_device_name = device_list[next_idx].name
+        if not next_device_name.endswith(".monitor") and not next_device_name in ignore_list:
+            break
+
+        next_idx = next_idx + 1
+
+    # Set the default device
+    pulse.default_set(device_list[next_idx])
+
+
+def main():
+    operation, device_type, amount = parse_arguments()
+
+    with pulsectl.Pulse('pulseaudio_device_control', threading_lock=True) as pulse:
+
+        device_list = pulse.sink_list() if device_type == DeviceType.SINK \
+            else pulse.source_list()
+
+        default_device_name = pulse.server_info().default_sink_name if device_type == DeviceType.SINK \
+            else pulse.server_info().default_source_name
+
+        if operation == Operation.NEXT:
+            next_device(pulse, device_type, device_list, default_device_name)
+
+        else:  # raise, lower, or mute
+
+            # Find the default device
+            for device in device_list:
+                if device.name == default_device_name:
+                    match operation:
+                        case Operation.RAISE:
+                            raise_volume(pulse, device, amount)
+                        case Operation.LOWER:
+                            lower_volume(pulse, device, amount)
+                        case Operation.MUTE:
+                            toggle_mute(pulse, device)
+
+
+if __name__ == '__main__':
+    main()
