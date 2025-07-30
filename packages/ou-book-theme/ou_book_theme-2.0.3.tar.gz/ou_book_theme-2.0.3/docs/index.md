3669d0e7eb3bfe472e84a8ab3b56eb49a2429218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:49 +0000

    Add package ou-book-theme/2.0.3/ou_book_theme-2.0.3.tar.gz

---
+++ packages/ou-book-theme/ou_book_theme-2.0.3.tar.gz/ou_book_theme-2.0.3/docs/index.md
@@ -0,0 +1,4 @@
+# OU Book Theme
+
+The OU Book Theme is an extension of the [Sphinx Book Theme](https://sphinx-book-theme.readthedocs.io), adding Open University specific theming,
+as well as custom directives and roles. It is meant for use with [Sphinx](https://www.sphinx-doc.org).
---
+++ packages/ou-book-theme/ou_book_theme-2.0.3.tar.gz/ou_book_theme-2.0.3/ou_book_theme/cli/__init__.py
@@ -0,0 +1,38 @@
+"""JupyterBook CLI extension."""
+
+import os
+from typing import Annotated
+
+from livereload import Server, shell
+from typer import Argument, Option, Typer
+
+cli = Typer()
+
+
+@cli.command()
+def build(path: Annotated[str, Argument(help="Path to the OU book")]) -> None:
+    """Serve the OU Book locally."""
+    full_build = shell(f"sphinx-build --builder html --fresh-env {path} {os.path.join(path, '_build', 'html')}")
+    full_build()
+
+
+@cli.command()
+def serve(
+    path: Annotated[str, Argument(help="Path to the OU book")],
+    host: Annotated[str, Option(help="The host to serve the book at")] = "127.0.0.1",
+    port: Annotated[int, Option(help="The port to serve the book at")] = 8000,
+) -> None:
+    """Serve the OU Book locally."""
+    partial_build = shell(f"sphinx-build --builder html {path} {os.path.join(path, '_build', 'html')}")
+    full_build = shell(f"sphinx-build --builder html --fresh-env {path} {os.path.join(path, '_build', 'html')}")
+    full_build()
+
+    server = Server()
+    server.watch(f"{path}/**/*.md", partial_build)
+    server.watch(f"{path}/**/*.yml", full_build)
+    server.watch(f"{path}/**/*.py", full_build)
+    server.watch(f"{path}/**/*.png", full_build)
+    server.watch(f"{path}/**/*.jpg", full_build)
+    server.watch(f"{path}/**/*.jpeg", full_build)
+    server.watch(f"{path}/**/*.svg", full_build)
+    server.serve(root=f"{path}/_build/html", port=port, host=host)
