d51a96eee27590cae154b11f907a700ce7a9d1cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:25 +0000

    Add package lovepackage/0.1/lovepackage-0.1.tar.gz

---
+++ packages/lovepackage/lovepackage-0.1.tar.gz/lovepackage-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lovepackage',
+    version='0.1',
+    description='A lovely message from Harshith to Mushaib',
+    author='Harshith',
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
