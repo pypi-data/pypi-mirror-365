820ec6d4b8d9810aeee05e84f855c2de3c1ae2af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:34 +0000

    Add package exotools/0.0.1/exotools-0.0.1-py3-none-any.whl

---
+++ packages/exotools/exotools-0.0.1-py3-none-any.whl/exotools/db/tic_db.py
@@ -0,0 +1,23 @@
+from astropy.table import QTable
+from .base_db import BaseDB
+
+_ID_FIELD = "tic_id"
+
+
+class TicDB(BaseDB):
+    """
+    Dtypes:
+    -------------------
+    tic_id        int64
+    gaia_id       int64
+    priority    float64
+    ra          float64
+    dec         float64
+    -------------------
+    """
+
+    def __init__(self, dataset: QTable):
+        super().__init__(dataset, id_field="tic_id")
+
+    def _factory(self, dataset: QTable) -> "TicDB":
+        return TicDB(dataset)
