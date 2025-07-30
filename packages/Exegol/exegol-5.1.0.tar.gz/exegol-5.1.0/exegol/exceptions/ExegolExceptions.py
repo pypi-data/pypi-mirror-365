108f12b14f2f0f3c821337f25f8044947b4b3aeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:14 +0000

    Add package Exegol/5.1.0/exegol-5.1.0.tar.gz

---
+++ packages/Exegol/exegol-5.1.0.tar.gz/exegol-5.1.0/exegol/exceptions/ExegolExceptions.py
@@ -0,0 +1,23 @@
+# Exceptions specific to the successful operation of exegol
+class ObjectNotFound(Exception):
+    """Custom exception when a specific container do not exist"""
+    pass
+
+
+class ProtocolNotSupported(Exception):
+    """Custom exception when a specific network protocol is not supported"""
+    pass
+
+
+class CancelOperation(Exception):
+    """Custom exception when an error occurred and the operation must be canceled ou skipped"""
+    pass
+
+
+class LicenseRevocation(Exception):
+    """Custom exception when the license has been revoked"""
+    pass
+
+
+class LicenseToleration(Exception):
+    pass
