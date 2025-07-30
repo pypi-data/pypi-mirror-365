e0c44c05156cc32a7f9147a80e21de11a4202b46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:45 +0000

    Add package phoneshift/0.8.5/phoneshift-0.8.5-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/phoneshift/phoneshift-0.8.5-cp311-cp311-macosx_11_0_arm64.whl/phoneshift/tests/__init__.py
@@ -0,0 +1,18 @@
+import unittest
+
+import sys
+import os
+
+sys.path.append(os.path.dirname(__file__)+'/../..') # Ensure this package is being tested
+import phoneshift.tests.nolicense_test as nolicense_test
+import phoneshift.tests.license_test as license_test
+
+def run():
+    testSuite = unittest.TestSuite()
+    testLoader = unittest.TestLoader()
+    testSuite.addTest(testLoader.loadTestsFromModule(nolicense_test))
+    testSuite.addTest(testLoader.loadTestsFromModule(license_test))
+
+    testRunner = unittest.runner.TextTestRunner(verbosity=3)
+    ret = testRunner.run(testSuite)
+    assert len(ret.failures) == 0
