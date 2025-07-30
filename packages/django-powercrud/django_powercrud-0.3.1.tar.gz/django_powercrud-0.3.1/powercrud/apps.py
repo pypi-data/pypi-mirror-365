aa82d8cdfc1b52d64feed5ad221591a56a612492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 23:07:56 +0000

    Add package django-powercrud/0.3.1/django_powercrud-0.3.1.tar.gz

---
+++ packages/django-powercrud/django_powercrud-0.3.1.tar.gz/django_powercrud-0.3.1/powercrud/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+class PowerCRUDConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "powercrud"
+    verbose_name = "PowerCRUD"
