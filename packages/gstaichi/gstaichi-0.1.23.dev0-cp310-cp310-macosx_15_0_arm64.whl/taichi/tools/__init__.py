665b58778c7f9deee7a8adf57c390181c5714fa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:10 +0000

    Add package gstaichi/0.1.23.dev0/gstaichi-0.1.23.dev0-cp310-cp310-macosx_15_0_arm64.whl

---
+++ packages/gstaichi/gstaichi-0.1.23.dev0-cp310-cp310-macosx_15_0_arm64.whl/taichi/tools/__init__.py
@@ -0,0 +1,12 @@
+# type: ignore
+
+"""Taichi utility module.
+
+- `image` submodule for image io.
+- `video` submodule for exporting results to video files.
+- `diagnose` submodule for printing system environment information.
+"""
+
+from taichi.tools.diagnose import *
+from taichi.tools.np2ply import *
+from taichi.tools.vtk import *
