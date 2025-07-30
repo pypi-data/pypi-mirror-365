114c2edb674d9c11ca94e3984e48f9239220d2f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 22:05:22 +0000

    Add package tvkit/0.1.1/tvkit-0.1.1.tar.gz

---
+++ packages/tvkit/tvkit-0.1.1.tar.gz/tvkit-0.1.1/tvkit/api/scanner/__init__.py
@@ -0,0 +1,9 @@
+"""
+TradingView Scanner API Module
+
+Provides models and utilities for interacting with TradingView's scanner API.
+"""
+
+# from .model import ScannerRequest, ScannerResponse, StockData
+
+__all__: list[str] = []
