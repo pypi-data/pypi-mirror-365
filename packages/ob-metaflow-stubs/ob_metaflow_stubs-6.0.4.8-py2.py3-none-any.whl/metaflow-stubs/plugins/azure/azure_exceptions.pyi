d06e9f274c79ce3b2abde9e83e38970929fe915f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:05:41 +0000

    Add package ob-metaflow-stubs/6.0.4.8/ob_metaflow_stubs-6.0.4.8-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.4.8-py2.py3-none-any.whl/metaflow-stubs/plugins/azure/azure_exceptions.pyi
@@ -0,0 +1,24 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.21.5+obcheckpoint(0.2.4);ob(v1)                                                   #
+# Generated on 2025-07-29T18:33:43.971594                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+import metaflow
+import typing
+if typing.TYPE_CHECKING:
+    import metaflow.exception
+
+from ...exception import MetaflowException as MetaflowException
+
+class MetaflowAzureAuthenticationError(metaflow.exception.MetaflowException, metaclass=type):
+    ...
+
+class MetaflowAzureResourceError(metaflow.exception.MetaflowException, metaclass=type):
+    ...
+
+class MetaflowAzurePackageError(metaflow.exception.MetaflowException, metaclass=type):
+    ...
+
---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.4.8-py2.py3-none-any.whl/metaflow-stubs/tuple_util.pyi
@@ -0,0 +1,15 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.21.5+obcheckpoint(0.2.4);ob(v1)                                                   #
+# Generated on 2025-07-29T18:33:43.896525                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+
+def namedtuple_with_defaults(typename, field_descr, defaults = ()):
+    ...
+
+foreach_frame_field_list: list
+
