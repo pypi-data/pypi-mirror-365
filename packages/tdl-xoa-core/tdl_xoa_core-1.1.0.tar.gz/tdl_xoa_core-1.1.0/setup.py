35817aefcb585ad48508c8dde2a3be237d6d04e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:46 +0000

    Add package tdl-xoa-core/1.1.0/tdl_xoa_core-1.1.0.tar.gz

---
+++ packages/tdl-xoa-core/tdl_xoa_core-1.1.0.tar.gz/tdl_xoa_core-1.1.0/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+
+def main() -> None:
+    with open("README.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
+
+    setuptools.setup(
+        name="tdl-xoa-core",
+        description="XOA framework for RFC test suites execution, integration, and development.",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        author="Leonard Yu",
+        author_email="leonard.yu@teledyne.com",
+        maintainer="Teledyne LeCroy Xena",
+        maintainer_email="support@xenanetworks.com",
+        url="https://github.com/xenanetworks/tdl-xoa-core",
+        packages=setuptools.find_packages(),
+        license='Apache 2.0',
+        install_requires=["tdl-xoa-driver>=1.3, <2", "pydantic>=2.0", "semver", "oyaml",],
+        classifiers=[
+            "Development Status :: 5 - Production/Stable",
+            "Intended Audience :: Developers",
+            "Topic :: Software Development :: Libraries :: Application Frameworks",
+            "License :: OSI Approved :: Apache Software License",
+            "Programming Language :: Python :: 3.11",
+            "Programming Language :: Python :: 3.12",
+            "Programming Language :: Python :: 3.13",
+            "Programming Language :: Python :: 3.14",
+        ],
+        python_requires=">=3.11",
+    )
+
+
+if __name__ == '__main__':
+    main()
