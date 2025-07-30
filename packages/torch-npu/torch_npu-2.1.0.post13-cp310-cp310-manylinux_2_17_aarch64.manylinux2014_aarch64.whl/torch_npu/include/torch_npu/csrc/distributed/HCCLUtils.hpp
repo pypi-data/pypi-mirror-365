74db152052a14d7432896b9f01e15aa22871cded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:05:09 +0000

    Add package torch-npu/2.1.0.post13/torch_npu-2.1.0.post13-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/torch-npu/torch_npu-2.1.0.post13-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/torch_npu/include/torch_npu/csrc/distributed/HCCLUtils.hpp
@@ -0,0 +1,264 @@
+#pragma once
+#include <map>
+#include <memory>
+#include <string>
+
+#include "torch_npu/csrc/core/npu/npu_log.h"
+#include "torch_npu/csrc/core/npu/sys_ctrl/npu_sys_ctrl.h"
+#include "torch_npu/csrc/core/npu/NPUException.h"
+
+#include <ATen/ATen.h>
+#include <c10/util/Optional.h>
+#include "third_party/hccl/inc/hccl/hccl.h"
+#include "third_party/hccl/inc/hccl/hccl_types.h"
+
+#define HCCL_CHECK_ERROR(err_code, ...)                                      \
+    do {                                                                     \
+        auto Error = err_code;                                               \
+        if ((Error) != HCCL_SUCCESS) {                                       \
+            CHECK_AND_THROW_ERROR_WITH_SPECIFIC_MESSAGE(Error);              \
+            if (c10_npu::option::OptionsManager::IsCompactErrorOutput()) {   \
+                std::ostringstream oss;                                      \
+                oss << " HCCL function error: " << getErrorFunction(#err_code, ##__VA_ARGS__)    \
+                   << ", error code is " << Error << " "                    \
+                   << DIST_ERROR(ErrCode::HCCL) + ".\n";                     \
+                std::string err_msg = oss.str();                          \
+                ASCEND_LOGE("%s", err_msg.c_str());                       \
+                TORCH_CHECK(                                                 \
+                    false,                                                   \
+                    c10_npu::c10_npu_get_error_message());                   \
+            } else {                                                         \
+            TORCH_CHECK(                                                     \
+                false,                                                       \
+                __func__,                                                    \
+                ":",                                                         \
+                __FILE__,                                                    \
+                ":",                                                         \
+                __LINE__,                                                    \
+                " HCCL function error: ", getErrorFunction(#err_code, ##__VA_ARGS__),   \
+                ", error code is ", Error,                                   \
+                DIST_ERROR(ErrCode::HCCL) + ".\n" +                          \
+                c10_npu::c10_npu_get_error_message());                               \
+        }                                                                    \
+    }                                                                       \
+    } while (0)
+
+#define ENABLE_HCCL_ERROR_CHECKING
+
+// Macro to throw on a non-successful HCCL return value.
+#define C10D_HCCL_CHECK(cmd)                                                        \
+    do {                                                                            \
+        HcclResult result = cmd;                                                    \
+        if (result != HCCL_SUCCESS) {                                               \
+            std::string err = "HCCL error in: " + std::string(__FILE__) + ":" +     \
+            std::to_string(__LINE__) + ", " +                                       \
+            "\n" + getHcclErrorDetailStr(result);                                   \
+            TORCH_CHECK(false, err, DIST_ERROR(ErrCode::HCCL));                     \
+        }                                                                           \
+    } while (0)
+
+namespace c10d_npu {
+extern HcclResult hcclGetCommAsyncError(HcclComm comm, HcclResult* asyncError);
+extern HcclResult hcclCommInitRootInfoConfig(uint32_t nRanks, const HcclRootInfo *rootInfo, uint32_t rank, HcclCommConfig* config, HcclComm *comm);
+extern HcclResult hcclCommInitClusterInfoConfig(const char *clusterInfo, uint32_t rank, HcclCommConfig *config, HcclComm *comm);
+extern HcclResult hcclCreateSubCommConfig(HcclComm *comm, uint32_t rankNum, uint32_t *rankIds, uint64_t subCommId, uint32_t subCommRankId,
+    HcclCommConfig* config, HcclComm *subComm);
+extern HcclResult hcclCommWorkingDevNicSet(HcclComm comm, uint32_t *ranks, bool *useBackup, uint32_t nRanks);
+
+// Provides additional detail into HCCL error codes based on when these are
+// thrown in the HCCL codebase.
+std::string getHcclErrorDetailStr(
+    HcclResult error,
+    c10::optional<std::string> processGroupFailureReason = c10::nullopt);
+
+HcclDataType getHcclDataType(at::ScalarType type);
+
+std::string getHcclDataTypeSerialString(HcclDataType type);
+
+bool isFileExists(const std::string& path);
+
+bool checkFilePathReadable(const std::string& file);
+
+bool isSupportHcclCommName();
+
+#define WARN_ENV_VAR_ONCE(deprecated_env, new_env)                      \
+TORCH_WARN_ONCE(                                                        \
+    "Environment variable " + (deprecated_env) + " is deprecated; use " + \
+    (new_env) + " instead");
+
+inline std::string getCvarString(
+    const std::vector<std::string> &env,
+    const char *def)
+{
+    const char *ret = def;
+
+    if (env.empty()) {
+        TORCH_CHECK(false, "No environment variables passed");
+        return ret;
+    }
+
+    /* parse environment variable in reverse order, so the early
+     * versions of a variable get higher priority than the latter
+     * versions of the same variable */
+    for (ssize_t i = static_cast<ssize_t>(env.size()) - 1; i >= 0; i--) {
+        const char *val = std::getenv(env[i].c_str());
+        if (val == nullptr) {
+            continue;
+        } else if (i != 0) {
+            WARN_ENV_VAR_ONCE(env[i], env[0]);
+        }
+        ret = val;
+    }
+    return ret;
+}
+
+inline int getCvarInt(const std::vector<std::string> &env, int def)
+{
+    int ret = def;
+
+    if (env.empty()) {
+        TORCH_CHECK(false, "No environment variables passed");
+        return ret;
+    }
+
+    /* parse environment variable in reverse order, so the early
+     * versions of a variable get higher priority than the latter
+     * versions of the same variable */
+    for (ssize_t i = static_cast<ssize_t>(env.size()) - 1; i >= 0; i--) {
+        const char *val = std::getenv(env[i].c_str());
+        if (val == nullptr) {
+            continue;
+        } else if (i != 0) {
+            WARN_ENV_VAR_ONCE(env[i], env[0]);
+        }
+        try {
+            ret = std::stoi(val);
+        } catch (std::exception &) {
+            TORCH_CHECK(false, "Invalid value for environment variable: " + env[i]);
+        }
+    }
+    return ret;
+}
+
+inline bool getCvarBool(const std::vector<std::string> &env, bool def)
+{
+    bool ret = def;
+    if (env.empty()) {
+        TORCH_CHECK(false, "No environment variables passed");
+        return ret;
+    }
+
+    /* parse environment variable in reverse order, so the early
+     * versions of a variable get higher priority than the latter
+     * versions of the same variable */
+    for (ssize_t i = static_cast<ssize_t>(env.size()) - 1; i >= 0; i--) {
+        const char *val_ = std::getenv(env[i].c_str());
+        if (val_ == nullptr) {
+            continue;
+        } else if (i != 0) {
+            WARN_ENV_VAR_ONCE(env[i], env[0]);
+        }
+
+        std::string val = std::string(val_);
+        for (auto &x : val) {
+            // NOLINTNEXTLINE(*-narrowing-conversions)
+            x = std::tolower(x);
+        }
+        if (val == "y" || val == "yes" || val == "1" || val == "t" ||
+            val == "true") {
+            ret = true;
+        } else if (
+            val == "n" || val == "no" || val == "0" || val == "f" ||
+            val == "false") {
+            ret = false;
+        } else {
+            TORCH_CHECK(false, "Invalid value for environment variable: " + env[i]);
+            return ret;
+        }
+    }
+
+    return ret;
+}
+
+// RAII wrapper for HCCL communicator
+class C10_NPU_API HCCLComm {
+public:
+    explicit HCCLComm(HcclComm hcclComm);
+    HCCLComm() : HCCLComm(nullptr) {}
+    ~HCCLComm();
+
+    static std::shared_ptr<HCCLComm> create(
+        int numRanks,
+        int rank,
+        HcclRootInfo& rootInfo);
+
+    static std::shared_ptr<HCCLComm> create_config(
+        int numRanks,
+        int rank,
+        HcclRootInfo& rootInfo,
+        HcclCommConfig* config);
+
+    static std::shared_ptr<HCCLComm> createGlobalHcclComm(
+        const char *clusterInfo,
+        uint32_t rank,
+        HcclCommConfig* config);
+
+    static std::shared_ptr<HCCLComm> createSubHcclComm(
+        std::shared_ptr<HCCLComm> comm,
+        uint32_t rankNum,
+        uint32_t *rankIds,
+        uint64_t subCommId,
+        uint32_t subCommRankId,
+        HcclCommConfig* config);
+
+    int hcclCommType;
+    int p2pPeer;
+
+    // Must not be copyable
+    HCCLComm(const HCCLComm&) = delete;
+    HCCLComm& operator=(const HCCLComm&) = delete;
+
+    // Move constructable
+    HCCLComm(HCCLComm&& other);
+
+    // Move assignable
+    HCCLComm& operator=(HCCLComm&& other);
+
+    HcclComm getHcclComm() const
+    {
+        return hcclComm_;
+    }
+
+    void destroyHcclComm();
+
+    HcclResult checkForHcclError();
+
+protected:
+    HcclComm hcclComm_;
+    mutable std::mutex mutex_;
+    HcclResult hcclAsyncErr_;
+};
+
+class TORCH_API DebugInfoWriter {
+public:
+    virtual ~DebugInfoWriter() = default;
+    virtual void write(const std::string &hcclTrace);
+    static DebugInfoWriter &getWriter(int rank);
+    static void registerWriter(std::unique_ptr<DebugInfoWriter> writer);
+    virtual std::string getWriterTarget()
+    {
+        return filename_;
+    }
+
+protected:
+    DebugInfoWriter(std::string namePrefix, int rank)
+    {
+        filename_ = c10::str(namePrefix, rank);
+    }
+    std::string filename_;
+
+private:
+    static std::unique_ptr<DebugInfoWriter> writer_;
+    static std::atomic<bool> hasWriterRegistered_;
+};
+} // namespace c10d_npu
---
+++ packages/torch-npu/torch_npu-2.1.0.post13-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/torch_npu/testing/__init__.py
@@ -0,0 +1,226 @@
+import json
+import copy
+import os
+import re
+import sys
+import unittest
+import warnings
+import inspect
+from typing import Any
+from torch.testing._internal import common_utils, common_device_type
+from torch.testing._internal.opinfo.core import OpInfo
+from torch.testing._internal.common_utils import remove_device_and_dtype_suffixes, TEST_WITH_SLOW, \
+        IS_SANDCASTLE, TEST_SKIP_FAST, RERUN_DISABLED_TESTS, DISABLED_TESTS_FILE, SLOW_TESTS_FILE, maybe_load_json, \
+        TEST_MPS, IS_FBCODE
+from torch.testing._internal.common_device_type import device_type_test_bases, MPSTestBase, \
+    filter_desired_device_types, LazyTestBase, PYTORCH_TESTING_DEVICE_ONLY_FOR_KEY, \
+    PYTORCH_TESTING_DEVICE_EXCEPT_FOR_KEY
+import torch_npu
+from torch_npu.testing._npu_testing_utils import update_skip_list, get_decorators
+
+__all__ = []
+
+
+def _get_tests_dict():
+
+    def _filter_json(data):
+        if _is_910A():
+            return {key: val for key, val in data.items() if len(val) > 1 and not(val[1] and "A2" in val[1])}
+        return {key: val for key, val in data.items() if len(val) > 1 and not(val[1] and "910A" in val[1])}
+
+    def _is_910A():
+        device_name = torch_npu.npu.get_device_name(0)
+        if "Ascend910A" in device_name or "Ascend910P" in device_name:
+            return True
+        return False
+
+    def _load_disabled_json(filename):
+        if os.path.isfile(filename):
+            with open(filename) as fp0:
+                disabled_dict = json.load(fp0, object_hook=_filter_json)
+
+                return disabled_dict
+        warnings.warn("Attempted to load json file '%s' but it does not exist.", filename)
+        return {}
+
+    # import test files
+    disabled_tests_dict = {}
+    slow_tests_dict = {}
+    # set them here in case the tests are running in a subprocess that doesn't call run_tests
+    if os.getenv("SLOW_TESTS_FILE", ""):
+        slow_tests_dict = maybe_load_json(os.getenv("SLOW_TESTS_FILE", ""))
+    if os.getenv("DISABLED_TESTS_FILE", ""):
+        disabled_tests_dict = _load_disabled_json(os.getenv("DISABLED_TESTS_FILE", ""))
+    if SLOW_TESTS_FILE:
+        if os.path.exists(SLOW_TESTS_FILE):
+            with open(SLOW_TESTS_FILE) as fp:
+                slow_tests_dict = json.load(fp)
+                # use env vars so pytest-xdist subprocesses can still access them
+                os.environ['SLOW_TESTS_FILE'] = SLOW_TESTS_FILE
+        else:
+            warnings.warn(f'slow test file provided but not found: {SLOW_TESTS_FILE}')
+
+    if DISABLED_TESTS_FILE:
+        if os.path.exists(DISABLED_TESTS_FILE):
+            disabled_tests_dict = _load_disabled_json(DISABLED_TESTS_FILE)
+            os.environ['DISABLED_TESTS_FILE'] = DISABLED_TESTS_FILE
+        else:
+            warnings.warn(f'disabled test file provided but not found: {DISABLED_TESTS_FILE}')
+
+    return disabled_tests_dict, slow_tests_dict
+
+
+def _check_if_enable_npu(test: unittest.TestCase):
+    disabled_tests_dict, slow_tests_dict = _get_tests_dict()
+
+    classname = str(test.__class__).split("'")[1].split(".")[-1]
+    sanitized_testname = remove_device_and_dtype_suffixes(test._testMethodName)
+
+    def matches_test(target: str):
+        target_test_parts = re.split(" (?=\\(__main__)", target) if "__main__" in target else target.split()
+        if len(target_test_parts) < 2:
+            # poorly formed target test name
+            return False
+        target_testname = target_test_parts[0]
+        target_classname = target_test_parts[1][1:-1].split(".")[-1]
+
+        if "_npu" in test._testMethodName:
+            testname_device_replace = test._testMethodName.replace("_npu", "_privateuse1")
+        elif "_privateuse1" in test._testMethodName:
+            testname_device_replace = test._testMethodName.replace("_privateuse1", "_npu")
+        else:
+            testname_device_replace = test._testMethodName
+
+        # if test method name or its sanitized version exactly matches the disabled
+        # test method name AND allow non-parametrized suite names to disable
+        # parametrized ones (TestSuite disables TestSuiteCPU)
+        return classname.startswith(target_classname) \
+                and (target_testname in (test._testMethodName, sanitized_testname, testname_device_replace))
+
+    if any(matches_test(x) for x in slow_tests_dict.keys()):
+        getattr(test, test._testMethodName).__dict__['slow_test'] = True
+        if not TEST_WITH_SLOW:
+            raise unittest.SkipTest("test is slow; run with PYTORCH_TEST_WITH_SLOW to enable test")
+
+    if not IS_SANDCASTLE:
+        should_skip = False
+        skip_msg = ""
+
+        for disabled_test, _ in disabled_tests_dict.items():
+            if matches_test(disabled_test):
+                should_skip = True
+                skip_msg = "this test is disabled now"
+                break
+
+        if should_skip and not RERUN_DISABLED_TESTS:
+            # Skip the disabled test when not running under --rerun-disabled-tests verification mode
+            raise unittest.SkipTest(skip_msg)
+
+        if not should_skip and RERUN_DISABLED_TESTS:
+            skip_msg = "Test is enabled but --rerun-disabled-tests verification mode is set, so only" \
+                " disabled tests are run"
+            raise unittest.SkipTest(skip_msg)
+
+    if TEST_SKIP_FAST:
+        if hasattr(test, test._testMethodName) and not getattr(test, test._testMethodName).__dict__.get('slow_test', False):
+            raise unittest.SkipTest("test is fast; we disabled it with PYTORCH_TEST_SKIP_FAST")
+
+
+def _instantiate_device_type_tests(generic_test_class, scope, except_for=None, only_for=None, include_lazy=False, allow_mps=False):
+    # Removes the generic test class from its enclosing scope so its tests
+    # are not discoverable.
+    del scope[generic_test_class.__name__]
+
+    # Creates an 'empty' version of the generic_test_class
+    # Note: we don't inherit from the generic_test_class directly because
+    #   that would add its tests to our test classes and they would be
+    #   discovered (despite not being runnable). Inherited methods also
+    #   can't be removed later, and we can't rely on load_tests because
+    #   pytest doesn't support it (as of this writing).
+    empty_name = generic_test_class.__name__ + "_base"
+    empty_class = type(empty_name, generic_test_class.__bases__, {})
+
+    # Acquires members names
+    # See Note [Overriding methods in generic tests]
+    generic_members = set(generic_test_class.__dict__.keys()) - set(empty_class.__dict__.keys())
+    generic_tests = [x for x in generic_members if x.startswith('test')]
+
+    # allow callers to specifically opt tests into being tested on MPS, similar to `include_lazy`
+    test_bases = device_type_test_bases.copy()
+    if allow_mps and TEST_MPS and MPSTestBase not in test_bases:
+        test_bases.append(MPSTestBase)
+    desired_device_type_test_bases = filter_desired_device_types(test_bases, except_for, only_for)
+    if include_lazy:
+        # Note [Lazy Tensor tests in device agnostic testing]
+        # Right now, test_view_ops.py runs with LazyTensor.
+        # We don't want to opt every device-agnostic test into using the lazy device,
+        # because many of them will fail.
+        # So instead, the only way to opt a specific device-agnostic test file into
+        # lazy tensor testing is with include_lazy=True
+        if IS_FBCODE:
+            print("TorchScript backend not yet supported in FBCODE/OVRSOURCE builds", file=sys.stderr)
+        else:
+            desired_device_type_test_bases.append(LazyTestBase)
+
+    def split_if_not_empty(x: str):
+        return x.split(",") if len(x) != 0 else []
+
+    # Filter out the device types based on environment variables if available
+    # Usage:
+    # export PYTORCH_TESTING_DEVICE_ONLY_FOR=cuda,cpu
+    # export PYTORCH_TESTING_DEVICE_EXCEPT_FOR=xla
+    env_only_for = split_if_not_empty(os.getenv(PYTORCH_TESTING_DEVICE_ONLY_FOR_KEY, ''))
+    env_except_for = split_if_not_empty(os.getenv(PYTORCH_TESTING_DEVICE_EXCEPT_FOR_KEY, ''))
+    if env_only_for:
+        desired_device_type_test_bases += filter(lambda x: x.device_type in env_only_for, test_bases)
+        desired_device_type_test_bases = list(set(desired_device_type_test_bases))
+    desired_device_type_test_bases = filter_desired_device_types(desired_device_type_test_bases,
+                                                                 env_except_for, env_only_for)
+
+    # Creates device-specific test cases
+    for base in desired_device_type_test_bases:
+        class_name = generic_test_class.__name__ + base.device_type.upper()
+
+        device_type_test_class: Any = type(class_name, (base, empty_class), {})
+
+        for name in generic_members:
+            if name in generic_tests:  # Instantiates test member
+                test = getattr(generic_test_class, name)
+                # XLA-compat shim (XLA's instantiate_test takes doesn't take generic_cls)
+                sig = inspect.signature(device_type_test_class.instantiate_test)
+                if len(sig.parameters) == 3:
+                    # Instantiates the device-specific tests
+                    device_type_test_class.instantiate_test(name, copy.deepcopy(test), generic_cls=generic_test_class)
+                else:
+                    device_type_test_class.instantiate_test(name, copy.deepcopy(test))
+            else:  # Ports non-test member
+                if name in device_type_test_class.__dict__:
+                    raise AssertionError(f"Redefinition of directly defined member {name}")
+                nontest = getattr(generic_test_class, name)
+                setattr(device_type_test_class, name, nontest)
+
+        # Mimics defining the instantiated class in the caller's file
+        # by setting its module to the given class's and adding
+        # the module to the given scope.
+        # This lets the instantiated class be discovered by unittest.
+        device_type_test_class.__module__ = generic_test_class.__module__
+        scope[class_name] = device_type_test_class
+
+
+def _test_for_npu():
+    os.environ['PYTORCH_TESTING_DEVICE_ONLY_FOR'] = 'privateuse1'
+    os.environ['PYTORCH_TESTING_DEVICE_EXCEPT_FOR'] = 'cuda,cpu'
+    common_device_type.onlyCUDA = common_device_type.onlyPRIVATEUSE1
+    common_utils.TEST_CUDA = common_utils.TEST_PRIVATEUSE1
+    common_device_type.instantiate_device_type_tests = _instantiate_device_type_tests
+
+
+def _apply_test_patchs():
+    update_skip_list()
+    OpInfo.get_decorators = get_decorators
+    common_utils.check_if_enable = _check_if_enable_npu
+
+
+#apply test_ops related patch
+_apply_test_patchs()
+_test_for_npu()
