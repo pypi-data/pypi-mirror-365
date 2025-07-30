9943ccaa7816fa77126a3489571ec575b60bbfb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:04:55 +0000

    Add package github-oss-contributions/0.3.1/github_oss_contributions-0.3.1.tar.gz

---
+++ packages/github-oss-contributions/github_oss_contributions-0.3.1.tar.gz/github_oss_contributions-0.3.1/github_oss_contributions/cli.py
@@ -0,0 +1,22 @@
+import argparse
+import asyncio
+from github_oss_contributions.main import GitHubOssContributions
+
+def main():
+    parser = argparse.ArgumentParser(description="Fetch and display your GitHub OSS contributions.")
+    parser.add_argument("--username", required=True, help="Your GitHub username")
+    parser.add_argument("--token", help="GitHub personal access token (optional, for higher rate limits)")
+    parser.add_argument("--raw", action="store_true", help="Print raw data instead of pretty output")
+    args = parser.parse_args()
+
+    client = GitHubOssContributions(username=args.username, token=args.token)
+    if args.raw:
+        org_data = client.get_contributions()
+        print(org_data)
+    else:
+        # For async pretty output
+        # asyncio.run(client.print_contributions()) # for async version of print_contributions
+        client.print_contributions()
+
+if __name__ == "__main__":
+    main()
---
+++ packages/github-oss-contributions/github_oss_contributions-0.3.1.tar.gz/github_oss_contributions-0.3.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    description = f.read()
+
+setup(
+    name="github_oss_contributions",
+    version="0.3.1",
+    description="Fetch organizations where a user created issues or PRs for Open Source",
+    packages=find_packages(),
+    author="Mohit Upadhyay and Anuj Kumar Upadhyay",
+    install_requires=[],
+    python_requires=">=3.7",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    entry_points={
+        "console_scripts": [
+            "github-oss-contributions=github_oss_contributions.cli:main"
+        ]
+    },
+)
