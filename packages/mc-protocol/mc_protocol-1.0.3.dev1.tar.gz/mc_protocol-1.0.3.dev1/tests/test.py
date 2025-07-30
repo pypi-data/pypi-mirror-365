4ae74dd04c0863c559be7ba8e375ad6d4ae0e784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 23:07:05 +0000

    Add package mc-protocol/1.0.3.dev1/mc_protocol-1.0.3.dev1.tar.gz

---
+++ packages/mc-protocol/mc_protocol-1.0.3.dev1.tar.gz/mc_protocol-1.0.3.dev1/tests/test.py
@@ -0,0 +1,21 @@
+
+from mc_protocol.network.game.packets.login.C2SLoginStartPacket import C2SLoginStartPacket
+from mc_protocol.network.game.packets.login.S2CEncryptionRequest import S2CEncryptionRequest
+from mc_protocol.network.game.packets.login.C2SEncryptionResponse import C2SEncryptionResponse
+import socket
+u = PlayerUtils.getOnlinePlayerUUIDFromMojangRest("wyh_")
+pinger = ModernPinger(765)
+pinger.setHost("cn-js-sq.wolfx.jp")
+pinger.setPort(25566)
+pinger.ping()
+protocol = pinger.getServerProtocol()
+with socket.create_connection(("cn-js-sq.wolfx.jp", 25566,), 5.0) as sock:
+    lsp = C2SLoginStartPacket("wyh_", u, protocol, 25566)
+    sock.send(lsp.getHandshake())
+    sock.send(lsp.getBytes())
+    er = sock.recv(4096)
+    s2cer = S2CEncryptionRequest(er)
+    c2ser= C2SEncryptionResponse(s2cer.getPublicKey(), s2cer.getVerifyToken())
+    sock.send(c2ser.getBytes())
+    print(c2ser.getEncryptor().deEncryptPacket(sock.recv(4096)))
+
---
+++ packages/mc-protocol/mc_protocol-1.0.3.dev1.tar.gz/mc_protocol-1.0.3.dev1/utils/player_utils.py
@@ -0,0 +1,45 @@
+from uuid import uuid3, NAMESPACE_OID, UUID
+import requests
+uuid_api = \
+{
+    "MOJANG-REST": "https://api.mojang.com/users/profiles/minecraft/{name}",
+
+}
+avatar_api = \
+{
+    "MINOTAR" : "https://minotar.net/avatar/{identifier}/{size}.png"
+
+}
+
+
+class PlayerUtils:
+    def __init__(self):
+        pass
+    @staticmethod
+    def getOfflinePlayerUUID(playerID: str):
+        return uuid3(NAMESPACE_OID, playerID)
+    @staticmethod
+    def getOnlinePlayerUUIDFromMojangRest(username: str):
+        try:
+            
+            url = uuid_api["MOJANG-REST"].replace("{name}", username)
+            response = requests.get(url)
+            if response.status_code != 200: return None
+            return str(UUID(response.json().get('id')))
+        except Exception as e:
+            print(e)
+            return None
+    @staticmethod
+    def getPlayerAvatarFromMinotar(id: str, size: int=64, savePath:str=None):
+        """id could be name or uuid"""
+        url = avatar_api['MINOTAR'].replace("{identifier}", id).replace("{size}", str(size))
+        try:
+            response = requests.get(url)
+            if savePath:
+                with open(savePath if savePath.endswith(".png") else savePath + ".png", "bw") as f:
+                    f.write(response.content)
+            return response.content if response.status_code == 200 else response.status_code
+        except Exception as e:
+            print(e)
+            return None
+
