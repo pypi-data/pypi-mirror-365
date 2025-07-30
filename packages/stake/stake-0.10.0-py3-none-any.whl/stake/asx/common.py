673c28c1d04f1ad27c6f1f1ca095292ddf94e24e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:24 +0000

    Add package stake/0.10.0/stake-0.10.0-py3-none-any.whl

---
+++ packages/stake/stake-0.10.0-py3-none-any.whl/stake/asx/common.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class Side(str, Enum):
+    BUY = "BUY"
+    SELL = "SELL"
+
+
+class TradeType(str, Enum):
+    """The type of trade the user is requesting."""
+
+    MARKET = "MARKET_TO_LIMIT"
+    LIMIT = "LIMIT"
