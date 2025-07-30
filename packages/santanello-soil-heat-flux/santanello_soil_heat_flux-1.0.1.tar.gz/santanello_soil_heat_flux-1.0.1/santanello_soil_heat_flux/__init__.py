2bc2292d104a935a7c1108bee1c9070b8bffab62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:47:01 +0000

    Add package santanello-soil-heat-flux/1.0.1/santanello_soil_heat_flux-1.0.1.tar.gz

---
+++ packages/santanello-soil-heat-flux/santanello_soil_heat_flux-1.0.1.tar.gz/santanello_soil_heat_flux-1.0.1/santanello_soil_heat_flux/__init__.py
@@ -0,0 +1,3 @@
+from .santanello_soil_heat_flux import *
+
+__version__ = "1.0.1"
---
+++ packages/santanello-soil-heat-flux/santanello_soil_heat_flux-1.0.1.tar.gz/santanello_soil_heat_flux-1.0.1/tests/test_import_SEBAL_soil_heat_flux.py
@@ -0,0 +1,2 @@
+def test_import_santanello_soil_heat_flux():
+    from santanello_soil_heat_flux import santanello_soil_heat_flux
