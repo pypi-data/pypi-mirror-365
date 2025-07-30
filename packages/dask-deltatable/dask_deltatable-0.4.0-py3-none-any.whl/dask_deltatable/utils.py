61560db08bb045a95110d0321790a56e2d8548eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:06 +0000

    Add package dask-deltatable/0.4.0/dask_deltatable-0.4.0-py3-none-any.whl

---
+++ packages/dask-deltatable/dask_deltatable-0.4.0-py3-none-any.whl/dask_deltatable/utils.py
@@ -0,0 +1,300 @@
+from __future__ import annotations
+
+import json
+from collections.abc import Iterator, Mapping
+from datetime import date, datetime
+from decimal import Decimal
+from math import inf
+from typing import Any, cast
+from urllib.parse import unquote
+
+from deltalake import DeltaTable
+
+from .types import Filter, Filters
+
+
+def get_bucket_region(path: str):
+    import boto3
+
+    if not path.startswith("s3://"):
+        raise ValueError(f"'{path}' is not an S3 path")
+    bucket = path.replace("s3://", "").split("/")[0]
+    resp = boto3.client("s3").get_bucket_location(Bucket=bucket)
+    # Buckets in region 'us-east-1' results in None, b/c why not.
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/client/get_bucket_location.html#S3.Client.get_bucket_location
+    return resp["LocationConstraint"] or "us-east-1"
+
+
+def maybe_set_aws_credentials(path: Any, options: dict[str, Any]) -> dict[str, Any]:
+    """
+    Maybe set AWS credentials into ``options`` if existing AWS specific keys
+    not found in it and path is s3:// format.
+
+    Parameters
+    ----------
+    path : Any
+        If it's a string, we'll check if it starts with 's3://' then determine bucket
+        region if the AWS credentials should be set.
+    options : dict[str, Any]
+        Options, any kwargs to be supplied to things like S3FileSystem or similar
+        that may accept AWS credentials set. A copy is made and returned if modified.
+
+    Returns
+    -------
+    dict
+        Either the original options if not modified, or a copied and updated options
+        with AWS credentials inserted.
+    """
+
+    is_s3_path = getattr(path, "startswith", lambda _: False)("s3://")
+    if not is_s3_path:
+        return options
+
+    # Avoid overwriting already provided credentials
+    keys = ("AWS_ACCESS_KEY", "AWS_SECRET_ACCESS_KEY", "access_key", "secret_key")
+    if not any(k in (options or ()) for k in keys):
+        # defers installing boto3 upfront, xref _read_from_catalog
+        import boto3
+
+        session = boto3.session.Session()
+        credentials = session.get_credentials()
+        if credentials is None:
+            return options
+        region = get_bucket_region(path)
+
+        options = (options or {}).copy()
+        options.update(
+            # Capitalized is used in delta specific API and lowercase is for S3FileSystem
+            dict(
+                # TODO: w/o this, we need to configure a LockClient which seems to require dynamodb.
+                AWS_S3_ALLOW_UNSAFE_RENAME="true",
+                AWS_SECRET_ACCESS_KEY=credentials.secret_key,
+                AWS_ACCESS_KEY_ID=credentials.access_key,
+                AWS_SESSION_TOKEN=credentials.token,
+                AWS_REGION=region,
+                secret_key=credentials.secret_key,
+                access_key=credentials.access_key,
+                token=credentials.token,
+                region=region,
+            )
+        )
+    return options
+
+
+def get_partition_filters(
+    partition_columns: list[str], filters: Filters
+) -> list[list[Filter]] | None:
+    """Retrieve only filters on partition columns. If there are any row filters in the outer
+    list (the OR list), return None, because we have to search through all partitions to apply
+    row filters
+
+    Parameters
+    ----------
+    partition_columns : List[str]
+        List of partitioned columns
+
+    filters : List[Tuple[str, str, Any]] | List[List[Tuple[str, str, Any]]]
+        List of filters. Examples:
+        1) (x == a) and (y == 3):
+           [("x", "==", "a"), ("y", "==", 3)]
+        2) (x == a) or (y == 3)
+            [[("x", "==", "a")], [("y", "==", 3)]]
+
+    Returns
+    -------
+    List[List[Tuple[str, str, Any]]] | None
+        List of partition filters, None if we can't apply a filter on partitions because
+        row filters are present
+    """
+    if filters is None or len(filters) == 0:
+        return None
+
+    if isinstance(filters[0][0], str):
+        filters = cast(list[list[Filter]], [filters])
+    filters = cast(list[list[Filter]], filters)
+
+    allowed_ops = {
+        "=": "=",
+        "==": "=",
+        "!=": "!=",
+        "!==": "!=",
+        "in": "in",
+        "not in": "not in",
+        ">": ">",
+        "<": "<",
+        ">=": ">=",
+        "<=": "<=",
+    }
+
+    expressions = []
+    for disjunction in filters:
+        inner_expressions = []
+        for col, op, val in disjunction:
+            if col in partition_columns:
+                normalized_op = allowed_ops[op]
+                inner_expressions.append((col, normalized_op, val))
+        if inner_expressions:
+            expressions.append(inner_expressions)
+        else:
+            return None
+
+    return expressions if expressions else None
+
+
+# Copied from delta-rs v0.25.5 (https://github.com/delta-io/delta-rs/blob/python-v0.25.5/LICENSE.txt)
+def get_partitions_from_path(path: str) -> tuple[str, dict[str, str | None]]:
+    if path[0] == "/":
+        path = path[1:]
+    parts = path.split("/")
+    parts.pop()  # remove filename
+    out: dict[str, str | None] = {}
+    for part in parts:
+        if part == "":
+            continue
+        key, value = part.split("=", maxsplit=1)
+        if value == "__HIVE_DEFAULT_PARTITION__":
+            out[key] = None
+        else:
+            out[key] = unquote(value)
+    return path, out
+
+
+# Copied from delta-rs v0.25.5 (https://github.com/delta-io/delta-rs/blob/python-v0.25.5/LICENSE.txt)
+def get_file_stats_from_metadata(
+    metadata: Any,
+    num_indexed_cols: int,
+    columns_to_collect_stats: list[str] | None,
+) -> dict[str, int | dict[str, Any]]:
+    """Get Delta's file stats from PyArrow's Parquet file metadata."""
+    stats = {
+        "numRecords": metadata.num_rows,
+        "minValues": {},
+        "maxValues": {},
+        "nullCount": {},
+    }
+
+    def iter_groups(metadata: Any) -> Iterator[Any]:
+        for i in range(metadata.num_row_groups):
+            if metadata.row_group(i).num_rows > 0:
+                yield metadata.row_group(i)
+
+    schema_columns = metadata.schema.names
+    if columns_to_collect_stats is not None:
+        idx_to_iterate = []
+        for col in columns_to_collect_stats:
+            try:
+                idx_to_iterate.append(schema_columns.index(col))
+            except ValueError:
+                pass
+    elif num_indexed_cols == -1:
+        idx_to_iterate = list(range(metadata.num_columns))
+    elif num_indexed_cols >= 0:
+        idx_to_iterate = list(range(min(num_indexed_cols, metadata.num_columns)))
+    else:
+        raise ValueError("delta.dataSkippingNumIndexedCols valid values are >=-1")
+
+    for column_idx in idx_to_iterate:
+        name = metadata.row_group(0).column(column_idx).path_in_schema
+
+        # If stats missing, then we can't know aggregate stats
+        if all(
+            group.column(column_idx).is_stats_set for group in iter_groups(metadata)
+        ):
+            stats["nullCount"][name] = sum(
+                group.column(column_idx).statistics.null_count
+                for group in iter_groups(metadata)
+            )
+
+            # Min / max may not exist for some column types, or if all values are null
+            if any(
+                group.column(column_idx).statistics.has_min_max
+                for group in iter_groups(metadata)
+            ):
+                # Min and Max are recorded in physical type, not logical type
+                # https://stackoverflow.com/questions/66753485/decoding-parquet-min-max-statistics-for-decimal-type
+                # TODO: Add logic to decode physical type for DATE, DECIMAL
+
+                minimums = (
+                    group.column(column_idx).statistics.min
+                    for group in iter_groups(metadata)
+                )
+                # If some row groups have all null values, their min and max will be null too.
+                min_value = min(minimum for minimum in minimums if minimum is not None)
+                # Infinity cannot be serialized to JSON, so we skip it. Saying
+                # min/max is infinity is equivalent to saying it is null, anyways.
+                if min_value != -inf:
+                    stats["minValues"][name] = min_value
+                maximums = (
+                    group.column(column_idx).statistics.max
+                    for group in iter_groups(metadata)
+                )
+                max_value = max(maximum for maximum in maximums if maximum is not None)
+                if max_value != inf:
+                    stats["maxValues"][name] = max_value
+    return stats
+
+
+# Copied from delta-rs v0.25.5 (https://github.com/delta-io/delta-rs/blob/python-v0.25.5/LICENSE.txt)
+class DeltaJSONEncoder(json.JSONEncoder):
+    def default(self, obj: Any) -> Any:
+        if isinstance(obj, bytes):
+            return obj.decode("unicode_escape", "backslashreplace")
+        elif isinstance(obj, date):
+            return obj.isoformat()
+        elif isinstance(obj, datetime):
+            return obj.isoformat()
+        elif isinstance(obj, Decimal):
+            return str(obj)
+        # Let the base class default method raise the TypeError
+        return json.JSONEncoder.default(self, obj)
+
+
+# Copied from delta-rs v0.25.5 (https://github.com/delta-io/delta-rs/blob/python-v0.25.5/LICENSE.txt)
+def enforce_append_only(
+    table: DeltaTable | None,
+    configuration: Mapping[str, str | None] | None,
+    mode: str,
+) -> None:
+    """Throw ValueError if table configuration contains delta.appendOnly and mode is not append"""
+    if table:
+        configuration = table.metadata().configuration
+    config_delta_append_only = (
+        configuration and configuration.get("delta.appendOnly", "false") == "true"
+    )
+    if config_delta_append_only and mode != "append":
+        raise ValueError(
+            "If configuration has delta.appendOnly = 'true', mode must be 'append'."
+            f" Mode is currently {mode}"
+        )
+
+
+# Inspired from delta-rs v0.25.5 (https://github.com/delta-io/delta-rs/blob/python-v0.25.5/LICENSE.txt)
+def get_num_idx_cols_and_stats_columns(
+    table: DeltaTable | None, configuration: Mapping[str, str | None] | None
+) -> tuple[int, list[str] | None]:
+    """Get the num_idx_columns and stats_columns from the table configuration in the state
+
+    If table does not exist (only can occur in the first write action) it takes
+    the configuration that was passed.
+    """
+    if table is not None:
+        configuration = table.metadata().configuration
+    if configuration is None:
+        num_idx_cols = -1
+        stats_columns = None
+    else:
+        # Parse configuration
+        dataSkippingNumIndexedCols = configuration.get(
+            "delta.dataSkippingNumIndexedCols", "-1"
+        )
+        num_idx_cols = (
+            int(dataSkippingNumIndexedCols)
+            if dataSkippingNumIndexedCols is not None
+            else -1
+        )
+        columns = configuration.get("delta.dataSkippingStatsColumns", None)
+        if columns is not None:
+            stats_columns = [col.strip() for col in columns.split(",")]
+        else:
+            stats_columns = None
+    return num_idx_cols, stats_columns
---
+++ packages/dask-deltatable/dask_deltatable-0.4.0-py3-none-any.whl/dask_deltatable/write.py
@@ -0,0 +1,377 @@
+from __future__ import annotations
+
+import json
+import uuid
+from collections.abc import Mapping
+from datetime import datetime
+from pathlib import Path
+from typing import Any, Literal
+
+import dask
+import dask.dataframe as dd
+import pyarrow as pa
+import pyarrow.dataset as ds
+import pyarrow.fs as pa_fs
+from dask.core import flatten
+from deltalake import CommitProperties, DeltaTable
+from deltalake import Schema as DeltaSchema
+from deltalake.exceptions import DeltaProtocolError
+from deltalake.fs import DeltaStorageHandler
+from deltalake.table import MAX_SUPPORTED_PYARROW_WRITER_VERSION
+from deltalake.transaction import AddAction, create_table_with_add_actions
+from deltalake.writer.writer import try_get_table_and_table_uri
+from toolz.itertoolz import pluck
+
+from . import utils
+from ._schema import pyarrow_to_deltalake, validate_compatible
+
+PYARROW_MAJOR_VERSION = int(pa.__version__.split(".")[0])
+
+
+def to_deltalake(
+    table_or_uri: str | Path | DeltaTable,
+    df: dd.DataFrame,
+    *,
+    schema: pa.Schema | None = None,
+    partition_by: list[str] | str | None = None,
+    filesystem: pa_fs.FileSystem | None = None,
+    mode: Literal["error", "append", "overwrite", "ignore"] = "error",
+    file_options: Mapping[str, Any] | None = None,
+    max_partitions: int | None = None,
+    max_open_files: int = 1024,
+    max_rows_per_file: int = 10 * 1024 * 1024,
+    min_rows_per_group: int = 64 * 1024,
+    max_rows_per_group: int = 128 * 1024,
+    name: str | None = None,
+    description: str | None = None,
+    configuration: Mapping[str, str | None] | None = None,
+    overwrite_schema: bool = False,
+    storage_options: dict[str, str] | None = None,
+    partition_filters: list[tuple[str, str, Any]] | None = None,
+    compute: bool = True,
+    custom_metadata: dict[str, str] | None = None,
+):
+    """Write a given dask.DataFrame to a delta table. The returned value is a Dask Scalar,
+    and the writing operation is only triggered when calling ``.compute()``
+
+    Parameters
+    ----------
+    table_or_uri: str | Path | DeltaTable
+        URI of a table or a DeltaTable object.
+    df: dd.DataFrame
+        Data to write
+    schema : pa.Schema | None. Default None
+        Optional schema to write.
+    partition_by : list[str] | str | None. Default None
+        List of columns to partition the table by. Only required
+        when creating a new table
+    filesystem : pa_fs.FileSystem | None. Default None
+        Optional filesystem to pass to PyArrow. If not provided will
+        be inferred from uri. The file system has to be rooted in the table root.
+        Use the pyarrow.fs.SubTreeFileSystem, to adopt the root of pyarrow file systems.
+    mode : Literal["error", "append", "overwrite", "ignore"]. Default "error"
+        How to handle existing data. Default is to error if table already exists.
+        If 'append', will add new data.
+        If 'overwrite', will replace table with new data.
+        If 'ignore', will not write anything if table already exists.
+    file_options : Mapping[str, Any] | None. Default None
+        Optional dict of options that can be used to initialize ParquetFileWriteOptions.
+        Please refer to https://github.com/apache/arrow/blob/master/python/pyarrow/_dataset_parquet.pyx
+        for the list of available options
+    max_partitions : int | None. Default None
+        The maximum number of partitions that will be used.
+    max_open_files : int. Default 1024
+        Limits the maximum number of
+        files that can be left open while writing. If an attempt is made to open
+        too many files then the least recently used file will be closed.
+        If this setting is set too low you may end up fragmenting your
+        data into many small files.
+    max_rows_per_file : int. Default 10 * 1024 * 1024
+        Maximum number of rows per file.
+        If greater than 0 then this will limit how many rows are placed in any single file.
+        Otherwise there will be no limit and one file will be created in each output directory
+        unless files need to be closed to respect max_open_files
+    min_rows_per_group : int. Default 64 * 1024
+        Minimum number of rows per group. When the value is set,
+        the dataset writer will batch incoming data and only write the row groups to the disk
+        when sufficient rows have accumulated.
+    max_rows_per_group : int. Default 128 * 1024
+        Maximum number of rows per group.
+        If the value is set, then the dataset writer may split up large incoming batches into multiple row groups.
+        If this value is set, then min_rows_per_group should also be set
+    name: str | None. Default None
+        User-provided identifier for this table.
+    description : str | None. Default None
+        User-provided description for this table
+    configuration : Mapping[str, str | None] | None. Default None
+        A map containing configuration options for the metadata action.
+    overwrite_schema : bool. Default False
+        If True, allows updating the schema of the table.
+    storage_options : dict[str, str] | None. Default None
+        Options passed to the native delta filesystem. Unused if 'filesystem' is defined
+    partition_filters : list[tuple[str, str, Any]] | None. Default None
+        The partition filters that will be used for partition overwrite.
+    compute : bool. Default True
+        Whether to trigger the writing operation immediately
+
+    Returns
+    -------
+    dask.Scalar
+    """
+    storage_options = utils.maybe_set_aws_credentials(table_or_uri, storage_options)  # type: ignore
+    table, table_uri = try_get_table_and_table_uri(table_or_uri, storage_options)
+
+    # We need to write against the latest table version
+    if table:
+        table.update_incremental()
+
+    utils.enforce_append_only(table=table, configuration=configuration, mode=mode)
+
+    if filesystem is None:
+        if table is not None:
+            storage_options = table._storage_options or {}
+            storage_options.update(storage_options or {})
+
+        storage_options = utils.maybe_set_aws_credentials(table_uri, storage_options)
+        filesystem = pa_fs.PyFileSystem(DeltaStorageHandler(table_uri, storage_options))
+
+    if isinstance(partition_by, str):
+        partition_by = [partition_by]
+
+    if schema is not None:
+        schema = pyarrow_to_deltalake(schema)
+
+    if table:  # already exists
+        if (
+            schema is not None
+            and schema != pa.schema(table.schema())
+            and not (mode == "overwrite" and overwrite_schema)
+        ):
+            raise ValueError(
+                "Schema of data does not match table schema\n"
+                f"Table schema:\n{schema}\nData Schema:\n{table.schema().to_arrow()}"
+            )
+
+        if mode == "error":
+            raise AssertionError("DeltaTable already exists.")
+        elif mode == "ignore":
+            return
+
+        current_version = table.version()
+
+        if partition_by:
+            assert partition_by == table.metadata().partition_columns
+        else:
+            partition_by = table.metadata().partition_columns
+
+        if table.protocol().min_writer_version > MAX_SUPPORTED_PYARROW_WRITER_VERSION:
+            raise DeltaProtocolError(
+                "This table's min_writer_version is "
+                f"{table.protocol().min_writer_version}, "
+                f"but this method only supports version {MAX_SUPPORTED_PYARROW_WRITER_VERSION}."
+            )
+    else:  # creating a new table
+        current_version = -1
+
+    # FIXME: schema is only known at this point if provided by the user
+    if partition_by and schema:
+        partition_schema = pa.schema([schema.field(name) for name in partition_by])
+        partitioning = ds.partitioning(partition_schema, flavor="hive")
+    else:
+        if partition_by:
+            raise NotImplementedError("Have to provide schema when using partition_by")
+        partitioning = None
+    if mode == "overwrite":
+        # FIXME: There are a couple of checks that are not migrated yet
+        raise NotImplementedError("mode='overwrite' is not implemented")
+    written = df.map_partitions(
+        _write_partition,
+        schema=schema,
+        partitioning=partitioning,
+        current_version=current_version,
+        file_options=file_options,
+        max_open_files=max_open_files,
+        max_rows_per_file=max_rows_per_file,
+        min_rows_per_group=min_rows_per_group,
+        max_rows_per_group=max_rows_per_group,
+        filesystem=filesystem,
+        max_partitions=max_partitions,
+        meta=(None, object),
+        table=DaskDeltaTable.from_delta_table(table) if table else None,
+        configuration=configuration,
+    )
+    result = dask.delayed(_commit, name="deltatable-commit")(
+        DaskDeltaTable.from_delta_table(table) if table else None,
+        written,
+        table_uri,
+        schema,
+        mode,
+        partition_by,
+        name,
+        description,
+        configuration,
+        storage_options,
+        partition_filters,
+        custom_metadata,
+    )
+
+    if compute:
+        result = result.compute()
+    return result
+
+
+def _commit(
+    table,
+    schemas_add_actions_nested,
+    table_uri,
+    schema,
+    mode,
+    partition_by,
+    name,
+    description,
+    configuration,
+    storage_options,
+    partition_filters,
+    custom_metadata,
+):
+    schemas = list(flatten(pluck(0, schemas_add_actions_nested)))
+    add_actions = list(flatten(pluck(1, schemas_add_actions_nested)))
+    # TODO: What should the behavior be if the schema is provided? Cast the
+    # data?
+    if schema:
+        schemas.append(schema)
+
+    # TODO: This is applying a potentially stricter schema control than what
+    # Delta requires but if this passes, it should be good to go
+    schema = validate_compatible(schemas)
+    assert schema
+    delta_schema = DeltaSchema.from_arrow(schema)
+    commit_properties = CommitProperties(custom_metadata=custom_metadata)
+    if table is None:
+        storage_options = utils.maybe_set_aws_credentials(table_uri, storage_options)
+        create_table_with_add_actions(
+            table_uri,
+            delta_schema,
+            add_actions,
+            mode,
+            partition_by or [],
+            name,
+            description,
+            configuration,
+            storage_options,
+            commit_properties,
+        )
+    else:
+        table._table.create_write_transaction(
+            add_actions,
+            mode,
+            partition_by or [],
+            delta_schema,
+            partition_filters,
+        )
+        table.update_incremental()
+
+
+def _write_partition(
+    df,
+    *,
+    schema,
+    partitioning,
+    current_version,
+    file_options,
+    max_open_files,
+    max_rows_per_file,
+    min_rows_per_group,
+    max_rows_per_group,
+    filesystem,
+    max_partitions,
+    table,
+    configuration,
+) -> tuple[pa.Schema, list[AddAction]]:
+    if schema is None:
+        #
+        schema = pyarrow_to_deltalake(pa.Schema.from_pandas(df))
+    data = pa.Table.from_pandas(df, schema=schema)
+
+    add_actions: list[AddAction] = []
+
+    def visitor(written_file: Any) -> None:
+        num_indexed_cols, stats_cols = utils.get_num_idx_cols_and_stats_columns(
+            table if table is not None else None, configuration
+        )
+        path, partition_values = utils.get_partitions_from_path(written_file.path)
+        stats = utils.get_file_stats_from_metadata(
+            written_file.metadata, num_indexed_cols, stats_cols
+        )
+
+        # PyArrow added support for written_file.size in 9.0.0
+        if PYARROW_MAJOR_VERSION >= 9:
+            size = written_file.size
+        else:
+            size = filesystem.get_file_info([path])[0].size
+
+        add_actions.append(
+            AddAction(
+                path,
+                size,
+                partition_values,
+                int(datetime.now().timestamp() * 1000),
+                True,
+                json.dumps(stats, cls=utils.DeltaJSONEncoder),
+            )
+        )
+
+    if file_options is not None:
+        file_options = ds.ParquetFileFormat().make_write_options(**file_options)
+
+    ds.write_dataset(
+        data,
+        base_dir="/",
+        basename_template=f"{current_version + 1}-{uuid.uuid4()}-{{i}}.parquet",
+        format="parquet",
+        partitioning=partitioning,
+        # It will not accept a schema if using a RBR
+        schema=schema,
+        existing_data_behavior="overwrite_or_ignore",
+        file_options=file_options,
+        max_open_files=max_open_files,
+        file_visitor=visitor,
+        max_rows_per_file=max_rows_per_file,
+        min_rows_per_group=min_rows_per_group,
+        max_rows_per_group=max_rows_per_group,
+        filesystem=filesystem,
+        max_partitions=max_partitions,
+    )
+    return schema, add_actions
+
+
+class DaskDeltaTable(DeltaTable):
+    @classmethod
+    def from_delta_table(
+        cls,
+        table: DeltaTable,
+    ) -> DaskDeltaTable:
+        config = table.table_config
+        return cls(
+            table_uri=table.table_uri,
+            version=table.version(),
+            storage_options=table._storage_options,
+            without_files=config.without_files,
+            log_buffer_size=config.log_buffer_size,
+        )
+
+    def __reduce__(self) -> tuple[type, tuple[Any, ...]]:
+        """
+        This allows DeltaTable to be pickled.
+        """
+        config = self.table_config
+        return (
+            self.__class__,
+            (
+                self.table_uri,
+                self.version(),
+                self._storage_options,
+                config.without_files,
+                config.log_buffer_size,
+            ),
+        )
