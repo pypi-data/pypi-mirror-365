385eac615db2f02f1a57815cce12cb9a0851938b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:19 +0000

    Add package attack-chain-server/1.0/attack_chain_server-1.0.tar.gz

---
+++ packages/attack-chain-server/attack_chain_server-1.0.tar.gz/attack_chain_server-1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+ 
+setup(
+    name='attack_chain_server',
+    version='1.0',
+    py_modules=['attack_chain_server',],
+    author='0xng',
+    author_email='anquan@163.com',
+    description='test',
+    long_description='test',
+    long_description_content_type='text/markdown',
+    url='',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
