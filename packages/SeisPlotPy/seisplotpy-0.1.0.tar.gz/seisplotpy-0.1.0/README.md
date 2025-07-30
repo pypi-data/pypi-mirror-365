a5b9108d3837655f3242dd21b566bce8b3460c18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:05:32 +0000

    Add package SeisPlotPy/0.1.0/seisplotpy-0.1.0.tar.gz

---
+++ packages/SeisPlotPy/seisplotpy-0.1.0.tar.gz/seisplotpy-0.1.0/README.md
@@ -0,0 +1,33 @@
+# SeisPlotPy
+A GUI tool to generate publishing quality images from SEG-Y seismic data with horizons/Faults.
+
+## Installation
+
+You can install SeisPlotPy using pip:
+
+```bash
+pip install SeisPlotPy
+```bash
+
+Alternatively, clone the repository and install it locally:
+
+```bash
+git clone https://github.com/arjun-vh/SeisPlotPy.git
+cd SeisPlotPy
+pip install -e .
+```bash
+
+##Usage
+To launch the GUI, run:
+
+```bash
+seisplotpy
+```bash
+
+##Citation
+
+If you use SeisPlotPy in your research or work, please acknowledge
+
+##License
+This project is licensed under the MIT License - see the LICENSE file for details.
+
---
+++ packages/SeisPlotPy/seisplotpy-0.1.0.tar.gz/seisplotpy-0.1.0/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 28 17:03:42 2025
+
+@author: Admin
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="SeisPlotPy",
+    version="0.1.0",
+    py_modules=["seisplotpy"],  # Changed from packages=find_packages()
+    install_requires=[
+        "segyio>=1.9.0",
+        "numpy>=1.21.0",
+        "matplotlib>=3.5.0",
+        "pandas>=1.3.0"
+    ],
+    entry_points={
+        "console_scripts": [
+            "seisplotpy=seisplotpy:SeismicViewerApp.main"  # Updated module name
+        ]
+    },
+    author="Arjun VH",
+    author_email="your.email@example.com",  # Replace with your email
+    description="A GUI tool to view and export SEG-Y seismic data.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/arjun-vh/SeisPlotPy",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
