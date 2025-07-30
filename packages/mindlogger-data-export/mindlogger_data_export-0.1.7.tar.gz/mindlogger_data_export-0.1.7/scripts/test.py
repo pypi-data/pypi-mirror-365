39d5490294399355cc7b33fb8d44ec99b218fe0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:50 +0000

    Add package mindlogger-data-export/0.1.7/mindlogger_data_export-0.1.7.tar.gz

---
+++ packages/mindlogger-data-export/mindlogger_data_export-0.1.7.tar.gz/mindlogger_data_export-0.1.7/scripts/test.py
@@ -0,0 +1,23 @@
+import marimo
+
+__generated_with = "0.14.10"
+app = marimo.App(width="full")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    import polars as pl
+    from pathlib import Path
+    return Path, pl
+
+
+@app.cell
+def _(Path, pl):
+    _df = pl.read_parquet(Path("data/hbn_ods/output/I2B2_Export_IUQ_P.parquet"))
+    _df
+    return
+
+
+if __name__ == "__main__":
+    app.run()
