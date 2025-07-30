c2e9134a6b674e84995eda196090c58f650d7fc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:22 +0000

    Add package befordata/0.3.1/befordata-0.3.1.tar.gz

---
+++ packages/befordata/befordata-0.3.1.tar.gz/befordata-0.3.1/befordata/_epochs.py
@@ -0,0 +1,218 @@
+"""Epochs Data Structure"""
+
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from typing import Tuple
+
+import numpy as np
+import pandas as pd
+import pyarrow as pa
+from numpy.typing import NDArray
+
+from .misc import try_num
+
+BSL_COL_NAME = '__befor_baseline__'
+
+@dataclass
+class BeForEpochs:
+    """Behavioural force data organized epoch-wis
+
+    Args
+    ----
+    dat: : 2d numpy array
+        data. Each row of the 2D numpy array represents one epoch. Thus, the number
+        of rows equals the number of epochs and number of columns equals the number
+        of samples in each epoch.
+
+    sample_rate: float
+        sampling rate of the force measurements
+
+    design : pd.DataFrame
+        design data frame
+
+    baseline : numpy array
+        baseline for each epoch at `zero_sample`
+
+    zero_sample : int, optional
+        sample index that represents the time 0
+
+    """
+
+    dat: NDArray[np.floating]
+    sampling_rate: float
+    design: pd.DataFrame = field(default_factory=pd.DataFrame())  # type: ignore
+    baseline: NDArray[np.float64] = field(
+        default_factory=lambda: np.array([], dtype=np.float64)
+    )
+    zero_sample: int = 0
+
+    def __post_init__(self):
+        self.dat = np.atleast_2d(self.dat)
+        if self.dat.ndim != 2:
+            raise ValueError("Epoch data but be a 2D numpy array")
+
+        ne = self.n_epochs()
+        if self.design.shape[0] > 0 and self.design.shape[0] != ne:
+            raise ValueError("Epoch data and design must have the same number of rows")
+
+        self.baseline = np.atleast_1d(self.baseline)
+        if self.baseline.ndim != 1:
+            raise ValueError("Baseline must be a 1D array.")
+        if len(self.baseline) > 0 and len(self.baseline) != ne:
+            raise ValueError(
+                "If baseline is not empty, the number of elements must match number of epochs."
+            )
+
+    def __repr__(self):
+        rtn = "BeForEpochs"
+        rtn += f"\n  n epochs: {self.n_epochs()}"
+        rtn += f", n_samples: {self.n_samples()}"
+        rtn += f"\n  sampling_rate: {self.sampling_rate}"
+        rtn += f", zero_sample: {self.zero_sample}"
+        if len(self.design) == 0:
+            rtn += "\n  design: None"
+        else:
+            rtn += f"\n  design: {list(self.design.columns)}".replace("[", "").replace(
+                "]", ""
+            )
+        # rtn += "\n" + str(self.dat)
+        return rtn
+
+    def n_epochs(self) -> int:
+        """number of epochs"""
+        return self.dat.shape[0]
+
+    def n_samples(self) -> int:
+        """number of sample of one epoch"""
+        return self.dat.shape[1]
+
+    def append(self, other: BeForEpochs):
+        """Append epochs to the data structure"""
+
+        if other.n_samples() != self.n_samples():
+            raise ValueError("Number of samples per epoch are not the same")
+        if other.sampling_rate != self.sampling_rate:
+            raise ValueError("Sampling rates are not the same.")
+        if other.zero_sample != self.zero_sample:
+            raise ValueError("Zero samples are not the same.")
+        if other.is_baseline_adjusted() != self.is_baseline_adjusted():
+            raise ValueError("One data structure is baseline adjusted, the other not.")
+        if np.any(other.design.columns != self.design.columns):
+            raise ValueError("Design column names are not the same.")
+
+        self.dat = np.concat([self.dat, other.dat], axis=0)
+        self.design = pd.concat([self.design, other.design], axis=0)
+        self.baseline = np.append(self.baseline, other.baseline)
+
+    def is_baseline_adjusted(self):
+        """Returns true if data is baseline adjusted"""
+        return len(self.baseline) > 0
+
+    def adjust_baseline(self, reference_window: Tuple[int, int]):
+        """Adjust the baseline of each epoch using the mean value of
+        a defined range of sample (reference window)
+
+        Parameters
+        ----------
+        reference_window : Tuple[int, int]
+            sample range that is used for the baseline adjustment
+
+        """
+
+        if self.is_baseline_adjusted():
+            dat = self.dat + np.atleast_2d(self.baseline).T  # rest baseline
+        else:
+            dat = self.dat
+        i = range(reference_window[0], reference_window[1])
+        self.baseline = np.mean(dat[:, i], axis=1)
+        self.dat = dat - np.atleast_2d(self.baseline).T
+
+    def to_arrow(self):
+        """converts BeForEpochs to ``pyarrow.Table``
+
+        Samples and design will be concatenated to one arrow table. If baseline
+        is adjusted, additionally the baseline value will be added a column.
+
+        Zero sample and sampling_rate will be included to schema meta data.
+        of schema will be defined.
+
+        Arrow tables can converted back to BeForRecord struct using
+        ``BeForEpochs.from_arrow()``
+
+        Returns
+        -------
+        pyarrow.Table
+
+        """
+
+        dat = pd.concat([pd.DataFrame(self.dat), self.design], axis=1)
+        if self.is_baseline_adjusted():
+            dat[BSL_COL_NAME] = self.baseline
+        tbl = pa.Table.from_pandas(dat, preserve_index=False)
+
+        schema_metadata = {
+            "sampling_rate": str(self.sampling_rate),
+            "zero_sample": str(self.zero_sample)
+        }
+        return tbl.replace_schema_metadata(schema_metadata)
+
+
+    @staticmethod
+    def from_arrow(
+        tbl: pa.Table,
+        sampling_rate: float | None = None,
+        zero_sample: int | None = None,
+    ) -> BeForEpochs:
+        """Creates BeForEpoch struct from `pyarrow.Table`
+
+        Parameters
+        ----------
+        tbl : pyarrow.Table
+
+        """
+
+        if not isinstance(tbl, pa.Table):
+            raise TypeError(f"must be pyarrow.Table, not {type(tbl)}")
+
+        # search arrow meta data for befor parameter
+        if tbl.schema.metadata is not None:
+            for k, v in tbl.schema.metadata.items():
+                if k == b"sampling_rate":
+                    if sampling_rate is None:
+                        sampling_rate = try_num(v)
+                elif k == b"zero_sample":
+                    if zero_sample is None:
+                        try:
+                            zero_sample = int(try_num(v))
+                        except ValueError:
+                            zero_sample = 0
+
+        if sampling_rate is None:
+            raise RuntimeError("No sampling rate defined!")
+        if zero_sample is None:
+            zero_sample = 0
+
+        dat = tbl.to_pandas()
+
+        try:
+            baseline = np.array(dat.pop(BSL_COL_NAME))
+        except KeyError:
+            baseline = np.array([])
+
+        # n_epoch_samples: count columns_name that have not int as name
+        n_epoch_samples = dat.shape[1]
+        for cn in reversed(dat.columns):
+            try:
+                int(cn)
+                break
+            except ValueError:
+                n_epoch_samples -= 1
+
+        return BeForEpochs(
+            dat= dat.iloc[:, :n_epoch_samples],
+            sampling_rate=sampling_rate,
+            design=dat.iloc[:, n_epoch_samples:],
+            baseline=baseline,
+            zero_sample=zero_sample
+        )
---
+++ packages/befordata/befordata-0.3.1.tar.gz/befordata-0.3.1/befordata/_record.py
@@ -0,0 +1,311 @@
+"""Before Data"""
+
+from __future__ import annotations
+
+import warnings
+from dataclasses import dataclass, field
+from typing import List
+
+import numpy as np
+import pandas as pd
+from numpy.typing import ArrayLike, NDArray
+from pyarrow import Table
+
+from ._epochs import BeForEpochs
+from .misc import ENC, try_num, values_as_string
+
+
+@dataclass
+class BeForRecord:
+    """Data Structure for handling behavioural force measurements
+
+    Args
+    ----
+    dat: Pandas Dataframe
+        data
+    sampling_rate: float
+        the sampling rate of the force measurements
+    sessions: list of integer
+        sample numbers at which a new recording session starts, if the exists
+    time_column :
+            str = ""
+    meta: dictionary
+        any kind of meta data
+    """
+
+    dat: pd.DataFrame
+    sampling_rate: float
+    sessions: List[int] = field(default_factory=list[int])
+    time_column: str = ""
+    meta: dict = field(default_factory=dict)
+
+    def __post_init__(self):
+        if not isinstance(self.dat, pd.DataFrame):
+            raise TypeError(f"must be pandas.DataFrame, not {type(self.dat)}")
+
+        if len(self.sessions) == 0:
+            self.sessions.append(0)
+        else:
+            if isinstance(self.sessions, int):
+                self.sessions = [self.sessions]
+            if self.sessions[0] < 0:
+                self.sessions[0] = 0
+            elif self.sessions[0] > 0:
+                self.sessions.insert(0, 0)
+
+        if len(self.time_column) > 0 and self.time_column not in self.dat:
+            raise ValueError(f"Time column {self.time_column} not found in DataFrame")
+
+    def __repr__(self):
+        rtn = "BeForRecord"
+        rtn += f"\n  sampling_rate: {self.sampling_rate}"
+        rtn += f", n sessions: {self.n_sessions()}"
+        if len(self.time_column) >= 0:
+            rtn += f"\n  time_column: {self.time_column}"
+        rtn += "\n  metadata"
+        for k, v in self.meta.items():
+            rtn += f"\n  - {k}: {v}".rstrip()
+        rtn += "\n" + str(self.dat)
+        return rtn
+
+    def force_cols(self) -> NDArray[np.intp]:
+        return np.flatnonzero(self.dat.columns != self.time_column)
+
+    def n_samples(self) -> int:
+        """Number of sample in all sessions"""
+        return self.dat.shape[0]
+
+    def n_forces(self) -> int:
+        """Number of force columns"""
+        return len(self.force_cols())
+
+    def n_sessions(self) -> int:
+        """Number of recoding sessions"""
+        return len(self.sessions)
+
+    def time_stamps(self) -> NDArray:
+        """The time stamps (numpy array)
+
+        Creates time stamps, of they are not define in the data
+        """
+        if len(self.time_column) > 0:
+            return self.dat.loc[:, self.time_column].to_numpy()
+        else:
+            step = 1000.0 / self.sampling_rate
+            final_time = self.dat.shape[0] * step
+            return np.arange(0, final_time, step)
+
+    def forces(self, session: int | None = None
+    ) -> pd.DataFrame | pd.Series:
+        """Returns force data of a particular column and/or a particular session"""
+        columns = self.force_cols()
+        if session is None:
+            return self.dat.loc[:, columns]  # type: ignore
+        else:
+            r = self.session_range(session)
+            return self.dat.loc[r.start:r.stop, columns] # type: ignore
+
+
+    def add_session(self, dat: pd.DataFrame):
+        """Adds data (dataframe) as a new recording
+
+        Dataframe has to have the same columns as the already existing data
+        """
+        nbefore = self.dat.shape[0]
+        self.dat = pd.concat([self.dat, dat], ignore_index=True)
+        self.sessions.append(nbefore)
+
+
+    def split_sessions(self) -> List[BeForRecord]:
+        """split record in list of records of separate sessions
+        """
+
+        rtn = []
+        for idx in self.session_ranges():
+            dat = BeForRecord(dat=self.dat.iloc[idx, :],  # type: ignore
+                        sampling_rate=self.sampling_rate,
+                        time_column=self.time_column,
+                        meta=self.meta)
+            rtn.append(dat)
+
+        return rtn
+
+    def session_ranges(self) ->  List[range]:
+        """list of ranges of the samples of all sessions
+
+        """
+        return [self.session_range(s) for s in range(len(self.sessions))]
+
+    def session_range(self, session: int) -> range:
+        """range of the samples (from, to) of one particular session
+        """
+        f = self.sessions[session]
+        try:
+            t = self.sessions[session + 1]
+        except IndexError:
+            t = self.dat.shape[0]
+        return range(f, t - 1)
+
+
+    def find_samples_by_time(self, times: ArrayLike) -> NDArray:
+        """returns sample index (i) of the closes time in the BeForRecord.
+        Takes the next larger element, if the exact time could not be found.
+
+        .. math:: \\text{time_stamps}[i-1] <= t < \\text{time_stamps}[i]
+
+        Parameters
+        ----------
+        times : ArrayLike
+            the sorted array of time stamps
+
+        """
+        return np.searchsorted(self.time_stamps(), np.atleast_1d(times), "right")
+
+    def extract_epochs(
+        self,
+        column: str,
+        zero_samples: List[int] | NDArray[np.int_],
+        n_samples: int,
+        n_samples_before: int = 0,
+        design: pd.DataFrame = pd.DataFrame(),
+    ) -> BeForEpochs:
+        """extracts epochs from BeForRecord
+
+        Parameters
+        ----------
+        column: str
+            name of column containing the force data to be used
+        zero_samples: List[int]
+            zero sample that define the epochs
+        n_samples: int
+            number of samples to be extract (from zero sample on)
+        n_samples_before: int, optional
+            number of samples to be extracted before the zero sample (default=0)
+
+        design: pd.DataFrame, optional
+            design information
+
+        Notes
+        -----
+        use `find_times` to detect zero samples with time-based
+
+        """
+
+        fd = self.dat.loc[:, column]
+        n = len(fd)  # samples for data
+        n_epochs = len(zero_samples)
+        n_col = n_samples_before + n_samples
+        force_mtx = np.empty((n_epochs, n_col), dtype=np.float64)
+        for r, zs in enumerate(zero_samples):
+            f = zs - n_samples_before
+            if f > 0 and f < n:
+                t = zs + n_samples
+                if t > n:
+                    warnings.warn(
+                        f"extract_force_epochs: last force epoch is incomplete, {t-n} samples missing.",
+                        RuntimeWarning,
+                    )
+                    tmp = fd[f:]
+                    force_mtx[r, : len(tmp)] = tmp
+                    force_mtx[r, len(tmp) :] = 0
+                else:
+                    force_mtx[r, :] = fd[f:t]
+
+        return BeForEpochs(
+            force_mtx,
+            sampling_rate=self.sampling_rate,
+            design=design,
+            zero_sample=n_samples_before,
+        )
+
+
+    def to_arrow(self) -> Table:
+        """converts BeForRecord to ``pyarrow.Table``
+
+        metadata of schema will be defined. Files can converted back to
+        BeForRecord struct using ``BeForRecord.from_arrow()``
+
+        Returns
+        -------
+        pyarrow.Table
+
+        Examples
+        --------
+        >>> from pyarrow import feather
+        >>> feather.write_feather(data.to_arrow(), "filename.feather",
+                          compression="lz4", compression_level=6)
+        """
+
+        # Convert the DataFrame to a PyArrow table
+        table = Table.from_pandas(self.dat, preserve_index=False)
+
+        # Add metadata to the schema (serialize sampling_rate, timestamp, trigger, and meta)
+        schema_metadata = {
+            "sampling_rate": str(self.sampling_rate),
+            "time_column": self.time_column,
+            "sessions": ",".join([str(x) for x in self.sessions]),
+        }
+        schema_metadata.update(values_as_string(self.meta))
+        return table.replace_schema_metadata(schema_metadata)
+
+    @staticmethod
+    def from_arrow(
+        tbl: Table,
+        sampling_rate: float | None = None,
+        sessions: List[int] | None = None,
+        time_column: str | None = None,
+        meta: dict | None = None,
+    ) -> BeForRecord:
+        """Creates BeForRecord struct from `pyarrow.Table`
+
+        Parameters
+        ----------
+        tbl : pyarrow.Table
+
+        Examples
+        --------
+        >>> from pyarrow import feather
+        >>> dat = BeForRecord.from_arrow(feather.read_table("my_force_data.feather"))
+
+        """
+
+        if not isinstance(tbl, Table):
+            raise TypeError(f"must be pyarrow.Table, not {type(tbl)}")
+
+        # search arrow meta data for befor record parameter
+        arrow_meta = {}
+        if tbl.schema.metadata is not None:
+            for k, v in tbl.schema.metadata.items():
+                if k == b"sampling_rate":
+                    if sampling_rate is None:
+                        sampling_rate = try_num(v)
+                elif k == b"time_column":
+                    if time_column is None:
+                        time_column = v.decode(ENC)
+                elif k == b"sessions":
+                    if sessions is None:
+                        sessions = [int(x) for x in v.decode(ENC).split(",")]
+                else:
+                    arrow_meta[k.decode(ENC)] = try_num(v.decode(ENC).strip())
+
+        # make befor meta data
+        if isinstance(meta, dict):
+            meta.update(arrow_meta)
+        else:
+            meta = arrow_meta
+
+        if sampling_rate is None:
+            raise RuntimeError("No sampling rate defined!")
+        if time_column is None:
+            time_column = ""
+        if sessions is None:
+            sessions = []
+
+        return BeForRecord(
+            dat=tbl.to_pandas(),
+            sampling_rate=sampling_rate,
+            sessions=sessions,
+            time_column=time_column,
+            meta=meta
+        )
+
