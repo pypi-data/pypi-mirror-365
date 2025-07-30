d6a03252728c7380ebb227830bcd943c03609c2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:05:40 +0000

    Add package pisek/2.0.0/pisek-2.0.0-py3-none-any.whl

---
+++ packages/pisek/pisek-2.0.0-py3-none-any.whl/pisek/jobs/job_pipeline.py
@@ -0,0 +1,127 @@
+# pisek  - Tool for developing tasks for programming competitions.
+#
+# Copyright (c)   2019 - 2022 Václav Volhejn <vaclav.volhejn@gmail.com>
+# Copyright (c)   2019 - 2022 Jiří Beneš <mail@jiribenes.com>
+# Copyright (c)   2020 - 2022 Michal Töpfer <michal.topfer@gmail.com>
+# Copyright (c)   2022        Jiří Kalvoda <jirikalvoda@kam.mff.cuni.cz>
+# Copyright (c)   2023        Daniel Skýpala <daniel@honza.info>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from abc import ABC, abstractmethod
+from collections import deque
+from colorama import Cursor, ansi
+from math import ceil
+import sys
+import re
+import time
+
+from pisek.env.env import Env
+from pisek.utils.terminal import terminal_width
+from pisek.jobs.jobs import State, PipelineItem, Job, JobManager
+from pisek.jobs.cache import Cache
+
+
+class JobPipeline(ABC):
+    """Runs given Jobs and JobManagers according to their prerequisites."""
+
+    @abstractmethod
+    def __init__(self) -> None:
+        self.failed: bool = False
+        self._tmp_lines: int = 0
+        self.all_accessed_files: set[str] = set()
+
+    def run_jobs(self, cache: Cache, env: Env) -> bool:
+        self.job_managers: deque[JobManager] = deque()
+        self.pipeline: deque[PipelineItem] = deque(self.pipeline)
+        while len(self.pipeline) or len(self.job_managers):
+            p_item = self.pipeline.popleft()
+            if isinstance(p_item, JobManager):
+                self.job_managers.append(p_item)
+                self.pipeline.extendleft(reversed(p_item.create_jobs(env)))
+            elif isinstance(p_item, Job):
+                p_item.run_job(cache)
+                p_item.finish()
+                self.all_accessed_files |= p_item.accessed_files
+            else:
+                raise TypeError(
+                    f"Objects in {self.__class__.__name__} should be either Job or JobManager."
+                )
+
+            if p_item.dirty:
+                self._tmp_lines = 0
+
+            # we really need to call status_update to update messages
+            # Also no logs into env for just writing to stdout
+            self.failed |= not self._status_update(env)
+            if self.failed and not env.full:
+                break
+
+        cache.export()  # Save last version of cache
+        return self.failed
+
+    def _status_update(self, env: Env) -> bool:
+        """Display current progress. Return true if there were no failures."""
+        self._clear_print_tmp()
+        while len(self.job_managers):
+            job_man = self.job_managers.popleft()
+            # We are updating job_man's state with this call!
+            ongoing_msg = job_man.update()
+            if not env.full and job_man.any_failed():
+                self._print(ongoing_msg, env)
+                self._print(job_man.failures(), env, end="", file=sys.stderr)
+                return False
+            if job_man.state == State.failed or job_man.ready():
+                self._print_tmp(ongoing_msg, env)
+                self._print_active_item(job_man, env)
+
+                job_man.dirty = False
+                msg = job_man.finalize()
+                if job_man.dirty:
+                    self._tmp_lines = 0
+
+                if msg:
+                    self._print(msg, env)
+                if job_man.state == State.failed:
+                    self._print(job_man.failures(), env, end="", file=sys.stderr)
+                    return False
+            elif job_man.state == State.cancelled:
+                self._print(ongoing_msg, env)
+            else:
+                self._print_tmp(ongoing_msg, env)
+                self.job_managers.appendleft(job_man)
+                break
+
+        if len(self.pipeline):
+            self._print_active_item(self.pipeline[0], env)
+        return True
+
+    def _clear_print_tmp(self):
+        for _ in range(self._tmp_lines):
+            print(f"{Cursor.UP()}{ansi.clear_line()}", end="")
+        self._tmp_lines = 0
+
+    def _print_active_item(self, p_item: PipelineItem, env: Env):
+        t = time.strftime("%H:%M:%S", time.localtime())
+        self._print_tmp(f"Active job: {p_item.name} ({t})", env)
+
+    def _print_tmp(self, msg, env: Env, *args, **kwargs):
+        """Prints a text to be rewriten latter."""
+        ansi_escape = re.compile(r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])")
+        if not env.no_jumps:
+            self._tmp_lines += sum(
+                max(ceil(len(re.sub(ansi_escape, "", line)) / terminal_width), 1)
+                for line in msg.split("\n")
+            )
+            print(str(msg), *args, **kwargs)
+
+    def _print(self, msg, env: Env, *args, **kwargs):
+        """Prints a text."""
+        self._clear_print_tmp()
+        print(str(msg), *args, **kwargs)
---
+++ packages/pisek/pisek-2.0.0-py3-none-any.whl/pisek/utils/util.py
@@ -0,0 +1,56 @@
+# pisek  - Tool for developing tasks for programming competitions.
+#
+# Copyright (c)   2019 - 2022 Václav Volhejn <vaclav.volhejn@gmail.com>
+# Copyright (c)   2019 - 2022 Jiří Beneš <mail@jiribenes.com>
+# Copyright (c)   2020 - 2022 Michal Töpfer <michal.topfer@gmail.com>
+# Copyright (c)   2022        Jiří Kalvoda <jirikalvoda@kam.mff.cuni.cz>
+# Copyright (c)   2023        Daniel Skýpala <daniel@honza.info>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import os
+import shutil
+from typing import Optional
+
+from pisek.utils.paths import BUILD_DIR, TESTS_DIR, INTERNALS_DIR
+
+
+class ChangedCWD:
+    def __init__(self, path):
+        self._path = path
+
+    def __enter__(self):
+        self._orig_path = os.getcwd()
+        os.chdir(self._path)
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        os.chdir(self._orig_path)
+
+
+def _clean_subdirs(task_dir: str, subdirs: list[str]) -> None:
+    for subdir in subdirs:
+        full = os.path.join(task_dir, subdir)
+        try:
+            shutil.rmtree(full)
+        except FileNotFoundError:
+            pass
+
+
+def clean_task_dir(task_dir: str, pisek_directory: Optional[str]) -> bool:
+    _clean_subdirs(task_dir, [BUILD_DIR, TESTS_DIR, INTERNALS_DIR])
+    return True
+
+
+def clean_non_relevant_files(accessed_files: set[str]) -> None:
+    accessed_dirs = {os.path.dirname(file) for file in accessed_files}
+    for root, _, files in os.walk(TESTS_DIR):
+        for file in files:
+            path = os.path.join(root, file)
+            if root in accessed_dirs and path not in accessed_files:
+                os.remove(path)
