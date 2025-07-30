65eec9aa85d2459ebd53dc0a0d04665e2e9dea64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:05 +0000

    Add package PyApso/1.1.0/pyapso-1.1.0.tar.gz

---
+++ packages/PyApso/pyapso-1.1.0.tar.gz/pyapso-1.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyapso',
+    version='1.1.0',  
+    description='Adaptive Particle Swarm Optimization for continuous, multi-objective, and multidimensional problems',
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/smohamadabedy/pyapso',  # replace with your GitHub
+    project_urls={
+        "Documentation": "https://github.com/smohamadabedy/pyapso#readme",
+        "Source": "https://github.com/smohamadabedy/pyapso",
+        "Issues": "https://github.com/smohamadabedy/pyapso/issues",
+    },
+    license='MIT',
+    keywords='optimization particle-swarm metaheuristic multiobjective',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'tqdm',
+        'matplotlib'
+    ],
+    include_package_data=True,
+)
