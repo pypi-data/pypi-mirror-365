cf72cdb6141ea9aef72507cffa588861319cde0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 21:06:35 +0000

    Add package slurm-top-python/1.0.0a3/slurm_top_python-1.0.0a3-py3-none-any.whl

---
+++ packages/slurm-top-python/slurm_top_python-1.0.0a3-py3-none-any.whl/slurm_top_python/constants.py
@@ -0,0 +1,14 @@
+PRIVELAGED_USERS = [
+    'root',
+    'administrator'
+]
+
+INVALID_PROCESSES = [
+	'zombie'
+]
+
+# This will be overriden by process sensor
+SYSTEM_USERS = []
+
+GENERAL_CONFIG_PATH = "config/general.yml"
+SENSOR_CONFIG_PATH  = "config/sensors.yml"
