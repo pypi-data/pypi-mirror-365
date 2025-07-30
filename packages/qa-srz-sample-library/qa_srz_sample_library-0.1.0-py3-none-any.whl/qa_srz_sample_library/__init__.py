36553da2829551c549b1dc74694b4040281df79c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:04:42 +0000

    Add package qa-srz-sample-library/0.1.0/qa_srz_sample_library-0.1.0-py3-none-any.whl

---
+++ packages/qa-srz-sample-library/qa_srz_sample_library-0.1.0-py3-none-any.whl/qa_srz_sample_library/__init__.py
@@ -0,0 +1,10 @@
+def greet(name:str) -> str:
+    """
+    Greets the given name.
+    """
+    return f"Hello, {name} from my-awesome-library!"
+
+def add(a:int, b:int) -> int:
+    """add tow numbers"""
+    return a +b
+
