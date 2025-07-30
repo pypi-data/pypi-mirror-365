6f051ecbdb5c4a66032031542446fdc45c066aee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 23:06:24 +0000

    Add package yxmb-compat-engine/2025.7.30.0/yxmb_compat_engine-2025.7.30.0.tar.gz

---
+++ packages/yxmb-compat-engine/yxmb_compat_engine-2025.7.30.0.tar.gz/yxmb_compat_engine-2025.7.30.0/yxmb_compat_engine/错误.py
@@ -0,0 +1,11 @@
+import logging
+
+
+class RecordNotFound(Exception):
+    def __init__(self, message='未找到记录'):
+        if not message.startswith('未找到'):
+            message = '未找到' + message
+
+        self.message = message
+        super().__init__(message)
+        logging.error(message)
