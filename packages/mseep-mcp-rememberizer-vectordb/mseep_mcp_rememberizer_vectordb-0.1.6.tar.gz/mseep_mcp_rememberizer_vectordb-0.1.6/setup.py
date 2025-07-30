90e1839f074916e5eb0b8ad30722827daa64e060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 21:06:37 +0000

    Add package mseep-mcp-rememberizer-vectordb/0.1.6/mseep_mcp_rememberizer_vectordb-0.1.6.tar.gz

---
+++ packages/mseep-mcp-rememberizer-vectordb/mseep_mcp_rememberizer_vectordb-0.1.6.tar.gz/mseep_mcp_rememberizer_vectordb-0.1.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mseep-mcp-rememberizer-vectordb',
+    version='0.1.6',
+    description='A Model Context Protocol server for interacting with Rememberizer Vector Store (https://docs.rememberizer.ai/developer/vector-stores).',
+    long_description="Package managed by MseeP.ai",
+    long_description_content_type="text/plain",
+    author='mseep',
+    author_email='support@skydeck.ai',
+    maintainer='mseep',
+    maintainer_email='support@skydeck.ai',
+    url='https://github.com/mseep',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['httpx>=0.27.2', 'mcp>=1.0.0', 'python-dotenv>=1.0.1'],
+    keywords=['mseep'],
+)
