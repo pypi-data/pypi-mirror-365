b9d80dd92177bfdeef256c121acb8ee708bd1e40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:05:44 +0000

    Add package get-handling-prompt/1.0/get_handling_prompt-1.0.tar.gz

---
+++ packages/get-handling-prompt/get_handling_prompt-1.0.tar.gz/get_handling_prompt-1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+ 
+setup(
+    name='get_handling_prompt',
+    version='1.0',
+    py_modules=['get_handling_prompt',],
+    author='0xng',
+    author_email='anquan@163.com',
+    description='test',
+    long_description='test',
+    long_description_content_type='text/markdown',
+    url='',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
