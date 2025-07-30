7fa528798e2c35138dce7562823fe5fdc8ec5cb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:05:37 +0000

    Add package sws-api-client/1.5.12/sws_api_client-1.5.12-py3-none-any.whl

---
+++ packages/sws-api-client/sws_api_client-1.5.12-py3-none-any.whl/sws_api_client/__init__.py
@@ -0,0 +1,19 @@
+from .sws_api_client import SwsApiClient
+from .discover import Discover
+from .datasets import Datasets
+from .datatable import Datatables
+from .tasks import (
+    TaskManager,
+    PluginPayload,
+    TaskDataset,
+    TaskInfo,
+    TaskResponse
+)
+from .tags import Tags
+from .plugins import Plugins
+from .codelist import Codelists
+from .flaglist import Flaglists
+from .sessions import Sessions
+from .s3 import S3
+
+__version__ = "1.0.12-beta.1"
