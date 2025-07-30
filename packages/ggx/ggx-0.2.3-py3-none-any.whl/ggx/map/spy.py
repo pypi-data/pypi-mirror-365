5035daf0cf864bb834e337487f5466279155335c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:06:39 +0000

    Add package ggx/0.2.3/ggx-0.2.3-py3-none-any.whl

---
+++ packages/ggx/ggx-0.2.3-py3-none-any.whl/ggx/map/spy.py
@@ -0,0 +1,53 @@
+from ..client.game_client import GameClient
+from loguru import logger
+
+
+
+
+
+
+
+class Spy(GameClient):
+    
+    
+    
+    
+    async def send_spy(
+        self,
+        kingdom: int,
+        source_id: int,
+        tx: int,
+        ty: int,
+        spies_nr: int,
+        precision: int,
+        spy_type: int = 0,
+        horses_type: int = -1,
+        slowdown: int = 0,
+        feathers: int = 0,
+        sync: bool = True
+    ) -> dict | bool:
+        
+        try:
+            await self.send_json_message(
+                "csm",
+                {
+                    "SID": source_id,
+                    "TX": tx,
+                    "TY": ty,
+                    "SC": spies_nr,
+                    "ST": spy_type,
+                    "SE": precision,
+                    "HBW": horses_type,
+                    "KID": kingdom,
+                    "PTT": feathers,
+                    "SD": slowdown
+                }
+            )
+            if sync:
+                response = await self.wait_for_response("csm")
+                return response
+            return True
+        
+        except Exception as e:
+            logger.error(e)
+            return False
