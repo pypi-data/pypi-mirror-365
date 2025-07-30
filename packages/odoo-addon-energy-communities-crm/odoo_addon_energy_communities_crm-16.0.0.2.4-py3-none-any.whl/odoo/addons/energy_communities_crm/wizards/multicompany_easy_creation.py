d6be15f1830c597771374d6a674d94c104a8c5a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:07:54 +0000

    Add package odoo-addon-energy-communities-crm/16.0.0.2.4/odoo_addon_energy_communities_crm-16.0.0.2.4-py3-none-any.whl

---
+++ packages/odoo-addon-energy-communities-crm/odoo_addon_energy_communities_crm-16.0.0.2.4-py3-none-any.whl/odoo/addons/energy_communities_crm/wizards/multicompany_easy_creation.py
@@ -0,0 +1,23 @@
+from odoo import api, fields, models
+
+
+class AccountMulticompanyEasyCreationWiz(models.TransientModel):
+    _name = "account.multicompany.easy.creation.wiz"
+    _inherit = "account.multicompany.easy.creation.wiz"
+
+    def action_accept(self):
+        success_msg = super().action_accept()
+        # create default sale team for new company
+        self.env["crm.team"].get_create_default_sale_team(self.new_company_id)
+        # create default stages for new company
+        self.env["crm.stage"].get_create_default_stages(self.new_company_id)
+        return success_msg
+
+    @api.model
+    def _get_company_creation_related_users_list(self, company_id):
+        users = [user.id for user in company_id.get_users()] + [
+            self.env.ref("base.public_user").id
+        ]
+        if self.env.ref("base.user_admin").id not in users:
+            users = users + [self.env.ref("base.user_admin").id]
+        return users
