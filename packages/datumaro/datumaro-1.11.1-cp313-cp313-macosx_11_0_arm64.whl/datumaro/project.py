0fbec568e0c9c4214163a0a98e7ff2108b8a6cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:27 +0000

    Add package datumaro/1.11.1/datumaro-1.11.1-cp313-cp313-macosx_11_0_arm64.whl

---
+++ packages/datumaro/datumaro-1.11.1-cp313-cp313-macosx_11_0_arm64.whl/datumaro/project.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2022 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
+# ruff: noqa: F401
+
+# This module is a usability proxy for components.project
+
+from .components.project import (
+    BuildStageType,
+    DiffStatus,
+    IgnoreMode,
+    ObjectId,
+    Pipeline,
+    Project,
+    ProjectBuilder,
+    ProjectBuildTargets,
+    ProjectSourceDataset,
+    Revision,
+    Tree,
+)
