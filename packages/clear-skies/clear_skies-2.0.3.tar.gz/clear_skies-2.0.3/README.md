8232dbf4a3a08c830a6df23531a0645081ced71e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:05:54 +0000

    Add package clear-skies/2.0.3/clear_skies-2.0.3.tar.gz

---
+++ packages/clear-skies/clear_skies-2.0.3.tar.gz/clear_skies-2.0.3/README.md
@@ -0,0 +1,15 @@
+# clearskies
+
+clearskies is a very opinionated Python framework intended for developing microservices in the cloud via declarative programming principles.  It is mainly intended for backend services and so is designed for RESTful API endpoints, queue listeners, scheduled tasks, and the like.
+
+## Installation, Documentation, and Usage
+
+To install:
+
+```bash
+pip3 install clear-skies
+```
+
+Documentation is under construction here:
+
+[https://clearskies.info](https://clearskies.info)
---
+++ packages/clear-skies/clear_skies-2.0.3.tar.gz/clear_skies-2.0.3/src/clearskies/autodoc/request/request.py
@@ -0,0 +1,38 @@
+from typing import Any
+
+
+class Request:
+    description: str = ""
+    relative_path: str = ""
+    request_methods: list[str] = []
+    parameters: list[Any] = []
+    responses = None
+    root_properties = None
+
+    def __init__(
+        self, description, responses, relative_path="", request_methods="GET", parameters=None, root_properties=None
+    ):
+        self.description = description
+        self.responses = responses
+        self.relative_path = relative_path.lstrip("/")
+        self.request_methods = [request_methods] if type(request_methods) == str else request_methods
+        self.set_parameters(parameters)
+        self.root_properties = root_properties if root_properties is not None else {}
+
+    def set_request_methods(self, request_methods):
+        self.request_methods = [request_methods] if type(request_methods) == str else request_methods
+        return self
+
+    def prepend_relative_path(self, path):
+        self.relative_path = path.rstrip("/") + "/" + self.relative_path.lstrip("/")
+        return self
+
+    def append_relative_path(self, path):
+        self.relative_path = self.relative_path.rstrip("/") + "/" + path.lstrip("/")
+        return self
+
+    def set_parameters(self, parameters=None):
+        self.parameters = parameters if parameters else []
+
+    def add_parameter(self, parameter):
+        self.parameters.append(parameter)
