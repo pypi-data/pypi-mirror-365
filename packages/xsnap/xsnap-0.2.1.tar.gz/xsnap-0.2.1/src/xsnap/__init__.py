2708bb47a8fa4cb6ded30ad742d11ea37c04bc06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 21:06:16 +0000

    Add package xsnap/0.2.1/xsnap-0.2.1.tar.gz

---
+++ packages/xsnap/xsnap-0.2.1.tar.gz/xsnap-0.2.1/src/xsnap/__init__.py
@@ -0,0 +1,9 @@
+"""
+xray_pipeline
+=============
+
+Modular pipeline for extracting, stacking and fitting X-ray spectra
+from Chandra, Swift-XRT and XMM-Newton.
+"""
+__all__ = ["chandra", "swiftxrt", "xmm", "nustar"]
+__version__ = "0.2.1"
