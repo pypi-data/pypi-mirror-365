f72c1d3296f60195667d1a85548e21dd3815d8ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:06:51 +0000

    Add package plotverse/0.0.7/plotverse-0.0.7.tar.gz

---
+++ packages/plotverse/plotverse-0.0.7.tar.gz/plotverse-0.0.7/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="plotverse",
+    version="0.0.7",
+    description="Powerful data visualization and analysis library combining Pandas, Matplotlib, and Seaborn.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Sanju",
+    author_email="sanjayskpy1@gmail.com",
+    url="https://github.com/sk-sanju/plotverse",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "pandas==2.2.2",
+        "matplotlib==3.8.4",
+        "seaborn==0.13.2"
+    ],
+    python_requires=">=3.11",
+    classifiers=[
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
