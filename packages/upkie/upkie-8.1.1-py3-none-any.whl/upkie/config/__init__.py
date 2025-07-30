24ad73b7921a7a4ec5297aac42ef1b606f2ce8a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:34 +0000

    Add package upkie/8.1.1/upkie-8.1.1-py3-none-any.whl

---
+++ packages/upkie/upkie-8.1.1-py3-none-any.whl/upkie/config/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2023 Inria
+
+## \namespace upkie.config
+## \brief Static robot configuration.
+
+from .spine_config import SPINE_CONFIG
+
+__all__ = [
+    "SPINE_CONFIG",
+]
---
+++ packages/upkie/upkie-8.1.1-py3-none-any.whl/upkie/envs/wrappers/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2023 Inria
+
+from .add_action_to_observation import AddActionToObservation
+from .add_lag_to_action import AddLagToAction
+from .differentiate_action import DifferentiateAction
+from .noisify_action import NoisifyAction
+from .noisify_observation import NoisifyObservation
+from .observation_based_reward import ObservationBasedReward
+from .random_push import RandomPush
+
+__all__ = [
+    "AddActionToObservation",
+    "AddLagToAction",
+    "DifferentiateAction",
+    "NoisifyAction",
+    "NoisifyObservation",
+    "ObservationBasedReward",
+    "RandomPush",
+]
