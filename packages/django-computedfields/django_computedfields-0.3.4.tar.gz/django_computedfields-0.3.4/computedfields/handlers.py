e45140b93893382df0ae0530b6cdf25bb3297665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:36 +0000

    Add package django-computedfields/0.3.4/django_computedfields-0.3.4.tar.gz

---
+++ packages/django-computedfields/django_computedfields-0.3.4.tar.gz/django_computedfields-0.3.4/computedfields/handlers.py
@@ -0,0 +1,187 @@
+"""
+Module containing the database signal handlers.
+
+The handlers are registered during application startup
+in ``apps.ready``.
+
+.. NOTE::
+
+    The handlers are not registered in the managment
+    commands ``makemigrations``, ``migrate`` and ``help``.
+"""
+from .thread_locals import get_DELETES, get_M2M_REMOVE, get_M2M_CLEAR, get_UPDATE_OLD
+from django.db import transaction
+from .resolver import active_resolver
+from .settings import settings
+from .signals import resolver_start, resolver_exit
+
+# typing imports
+from typing import Iterable, Type, cast
+from django.db.models import Model
+
+
+def get_old_handler(sender: Type[Model], instance: Model, **kwargs) -> None:
+    """
+    ``get_old_handler`` handler.
+
+    ``pre_save`` signal handler to spot incoming fk relation changes.
+    This is needed to correctly update old relations after fk changes,
+    that would contain dirty computed field values after a save.
+    The actual updates on old relations are done during ``post_save``.
+    Skipped during fixtures.
+    """
+    # do not handle fixtures
+    if kwargs.get('raw'):
+        return
+    # exit early if instance is new
+    # issue #67: also exit on empty pk (cloning does not reset state)
+    if instance._state.adding or not instance.pk:
+        return
+    contributing_fks = active_resolver._fk_map.get(sender)
+    # exit early if model contains no contributing fk fields
+    if not contributing_fks:
+        return
+    candidates = set(contributing_fks)
+    if kwargs.get('update_fields'):
+        candidates &= set(cast(Iterable[str], kwargs.get('update_fields')))
+    # exit early if no contributing fk field will be updated
+    if not candidates:
+        return
+    # we got an update instance with possibly dirty fk fields
+    # we do simply a full update on all old related fk records for now
+    # FIXME: this might turn out as a major update bottleneck, if so
+    #        filter by individual field changes instead? (tests are ~10% slower)
+    data = active_resolver.preupdate_dependent(instance, sender)
+    if data:
+        get_UPDATE_OLD()[instance] = data
+    return
+
+
+def postsave_handler(sender: Type[Model], instance: Model, **kwargs) -> None:
+    """
+    ``post_save`` handler.
+
+    Directly updates dependent objects.
+    Skipped during fixtures.
+    """
+    # do not update for fixtures
+    if not kwargs.get('raw'):
+        active_resolver.update_dependent(
+            instance, sender, kwargs.get('update_fields'),
+            old=get_UPDATE_OLD().pop(instance, None),
+            update_local=False,
+            querysize=settings.COMPUTEDFIELDS_QUERYSIZE
+        )
+
+
+def predelete_handler(sender: Type[Model], instance: Model, **_) -> None:
+    """
+    ``pre_delete`` handler.
+
+    Gets all dependent objects as pk lists and saves
+    them in thread local storage.
+    """
+    # get the querysets as pk lists to hold them in storage
+    # we have to get pks here since the queryset will be empty after deletion
+    data = active_resolver.preupdate_dependent(instance, sender)
+    if data:
+        get_DELETES()[instance] = data
+
+
+def postdelete_handler(sender: Type[Model], instance: Model, **kwargs) -> None:
+    """
+    ``post_delete`` handler.
+
+    Loads the dependent objects from the previously saved pk lists
+    and updates them.
+    """
+    # after deletion we can update the associated computed fields
+    updates = get_DELETES().pop(instance, None)
+    if updates:
+        resolver_start.send(sender=active_resolver)
+        with transaction.atomic():
+            for model, [pks, fields] in updates.items():
+                active_resolver.bulk_updater(
+                    model._base_manager.filter(pk__in=pks),
+                    fields,
+                    querysize=settings.COMPUTEDFIELDS_QUERYSIZE
+                )
+        resolver_exit.send(sender=active_resolver)
+
+
+# Fix #187, runtime patched M2M through models, that have no entry in the M2M resolver map.
+def _patch_fields(sender: Type[Model], model: Type[Model], reverse: bool):
+    if not active_resolver.has_computedfields(sender):
+        return
+    for field in model._meta.get_fields():
+        if field.many_to_many:
+            if not reverse:
+                field = field.remote_field
+            if field.remote_field.through is sender:
+                active_resolver._m2m[sender] = {
+                    'left': field.m2m_field_name(),
+                    'right': field.m2m_reverse_field_name()
+                }
+                return active_resolver._m2m[sender]
+
+
+def m2m_handler(sender: Type[Model], instance: Model, **kwargs) -> None:
+    """
+    ``m2m_change`` handler.
+
+    Works like the other handlers but on the corresponding
+    m2m actions.
+    """
+    action = kwargs['action']
+    reverse = kwargs['reverse']
+    fields = active_resolver._m2m.get(sender, _patch_fields(sender, kwargs['model'], reverse))
+    # exit early if we have no update rule for the through model
+    if not fields:
+        return
+
+    left = fields['right'] if reverse else fields['left']   # fieldname on instance
+    right = fields['left'] if reverse else fields['right']  # fieldname on model
+
+    if action == 'post_add':
+        active_resolver.update_dependent(
+            sender.objects.filter(**{left: instance.pk, right+'__in': kwargs['pk_set']}),
+            sender,
+            update_local=True,
+            querysize=settings.COMPUTEDFIELDS_QUERYSIZE
+        )
+
+    elif action == 'pre_remove':
+        get_M2M_REMOVE()[instance] = active_resolver.preupdate_dependent(
+            sender.objects.filter(**{left: instance.pk, right+'__in': kwargs['pk_set']})
+        )
+
+    elif action == 'post_remove':
+        old = get_M2M_REMOVE().pop(instance, None)
+        if old:
+            resolver_start.send(sender=active_resolver)
+            with transaction.atomic():
+                for _model, [pks, update_fields] in old.items():
+                    active_resolver.bulk_updater(
+                        _model._base_manager.filter(pk__in=pks),
+                        update_fields,
+                        querysize=settings.COMPUTEDFIELDS_QUERYSIZE
+                    )
+            resolver_exit.send(sender=active_resolver)
+
+    elif action == 'pre_clear':
+        get_M2M_CLEAR()[instance] = active_resolver.preupdate_dependent(
+            sender.objects.filter(**{left: instance.pk})
+        )
+
+    elif action == 'post_clear':
+        old = get_M2M_CLEAR().pop(instance, None)
+        if old:
+            resolver_start.send(sender=active_resolver)
+            with transaction.atomic():
+                for _model, [pks, update_fields] in old.items():
+                    active_resolver.bulk_updater(
+                        _model._base_manager.filter(pk__in=pks),
+                        update_fields,
+                        querysize=settings.COMPUTEDFIELDS_QUERYSIZE
+                    )
+            resolver_exit.send(sender=active_resolver)
---
+++ packages/django-computedfields/django_computedfields-0.3.4.tar.gz/django_computedfields-0.3.4/computedfields/models.py
@@ -0,0 +1,148 @@
+from typing import Iterable, Optional
+from django.db import models
+from django.contrib.contenttypes.models import ContentType, ContentTypeManager
+from django.utils.translation import gettext_lazy as _
+from .resolver import active_resolver, _ComputedFieldsModelBase, NotComputed
+
+__all__ = [
+    'ComputedFieldsModel',
+    'ComputedField',
+    'computed',
+    'precomputed',
+    'compute',
+    'update_computedfields',
+    'update_dependent',
+    'preupdate_dependent',
+    'has_computedfields',
+    'get_computedfields',
+    'is_computedfield',
+    'get_contributing_fks',
+    'ComputedFieldsAdminModel',
+    'ContributingModelsModel'
+]
+
+class ComputedFieldsModel(_ComputedFieldsModelBase, models.Model):
+    """
+    Abstract base class for models with computed fields. Overloads ``save`` to update
+    local computed field values before they are written to the database.
+
+    All models containing a computed field must be derived from this class.
+    """
+    class Meta:
+        abstract = True
+
+    def save(
+        self,
+        force_insert: bool = False,
+        force_update: bool = False,
+        using: Optional[str] = None,
+        update_fields: Optional[Iterable[str]] = None,
+        skip_computedfields: bool = False
+    ) -> None:
+        """
+        Overloaded to update computed field values before writing to the database.
+
+        The method understands a special argument `skip_computedfields` to skip
+        the calculation of local computed fields. This is used by the ``@precomputed`` decorator
+        to allow to skip a second field calculation with ``@precomputed(skip_after=True)``.
+        If you use `skip_computedfields` yourself, make sure to synchronize computed fields
+        yourself by other means, e.g. by calling ``update_computedfields`` before writing
+        the instance or by a later ``update_dependent`` call.
+        """
+        if not skip_computedfields:
+            update_fields = update_computedfields(self, update_fields)
+        return super(ComputedFieldsModel, self).save(
+            force_insert=force_insert,
+            force_update=force_update,
+            using=using,
+            update_fields=update_fields
+        )
+
+
+# some convenient access mappings
+
+# decorators
+#: Convenient access to the decorator :meth:`@computed<.resolver.Resolver.computed>`.
+computed = active_resolver.computed
+#: Convenient access to the decorator :meth:`@precomputed<.resolver.Resolver.precomputed>`.
+precomputed = active_resolver.precomputed
+
+# ComputedField factory
+#: Convenient access to :meth:`computedfield_factory<.resolver.Resolver.computedfield_factory>`.
+ComputedField = active_resolver.computedfield_factory
+
+# not_computed context
+#: Convenient access to :meth:`computedfield_factory<.resolver.NotComputedContextManager>`.
+not_computed = NotComputed
+
+# computed field updates
+#: Convenient access to :meth:`compute<.resolver.Resolver.compute>`.
+compute = active_resolver.compute
+#: Convenient access to :meth:`update_computedfields<.resolver.Resolver.update_computedfields>`.
+update_computedfields = active_resolver.update_computedfields
+#: Convenient access to :meth:`update_dependent<.resolver.Resolver.update_dependent>`.
+update_dependent = active_resolver.update_dependent
+#: Convenient access to :meth:`preupdate_dependent<.resolver.Resolver.preupdate_dependent>`.
+preupdate_dependent = active_resolver.preupdate_dependent
+
+# helper
+#: Convenient access to :meth:`has_computedfields<.resolver.Resolver.has_computedfields>`.
+has_computedfields = active_resolver.has_computedfields
+#: Convenient access to :meth:`get_computedfields<.resolver.Resolver.get_computedfields>`.
+get_computedfields = active_resolver.get_computedfields
+#: Convenient access to :meth:`is_computedfield<.resolver.Resolver.is_computedfield>`.
+is_computedfield = active_resolver.is_computedfield
+#: Convenient access to :meth:`get_contributing_fks<.resolver.Resolver.get_contributing_fks>`.
+get_contributing_fks = active_resolver.get_contributing_fks
+
+
+class ComputedModelManager(ContentTypeManager):
+    def get_queryset(self):
+        objs = ContentType.objects.get_for_models(
+            *active_resolver.computed_models.keys()).values()
+        pks = [model.pk for model in objs]
+        return ContentType.objects.filter(pk__in=pks)
+
+
+class ComputedFieldsAdminModel(ContentType):
+    """
+    Proxy model to list all ``ComputedFieldsModel`` models with their
+    field dependencies in admin. This might be useful during development.
+    To enable it, set ``COMPUTEDFIELDS_ADMIN = True`` in `settings.py`.
+    """
+    objects = ComputedModelManager()
+
+    class Meta:
+        proxy = True
+        managed = False
+        verbose_name = _('Computed Fields Model')
+        verbose_name_plural = _('Computed Fields Models')
+        ordering = ('app_label', 'model')
+
+
+class ModelsWithContributingFkFieldsManager(ContentTypeManager):
+    def get_queryset(self):
+        objs = ContentType.objects.get_for_models(
+            *active_resolver.get_contributing_fks().keys()).values()
+        pks = [model.pk for model in objs]
+        return ContentType.objects.filter(pk__in=pks)
+
+
+class ContributingModelsModel(ContentType):
+    """
+    Proxy model to list all models in admin, that contain foreign key relations
+    contributing to computed fields. This might be useful during development.
+    To enable it, set ``COMPUTEDFIELDS_ADMIN = True`` in `settings.py`.
+
+    .. NOTE::
+        A foreign key relation is considered contributing, if it is part of
+        a computed field dependency in reverse direction.
+    """
+    objects = ModelsWithContributingFkFieldsManager()
+
+    class Meta:
+        proxy = True
+        managed = False
+        verbose_name = _('Model with contributing ForeignKey Fields')
+        verbose_name_plural = _('Models with contributing ForeignKey Fields')
+        ordering = ('app_label', 'model')
