b01ed68ec5300d92fdd96b84576b87081fa2921a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 22:05:23 +0000

    Add package hidden-selenium/0.1.1/hidden_selenium-0.1.1.tar.gz

---
+++ packages/hidden-selenium/hidden_selenium-0.1.1.tar.gz/hidden_selenium-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hidden_selenium',
+    version='0.1.1',
+    author='Max Base',
+    description='A stealthy undetected browser automation tool using Selenium.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'undetected-chromedriver>=3.5.3',
+    ],
+    python_requires='>=3.7',
+)
