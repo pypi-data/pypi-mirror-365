a58db3974a49ee9348d01a206bd591c949981ad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:05:18 +0000

    Add package pddiktipy/2.0.5.post1/pddiktipy-2.0.5.post1.tar.gz

---
+++ packages/pddiktipy/pddiktipy-2.0.5.post1.tar.gz/pddiktipy-2.0.5.post1/pddiktipy/__init__.py
@@ -0,0 +1,26 @@
+# flake8: noqa
+
+# __variables__ with double-quoted values will be available in setup.py
+__version__ = "2.0.5.post1"
+
+from .api import api
+from .exceptions import (
+    PDDIKTIError,
+    APIConnectionError,
+    APITimeoutError,
+    APIRateLimitError,
+    APIResponseError,
+    ValidationError,
+    AuthenticationError
+)
+
+__all__ = [
+    'api',
+    'PDDIKTIError',
+    'APIConnectionError', 
+    'APITimeoutError',
+    'APIRateLimitError',
+    'APIResponseError',
+    'ValidationError',
+    'AuthenticationError'
+]
