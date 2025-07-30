9ae1e516b3c27e56b6c54631a5b0753a6eb3e702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:07:07 +0000

    Add package django-handyhelpers/0.3.41/django_handyhelpers-0.3.41-py3-none-any.whl

---
+++ packages/django-handyhelpers/django_handyhelpers-0.3.41-py3-none-any.whl/handyhelpers/context_processors.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+
+
+def base_template(request):
+    return {'BASE_TEMPLATE': getattr(settings, 'BASE_TEMPLATE', 'handyhelpers/handyhelpers_base_bs5.htm')}
+
+
+def get_settings(request):
+    return {'BASE_TEMPLATE': getattr(settings, 'BASE_TEMPLATE', 'handyhelpers/handyhelpers_base_bs5.htm'),
+            'PROJECT_NAME': getattr(settings, 'PROJECT_NAME', ''),
+            'PROJECT_VERSION': getattr(settings, 'PROJECT_VERSION', ''),
+            'HH_STARTTIME': getattr(settings, 'HH_STARTTIME', ''),
+            }
