a7a59a3739ab146f84e524fe62ab7e1f494adf74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:14:42 +0000

    Add package abstra-connectors/0.0.8/abstra_connectors-0.0.8-py3-none-any.whl

---
+++ packages/abstra-connectors/abstra_connectors-0.0.8-py3-none-any.whl/connectors/monday/__init__.py
@@ -0,0 +1,26 @@
+from .monday import (
+    AbstraMondayException,
+    SubscriptableDataclass,
+    ItemsPageDTO,
+    ColumnDTO,
+    ColumnValueDTO,
+    GroupDTO,
+    BoardDTO,
+    ItemDTO,
+    Item,
+    MondayConnector,
+)
+
+
+__all__ = [
+    "AbstraMondayException",
+    "SubscriptableDataclass",
+    "ItemsPageDTO",
+    "ColumnDTO",
+    "ColumnValueDTO",
+    "GroupDTO",
+    "BoardDTO",
+    "ItemDTO",
+    "Item",
+    "MondayConnector",
+]
