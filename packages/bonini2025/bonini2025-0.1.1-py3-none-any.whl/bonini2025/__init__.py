a80e2ef7ac7ec6b1bafd0ef498844bdf28667c9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:31 +0000

    Add package bonini2025/0.1.1/bonini2025-0.1.1-py3-none-any.whl

---
+++ packages/bonini2025/bonini2025-0.1.1-py3-none-any.whl/bonini2025/__init__.py
@@ -0,0 +1,14 @@
+"""
+Data and formalisms from Bonini et al. (2025)
+"""
+# {# pkglts, src
+# FYEO
+# #}
+# {# pkglts, version, after src
+from . import version
+
+__version__ = version.__version__
+# #}
+# {# pkglts, glabdata, after version
+from .info import *
+# #}
