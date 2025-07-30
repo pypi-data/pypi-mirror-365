3c76a7999be8ce97ef350efa44d20bf0681eba7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:10:05 +0000

    Add package matched-betting-calculator/0.2.0/matched_betting_calculator-0.2.0.tar.gz

---
+++ packages/matched-betting-calculator/matched_betting_calculator-0.2.0.tar.gz/matched_betting_calculator-0.2.0/matched_betting_calculator/constants.py
@@ -0,0 +1,12 @@
+"""
+Constants used throughout the matched betting calculator.
+"""
+
+from typing import Final
+
+
+class PercentageConstants:
+    """Constants related to percentage calculations."""
+
+    PERCENT_DIVISOR: Final[int] = 100
+    DECIMAL_PLACES: Final[int] = 2
