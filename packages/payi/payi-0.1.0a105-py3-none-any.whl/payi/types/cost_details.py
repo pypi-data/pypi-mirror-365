4ba493d1ef24eb1f413a0612e5afcf2f95a8d5bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:48 +0000

    Add package payi/0.1.0a105/payi-0.1.0a105-py3-none-any.whl

---
+++ packages/payi/payi-0.1.0a105-py3-none-any.whl/payi/types/cost_details.py
@@ -0,0 +1,15 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Optional
+
+from .._models import BaseModel
+
+__all__ = ["CostDetails"]
+
+
+class CostDetails(BaseModel):
+    base: float
+
+    overage_base: Optional[float] = None
+
+    overrun_base: Optional[float] = None
