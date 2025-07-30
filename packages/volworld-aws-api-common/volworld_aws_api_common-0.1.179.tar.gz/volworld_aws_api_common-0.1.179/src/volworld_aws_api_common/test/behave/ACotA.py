9488d06e800d6f03da85903d7d97186d4a097dd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:43 +0000

    Add package volworld-aws-api-common/0.1.179/volworld_aws_api_common-0.1.179.tar.gz

---
+++ packages/volworld-aws-api-common/volworld_aws_api_common-0.1.179.tar.gz/volworld_aws_api_common-0.1.179/src/volworld_aws_api_common/test/behave/ACotA.py
@@ -0,0 +1,16 @@
+from typing import Final
+
+# ====== ContextA: Attribute ======
+
+
+class ACotA:
+
+    LoginUser: Final[str] = "LoginUser"
+
+    MockScenario: Final[str] = "MockScenario"
+
+    TargetPage: Final[str] = "TargetPage"
+
+    UserName: Final[str] = "UserName"
+    UserPassword: Final[str] = "UserPassword"
+
