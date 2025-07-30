08afab28f81b35b09e5eec43aa0d982e8eb1ec06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:47:09 +0000

    Add package niemabf/1.0.5/niemabf-1.0.5.tar.gz

---
+++ packages/niemabf/niemabf-1.0.5.tar.gz/niemabf-1.0.5/niemabf/common.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python3
+'''
+Common things in NiemaBF
+'''
+
+# standard imports
+from gzip import open as gopen
+
+# non-standard imports
+from numpy import uint8
+
+# useful constants
+NIEMABF_VERSION = '1.0.5'
+DEFAULT_BUFSIZE = 1048576 # 1 MB #8192 # 8 KB
+
+def open_file(fn, mode='rt', buffering=DEFAULT_BUFSIZE):
+    '''Open a file (or stream) and return the file(-like) object
+    
+    Args:
+        fn (str): Filename to open, or `-` for standard input (if read mode) or standard output (if write mode)
+        mode (str): Mode string containing `r` (read) or `w` (write), and containing `t` (text) or `b` (binary)
+        buffering (int): The buffer size when opening a file
+
+    Returns:
+        file: The file(-like) object
+    '''
+    mode = mode.lower().strip()
+    if fn == '-':
+        if 'w' in mode:
+            from sys import stdout as out_file
+        else:
+            from sys import stdin as out_file
+    elif fn.strip().lower().endswith('.gz'):
+        out_file = gopen(fn, mode=mode)
+    else:
+        out_file = open(fn, mode=mode, buffering=buffering)
+    return out_file
