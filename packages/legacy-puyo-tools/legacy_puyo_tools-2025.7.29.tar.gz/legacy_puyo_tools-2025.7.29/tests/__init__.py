078b6a61265bec5fb402d3bf993405844311b54a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 21:06:01 +0000

    Add package legacy-puyo-tools/2025.7.29/legacy_puyo_tools-2025.7.29.tar.gz

---
+++ packages/legacy-puyo-tools/legacy_puyo_tools-2025.7.29.tar.gz/legacy_puyo_tools-2025.7.29/tests/__init__.py
@@ -0,0 +1,5 @@
+"""Testing infrastructure for legacy_puyo_tools.
+
+SPDX-FileCopyrightText: 2025 Samuel Wu
+SPDX-License-Identifier: MIT
+"""
---
+++ packages/legacy-puyo-tools/legacy_puyo_tools-2025.7.29.tar.gz/legacy_puyo_tools-2025.7.29/tests/create_mtx_test.py
@@ -0,0 +1,7 @@
+"""Test creating mtx formats.
+
+SPDX-FileCopyrightText: 2025 Samuel Wu
+SPDX-License-Identifier: MIT
+"""
+
+# TODO: Implement the test for creating an MTX file once it has been implemented
