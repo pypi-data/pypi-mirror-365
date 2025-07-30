2abf72a13fb1202e160398291651423bc3cb8a37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:24 +0000

    Add package perian/0.5.3/perian-0.5.3.tar.gz

---
+++ packages/perian/perian-0.5.3.tar.gz/perian-0.5.3/README.md
@@ -0,0 +1,4 @@
+# PERIAN Sky Platform API Python Client
+
+### Getting Started 
+To get started on our platform, follow [our guide](https://storage.googleapis.com/perian-public-files/Perian_Sky_Platform_Getting_Started.pdf) that will walk you through all the necessary setup and features.
---
+++ packages/perian/perian-0.5.3.tar.gz/perian-0.5.3/perian/api/__init__.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+
+# import apis into api package
+from perian.api.accelerator_type_api import AcceleratorTypeApi
+from perian.api.billing_api import BillingApi
+from perian.api.inference_api import InferenceApi
+from perian.api.instance_type_api import InstanceTypeApi
+from perian.api.job_api import JobApi
+from perian.api.metrics_api import MetricsApi
+from perian.api.model_metadata_api import ModelMetadataApi
+from perian.api.model_template_api import ModelTemplateApi
+from perian.api.organization_api import OrganizationApi
+
