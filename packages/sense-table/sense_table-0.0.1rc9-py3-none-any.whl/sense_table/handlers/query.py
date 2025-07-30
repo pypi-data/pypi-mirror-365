aed64ce81ce318b4e416f674812f9f64e30ec6e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:53 +0000

    Add package sense-table/0.0.1rc9/sense_table-0.0.1rc9-py3-none-any.whl

---
+++ packages/sense-table/sense_table-0.0.1rc9-py3-none-any.whl/sense_table/handlers/query.py
@@ -0,0 +1,36 @@
+import logging
+import os
+from flask import request, jsonify, current_app
+import duckdb
+from timeit import default_timer
+from flask import Blueprint, jsonify
+from sense_table.utils.serialization import serialize
+
+logger = logging.getLogger(__name__)
+query_bp = Blueprint('query', __name__)
+
+
+@query_bp.post('/query')
+def query():
+    time_start = default_timer()
+    query = request.json['query']
+    con = duckdb.connect()
+    column_names = []
+    rows = []
+    error = None
+    try:
+        result = con.execute(query)
+        column_names = [desc[0] for desc in result.description]
+        rows = result.fetchall()
+   
+    except Exception as e:
+        error = str(e)
+    
+    return jsonify({
+            'status': 'success' if not error else 'error',
+            'column_names': column_names,
+            'rows': serialize(rows),
+            'runtime': default_timer() - time_start,
+            'error': error,
+        })
+
