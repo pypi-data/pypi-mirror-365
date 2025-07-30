13a929c1a502638d79621bc39a8156f53cde53ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:32:47 +0000

    Add package StereoMolGraph/0.0.0b6/stereomolgraph-0.0.0b6-py3-none-any.whl

---
+++ packages/StereoMolGraph/stereomolgraph-0.0.0b6-py3-none-any.whl/stereomolgraph/__init__.py
@@ -0,0 +1,48 @@
+# ruff: noqa: F401
+"""Simple access of key classes MolGraph, StereoMolGraph,
+CondensedReactionGraph and StereoCondensedReactionGraph"""
+
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from stereomolgraph.graphs.mg import AtomId, Bond, MolGraph
+    from stereomolgraph.graphs.smg import StereoMolGraph
+    from stereomolgraph.graphs.crg import CondensedReactionGraph
+    from stereomolgraph.graphs.scrg import StereoCondensedReactionGraph
+
+
+def __getattr__(name: str):
+    match name:
+        case "AtomId":
+            from stereomolgraph.graphs.mg import AtomId
+
+            return AtomId
+        case "Bond":
+            from stereomolgraph.graphs.mg import Bond
+
+            return Bond
+        case "MolGraph":
+            from stereomolgraph.graphs.mg import MolGraph
+
+            return MolGraph
+        case "StereoMolGraph":
+            from stereomolgraph.graphs.smg import StereoMolGraph
+
+            return StereoMolGraph
+        case "CondensedReactionGraph":
+            from stereomolgraph.graphs.crg import CondensedReactionGraph
+
+            return CondensedReactionGraph
+        case "StereoCondensedReactionGraph":
+            from stereomolgraph.graphs.scrg import StereoCondensedReactionGraph
+
+            return StereoCondensedReactionGraph
+        
+        case "__version__":
+            from importlib.metadata import version
+
+            return version("stereomolgraph")
+
+        case _:
+            raise AttributeError(f"module has no attribute {name}")
