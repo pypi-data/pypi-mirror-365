4ff03b39a7cd4c625b2d7ce5db7a2766f6300b64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:29 +0000

    Add package CFstreamparser/0.1.1/cfstreamparser-0.1.1-py3-none-any.whl

---
+++ packages/CFstreamparser/cfstreamparser-0.1.1-py3-none-any.whl/__init__.py
@@ -0,0 +1,2 @@
+from .parser import parse_stream_file, ParsedStream, StreamChunk, Crystal, Geometry, UnitCell
+__all__ = ["parse_stream_file","ParsedStream","StreamChunk","Crystal","Geometry","UnitCell"]
