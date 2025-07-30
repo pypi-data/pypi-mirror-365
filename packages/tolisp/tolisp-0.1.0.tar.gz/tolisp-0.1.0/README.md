e48f5391baa32fb0340dccc8237022bd1a8c45d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:21 +0000

    Add package tolisp/0.1.0/tolisp-0.1.0.tar.gz

---
+++ packages/tolisp/tolisp-0.1.0.tar.gz/tolisp-0.1.0/README.md
@@ -0,0 +1,37 @@
+# toLisp
+
+Convert structured YAML or JSON files into Lisp style S-expressions.
+
+### Installation
+
+```bash
+pip install tolisp
+```
+
+### Usage
+
+```bash
+tolisp < yaml or json file input >
+```
+
+## Run Locally
+
+Clone the repository:
+
+```bash
+git clone https://github.com/riAssinstAr/2Lisp
+```
+
+Build the package:
+
+```bash
+python setup.py sdist bdist_wheel
+```
+
+Install:
+
+```bash
+pip install .\dist\*.whl
+```
+
+NOTE: This project was a part of LFX mentorship task submission under RISC-V.
---
+++ packages/tolisp/tolisp-0.1.0.tar.gz/tolisp-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tolisp",
+    version="0.1.0",
+    packages=find_packages(),
+    author="Ankit Kumar Ravi",
+    description="Convert YAML/JSON files to Lisp style S-expressions",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/riAssinstAr",
+    license="MIT",
+    python_requires=">=3.7",
+    install_requires=["pyyaml"],
+    entry_points={
+        "console_scripts": [
+            "tolisp=tolisp.main:main"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
