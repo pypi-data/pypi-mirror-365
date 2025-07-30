a0273d2b4a2b4cff112a71b726f1373327a62439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:32 +0000

    Add package AutoGridPy/0.0.0/autogridpy-0.0.0.tar.gz

---
+++ packages/AutoGridPy/autogridpy-0.0.0.tar.gz/autogridpy-0.0.0/src/autogrid/__init__.py
@@ -0,0 +1,6 @@
+"""Python wrapper for [AutoDock](https://autodock.scripps.edu/)'s AutoGrid4.
+
+References
+----------
+- [AutoDock User Guide](https://autodock.scripps.edu/wp-content/uploads/sites/56/2022/04/AutoDock4.2.6_UserGuide.pdf)
+"""
