6ed29c87ee0564d38dbb17a840ec15246d579b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:26 +0000

    Add package BeamLogger/0.1.3/BeamLogger-0.1.3.tar.gz

---
+++ packages/BeamLogger/BeamLogger-0.1.3.tar.gz/BeamLogger-0.1.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="BeamLogger",
+    version="0.1.3",  # Updated version
+    author="Bhaskar",
+    author_email="toolscord.supp@gmail.com",
+    description="A stylish and colorful Python logging module with customizable console output",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bhaskarsaikia-17/BeamLogger",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "colorama",
+        "pystyle",
+    ],
+)
+
+# Crafted With <3 By Bhaskar 
