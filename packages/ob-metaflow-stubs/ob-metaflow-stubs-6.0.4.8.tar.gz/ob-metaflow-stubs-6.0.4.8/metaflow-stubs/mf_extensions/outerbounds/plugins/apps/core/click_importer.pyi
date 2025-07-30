7565cdfe7d7a53564d3ff7343852e675035967d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:05:41 +0000

    Add package ob-metaflow-stubs/6.0.4.8/ob-metaflow-stubs-6.0.4.8.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.4.8.tar.gz/ob-metaflow-stubs-6.0.4.8/metaflow-stubs/mf_extensions/outerbounds/plugins/apps/core/click_importer.pyi
@@ -0,0 +1,12 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.21.5+obcheckpoint(0.2.4);ob(v1)                                                   #
+# Generated on 2025-07-29T18:33:43.948698                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from ......_vendor import click as metaflow_click
+from ......_vendor import click as click
+
---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.4.8.tar.gz/ob-metaflow-stubs-6.0.4.8/metaflow-stubs/plugins/gcp/gs_exceptions.pyi
@@ -0,0 +1,18 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.21.5+obcheckpoint(0.2.4);ob(v1)                                                   #
+# Generated on 2025-07-29T18:33:43.959409                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+import metaflow
+import typing
+if typing.TYPE_CHECKING:
+    import metaflow.exception
+
+from ...exception import MetaflowException as MetaflowException
+
+class MetaflowGSPackageError(metaflow.exception.MetaflowException, metaclass=type):
+    ...
+
