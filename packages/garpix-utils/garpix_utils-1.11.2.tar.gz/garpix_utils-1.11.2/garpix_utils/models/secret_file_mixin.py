7b16b60db58a9105f3182988a152785d95044585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:21 +0000

    Add package garpix-utils/1.11.2/garpix_utils-1.11.2.tar.gz

---
+++ packages/garpix-utils/garpix_utils-1.11.2.tar.gz/garpix_utils-1.11.2/garpix_utils/models/secret_file_mixin.py
@@ -0,0 +1,11 @@
+from django.db import models
+from garpix_utils.file.file_field import UploadTo, secret_file_storage
+from garpix_utils.string import get_uuid4_hash
+
+
+class SecretFileMixin(models.Model):
+    file = models.FileField(verbose_name='Файл', upload_to=UploadTo('file'), storage=secret_file_storage)
+    share_hash = models.CharField(max_length=32, null=True, default=get_uuid4_hash, blank=True)
+
+    class Meta:
+        abstract = True
---
+++ packages/garpix-utils/garpix_utils-1.11.2.tar.gz/garpix_utils-1.11.2/garpix_utils/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+from os import path
+
+
+here = path.join(path.abspath(path.dirname(__file__)), 'garpix_utils')
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='garpix_utils',
+    version='1.11.2',
+    description='',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/garpixcms/garpix_utils',
+    author='Garpix LTD',
+    author_email='info@garpix.com',
+    license='MIT',
+    packages=find_packages(exclude=['testproject', 'testproject.*']),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Programming Language :: Python :: 3.8',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Django >= 1.11, < 5',
+        'django-uuslug >= 1.2.0',
+        'django-polymorphic-tree-for-garpix-page >= 2.2.2',
+        'djangorestframework >= 3.13.0',
+        'cef-logger >= 1.0.0'
+    ],
+)
