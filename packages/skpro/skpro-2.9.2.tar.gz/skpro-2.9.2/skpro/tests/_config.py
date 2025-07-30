c758557dc778dcc58c9f756534d86abc11cbe2a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:09 +0000

    Add package skpro/2.9.2/skpro-2.9.2.tar.gz

---
+++ packages/skpro/skpro-2.9.2.tar.gz/skpro-2.9.2/skpro/tests/_config.py
@@ -0,0 +1,14 @@
+"""Test configs."""
+
+# list of str, names of estimators to exclude from testing
+# WARNING: tests for these estimators will be skipped
+EXCLUDE_ESTIMATORS = [
+    "DummySkipped",
+    "ClassName",  # exclude classes from extension templates
+]
+
+
+EXCLUDED_TESTS = {
+    "GLMRegressor": ["test_online_update"],  # see 497
+    "Hurdle": ["test_ppf_and_cdf"],  # see 556
+}
