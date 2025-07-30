d7364167b75f024dea96abdcca79aef5e7aeef84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 21:05:53 +0000

    Add package vsai/1.3.8/vsai-1.3.8.tar.gz

---
+++ packages/vsai/vsai-1.3.8.tar.gz/vsai-1.3.8/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+setuptools.setup(
+    name="vsai",
+    version="1.3.8",
+    packages=setuptools.find_packages(),
+    url="https://github.com/Virtosync/vsai",
+    author="Virtosync",
+    license="Virto License",
+    author_email="virtosync@gmail.com",
+    description="A new AI assistant for your terminal",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "nltk",
+        "textblob",
+        "requests",
+        "pyttsx3",
+        "SpeechRecognition",
+        "cryptography",
+    ],
+    entry_points={
+        "console_scripts": [
+            "vsai=vsai.bot:start"
+        ]
+    }
+)
