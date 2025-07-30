f53dc8f21126b5bcf163a0a2d92849ca8ea4159e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:42 +0000

    Add package open-geodata/23.0.16/open_geodata-23.0.16.tar.gz

---
+++ packages/open-geodata/open_geodata-23.0.16.tar.gz/open_geodata-23.0.16/open_geodata/providers/br/ibge/ibge_sidra.py
@@ -0,0 +1,56 @@
+"""
+sssss
+
+"""
+
+import sidrapy
+import pandas as pd
+
+
+def get_estimated_population(id_municipio):
+    """
+    Retorna a população estimada
+    :param id_municipio:
+    :return:
+    """
+    # Get Table
+    df = sidrapy.get_table(
+        table_code='6579',
+        territorial_level='6',
+        ibge_territorial_code=id_municipio,
+        period='all',
+        header='n',
+    )
+
+    # Dict
+    dict_col = {
+        'D1C': 'id_municipio',
+        'D1N': 'municipio_nome',
+        'V': 'n_habitantes',
+        'D2N': 'ano'
+    }
+
+    # Rename Columns
+    df.rename(
+        dict_col,
+        axis=1,
+        inplace=True
+    )
+
+    # Select Columns
+    df = df[[v for k, v in dict_col.items()]]
+
+    # Adjust Columns
+    df.sort_values(by=['ano'], inplace=True)
+    df['id_municipio'] = pd.to_numeric(df['id_municipio'], errors='coerce')
+    df['n_habitantes'] = pd.to_numeric(df['n_habitantes'], errors='coerce')
+    df['ano'] = pd.to_numeric(df['ano'], errors='coerce')
+
+    # Results
+    return df
+
+
+if __name__ == '__main__':
+    cod_ibge = '3526902'  # Limeira
+    pop = get_estimated_population(cod_ibge)
+    print(pop)
