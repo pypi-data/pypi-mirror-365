3477db8831dc0606298854ed18bb4e2fa1200bcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:33 +0000

    Add package aiobale/0.1.1/aiobale-0.1.1.tar.gz

---
+++ packages/aiobale/aiobale-0.1.1.tar.gz/aiobale-0.1.1/aiobale/methods/auth/__init__.py
@@ -0,0 +1,10 @@
+from .start_phone_auth import StartPhoneAuth
+from .validate_code import ValidateCode
+from .validate_password import ValidatePassword
+
+
+__all__ = (
+    "StartPhoneAuth",
+    "ValidateCode",
+    "ValidatePassword",
+)
