6e29b4b34ad48e588f5b615f89cd287d77b24746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:39 +0000

    Add package baai-datacube/0.1.2/baai_datacube-0.1.2-cp39-cp39-macosx_10_12_x86_64.whl

---
+++ packages/baai-datacube/baai_datacube-0.1.2-cp39-cp39-macosx_10_12_x86_64.whl/baai_datacube/meta.py
@@ -0,0 +1,27 @@
+import json
+
+import requests
+
+from .config import DATACUBE_HOME
+
+def download_meta(dataset_id: str, host="http://127.0.0.1:30201"):
+    login_api = f"{host}/auth/user-access-key-login"
+
+    config_path = DATACUBE_HOME / "config.json"
+
+    with open(config_path) as f:
+        config = json.load(f)
+
+    access_key = config.get("access_key")
+    secret_key = config.get("secret_key")
+
+    resp_login = requests.post(login_api, json={"accessKey": access_key, "secretKey": secret_key})
+    token = resp_login.json().get("data").get("token")
+
+
+
+    meta_api = f"{host}/storage/download/{dataset_id}"
+    resp_meta = requests.get(meta_api, headers={"Authorization": f"Bearer {token}"})
+    return resp_meta.text
+
+
