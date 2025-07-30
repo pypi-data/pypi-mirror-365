eea3db25c0494ec0c3f6c11d3011c69efd26b9f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 23:06:40 +0000

    Add package ap-cfg/1.0.0.20250730/ap_cfg-1.0.0.20250730-py3-none-any.whl

---
+++ packages/ap-cfg/ap_cfg-1.0.0.20250730-py3-none-any.whl/ap_cfg/do.py
@@ -0,0 +1,18 @@
+import sys
+import ut_com.dec as dec
+from ut_com.com import Com
+from .parms import Parms
+from .task import Task
+
+
+class Do:
+
+    @classmethod
+    @dec.handle_error
+    @dec.timer
+    def do(cls) -> None:
+        Task.do(Com.sh_kwargs(cls, Parms.d_eq, sys.argv))
+
+
+if __name__ == "__main__":
+    Do.do()
