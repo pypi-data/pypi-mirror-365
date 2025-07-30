13556c90e23fae77fd88cdd90c16e18a60947646
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:05 +0000

    Add package daft-lts/0.5.14/daft_lts-0.5.14-cp39-abi3-win_amd64.whl

---
+++ packages/daft-lts/daft_lts-0.5.14-cp39-abi3-win_amd64.whl/daft/runners/runner_io.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from abc import abstractmethod
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from daft.daft import FileFormatConfig, FileInfos, IOConfig
+
+
+class RunnerIO:
+    """Reading and writing data from the Runner.
+
+    This is an abstract class and each runner must write their own implementation.
+    """
+
+    @abstractmethod
+    def glob_paths_details(
+        self,
+        source_path: list[str],
+        file_format_config: FileFormatConfig | None = None,
+        io_config: IOConfig | None = None,
+    ) -> FileInfos:
+        """Globs the specified filepath to construct a FileInfos object containing file and dir metadata.
+
+        Args:
+            source_path (str): path to glob
+
+        Returns:
+            FileInfo: The file infos for the globbed paths.
+        """
+        raise NotImplementedError()
