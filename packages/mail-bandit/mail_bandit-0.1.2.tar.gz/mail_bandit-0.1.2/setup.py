2f2859a0a13ae75950a89c0711f46066a5521ca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:24 +0000

    Add package mail-bandit/0.1.2/mail_bandit-0.1.2.tar.gz

---
+++ packages/mail-bandit/mail_bandit-0.1.2.tar.gz/mail_bandit-0.1.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mail_bandit",
+    version="0.1.2",  # 🚨 increment this every time you re-upload
+    author="Jabez Gershon Aldrin",
+    author_email="Jabez123jaze@gmail.com",
+    description="Mail Bandit is a fast, lightweight tool that classifies emails into five categories using DistilBERT and XGBoost.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/mail-bandit",  # Replace if needed
+    packages=find_packages(),
+    install_requires=[
+        "transformers",
+        "scikit-learn",
+        "pandas",
+        "joblib",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    entry_points={
+        'console_scripts': [
+            'mailbandit = mailbandit.main:main',  # if main.py has a main()
+        ],
+    },
+)
