6ec6ef351f608828de3ceb01b5e933915afa1120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:33 +0000

    Add package ggx/0.2.2/ggx-0.2.2-py3-none-any.whl

---
+++ packages/ggx/ggx-0.2.2-py3-none-any.whl/ggx/social/social.py
@@ -0,0 +1,57 @@
+from ..client.game_client import GameClient
+from loguru import logger
+
+
+
+
+
+class Social(GameClient):
+    
+    
+    
+    
+    async def send_player_sms(
+        self,
+        player_name: str,
+        sms_title: str,
+        sms_text: str,
+        sync: bool = True
+    ) -> dict | bool:
+        
+        try:
+            await self.send_json_message(
+                "sms",
+                {
+                    "RN": player_name,
+                    "MH": sms_title,
+                    "TXT": sms_text
+                }
+            )
+            if sync:
+                response = await self.wait_for_response("sms")
+                return response
+            return True
+
+        except Exception as e:
+            logger.error(e)
+            
+            
+    async def delete_message(self, message_id, sync = True):
+        
+        try:
+            
+            await self.send_json_message(
+                "dms", {
+                    "MID": message_id
+                }
+            )
+            
+            if sync:
+                response = self.pending_get_status("dms")
+                return response
+            logger.info(f'Message {message_id} removed!')
+            return True
+            
+        except Exception as e:
+            logger.error(e) 
+            return False
