45fafcde6926f4bcd21c1f9c807a4cd2b8c7ba3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:52 +0000

    Add package adpkg/0.1.1/adpkg-0.1.1.tar.gz

---
+++ packages/adpkg/adpkg-0.1.1.tar.gz/adpkg-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='adpkg', 
+    version='0.1.1', 
+    author='Amit Dutta',
+    author_email='amitdutta4255@gmail.com',
+    description='A package for advanced mathematical calculations including interest, custom math, and geometry.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/notamitgamer', 
+    packages=find_packages(), 
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License', 
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Mathematics',
+    ],
+    python_requires='>=3.6', 
+)
