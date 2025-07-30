836861edb461c8339579e1e2a2d53478f04f19dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:18 +0000

    Add package django-lms-geotool/0.4.0/django_lms_geotool-0.4.0-py3-none-any.whl

---
+++ packages/django-lms-geotool/django_lms_geotool-0.4.0-py3-none-any.whl/django_lms_geotool/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("convert/", views.convert_html, name='convert'),
+    path("matrix/", views.matrix_html, name='matrix'),
+    path("dimsum/", views.dimsum_html, name='dimsum'),
+    path("translation/", views.translation_html, name='translation')
+]
