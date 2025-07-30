df6b89200c4a5d47577f857692db142843fb06ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:33 +0000

    Add package handwriting-sample/1.0.6/handwriting_sample-1.0.6-py3-none-any.whl

---
+++ packages/handwriting-sample/handwriting_sample-1.0.6-py3-none-any.whl/handwriting_sample/reader/exceptions.py
@@ -0,0 +1,37 @@
+from handwriting_sample.base import HandwritingDataBase
+
+
+class ReaderException(Exception, HandwritingDataBase):
+    """ Base class for TransformerException """
+
+    def __init__(self, message):
+        super(ReaderException, self).__init__(message)
+        self.log(message)
+
+
+class HTMLPointerNotAllowedException(ReaderException):
+    """ Base class for TransformerException """
+
+    def __init__(self, message):
+        super(HTMLPointerNotAllowedException, self).__init__(message)
+
+
+class HTMLDataMissingColumn(ReaderException):
+    """ Base class for TransformerException """
+
+    def __init__(self, message):
+        super(HTMLDataMissingColumn, self).__init__(message)
+
+
+class HTMLDataMColumnMissingValues(ReaderException):
+    """ Base class for TransformerException """
+
+    def __init__(self, message):
+        super(HTMLDataMColumnMissingValues, self).__init__(message)
+
+
+class HTMLDataTransformationArgumentNotAllowed(ReaderException):
+    """ Base class for TransformerException """
+
+    def __init__(self, message):
+        super(HTMLDataTransformationArgumentNotAllowed, self).__init__(message)
