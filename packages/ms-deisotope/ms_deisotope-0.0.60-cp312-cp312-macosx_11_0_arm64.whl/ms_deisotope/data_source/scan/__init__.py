f95db63c7e6f60d5b997a76cf386a6ec254761e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 21:06:06 +0000

    Add package ms-deisotope/0.0.60/ms_deisotope-0.0.60-cp312-cp312-macosx_11_0_arm64.whl

---
+++ packages/ms-deisotope/ms_deisotope-0.0.60-cp312-cp312-macosx_11_0_arm64.whl/ms_deisotope/data_source/scan/__init__.py
@@ -0,0 +1,51 @@
+
+from .base import (
+    ScanBase, ScanBunch, ChargeNotProvided,
+    DEFAULT_CHARGE_WHEN_NOT_RESOLVED, RawDataArrays,
+    PrecursorInformation
+)
+
+from .scan import (
+    Scan, ProcessedScan,
+    WrappedScan, AveragedScan)
+
+from .scan_iterator import (
+    _ScanIteratorImplBase, _SingleScanIteratorImpl,
+    _FakeGroupedScanIteratorImpl, _GroupedScanIteratorImpl,
+    _InterleavedGroupedScanIteratorImpl)
+
+from .loader import (
+    ScanDataSource, ScanIterator, RandomAccessScanSource,
+    ScanFileMetadataBase)
+
+from .proxy import ScanProxyContext
+
+from .mobility_frame import (
+    IonMobilityFrame,
+    ProcessedIonMobilityFrame,
+    IonMobilitySource,
+    Generic3DIonMobilityFrameSource,
+    FramedIonMobilityFrameSource,
+    IonMobilitySourceRandomAccessFrameSource,
+    RawDataArrays3D
+)
+
+__all__ = [
+    "ScanBunch", "Scan", "ProcessedScan",
+    "PrecursorInformation", "WrappedScan", "AveragedScan",
+    "ScanBase", "RawDataArrays", "ChargeNotProvided",
+    "DEFAULT_CHARGE_WHEN_NOT_RESOLVED",
+
+    "_ScanIteratorImplBase", "_SingleScanIteratorImpl",
+    "_FakeGroupedScanIteratorImpl", "_GroupedScanIteratorImpl",
+    "_InterleavedGroupedScanIteratorImpl",
+
+    "ScanDataSource", "ScanIterator", "RandomAccessScanSource",
+    "ScanFileMetadataBase", "ScanProxyContext",
+
+    "IonMobilityFrame", "ProcessedIonMobilityFrame",
+    "IonMobilitySource", "Generic3DIonMobilityFrameSource",
+    "FramedIonMobilityFrameSource",
+    "IonMobilitySourceRandomAccessFrameSource",
+    "RawDataArrays3D",
+]
