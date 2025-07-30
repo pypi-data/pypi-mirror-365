d2c03698e63163fdc2d0024e44b875bad9d29dc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 23:06:22 +0000

    Add package wujing/2.7.3/wujing-2.7.3.tar.gz

---
+++ packages/wujing/wujing-2.7.3.tar.gz/wujing-2.7.3/push.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -eux
+
+uv publish --username=__token__ --password=pypi-AgEIcHlwaS5vcmcCJDY4MjU5ZDU1LTFmNmEtNDBkZC1hZDViLTBmNjI5MGEwODNkNgACDlsxLFsid3VqaW5nIl1dAAIsWzIsWyI0NGUwY2QxNC00ODM0LTQzN2MtYTkyNi1kZjNjYTk3OGEyNzUiXV0AAAYgv1j-0ZsRpBEl8tpFylPvO1HZ41HaFNBA-JHP_zFRCak
