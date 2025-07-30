87561c4290a97acb9c65f6b732d70a1c16fac08d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 23:07:15 +0000

    Add package konfigurator/0.2.0/konfigurator-0.2.0-py3-none-any.whl

---
+++ packages/konfigurator/konfigurator-0.2.0-py3-none-any.whl/konfigurator/__init__.py
@@ -0,0 +1,15 @@
+from .utils import (  # noqa: F401
+    FieldReference,
+    class_from_path,
+    get_class_path,
+    instantiate_object_from_config,
+    load_config,
+)
+
+__all__ = [
+    "get_class_path",
+    "class_from_path",
+    "instantiate_object_from_config",
+    "load_config",
+    "FieldReference",
+]
