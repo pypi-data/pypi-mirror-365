4df0d11d093d03661269928b6af59f6ae380f492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:46 +0000

    Add package otary/0.2.1/otary-0.2.1.tar.gz

---
+++ packages/otary/otary-0.2.1.tar.gz/otary-0.2.1/otary/__init__.py
@@ -0,0 +1,47 @@
+"""
+Base init file to import all base modules to be used by end-users.
+"""
+
+__all__ = [
+    "Image",
+    "EllipsesRender",
+    "CirclesRender",
+    "PointsRender",
+    "SegmentsRender",
+    "PolygonsRender",
+    "LinearSplinesRender",
+    "OcrSingleOutputRender",
+    "Ellipse",
+    "Circle",
+    "Point",
+    "Segment",
+    "Polygon",
+    "Rectangle",
+    "Triangle",
+    "LinearSpline",
+    "Vector",
+    "VectorizedLinearSpline",
+]
+
+from otary.image import (
+    Image,
+    EllipsesRender,
+    CirclesRender,
+    PointsRender,
+    SegmentsRender,
+    PolygonsRender,
+    LinearSplinesRender,
+    OcrSingleOutputRender,
+)
+from otary.geometry import (
+    Ellipse,
+    Circle,
+    Point,
+    Segment,
+    Polygon,
+    Rectangle,
+    Triangle,
+    LinearSpline,
+    Vector,
+    VectorizedLinearSpline,
+)
