0e699084306b2c8a69b01104a03cb552acb9c8e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:07 +0000

    Add package mdis-sec/0.1.0/mdis_sec-0.1.0-py3-none-any.whl

---
+++ packages/mdis-sec/mdis_sec-0.1.0-py3-none-any.whl/MDIS/__init__.py
@@ -0,0 +1,13 @@
+"""
+MDIS - Malware Detection and Identification System
+--------------------------------------------------
+
+A minimal yet expressive malware identifier system,
+designed for fast parsing, clarity, and extensibility.
+"""
+
+from .mdis import (
+    MDISParser,
+    MDISParserError,
+    DICTIONARIES,
+)
