10a3b4f926f96dc33cca094982e3c8627bc65cd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:05:07 +0000

    Add package graphql-http-server/1.5.3/graphql_http_server-1.5.3-py3-none-any.whl

---
+++ packages/graphql-http-server/graphql_http_server-1.5.3-py3-none-any.whl/tests/app.py
@@ -0,0 +1,22 @@
+from graphql_api import GraphQLAPI
+from graphql_http_server import GraphQLHTTPServer
+
+api = GraphQLAPI()
+
+
+@api.type(is_root_type=True)
+class HelloWorld:
+    @api.field
+    def hello_world(self, name: str) -> str:
+        return f"Hello {name}!"
+
+
+default_query = "query HelloQuery($name: String!){  helloWorld(name: $name) }"
+
+server = GraphQLHTTPServer.from_api(
+    api=api,
+    graphiql_default_query=default_query
+)
+
+if __name__ == "__main__":
+    server.run(port=3501)
