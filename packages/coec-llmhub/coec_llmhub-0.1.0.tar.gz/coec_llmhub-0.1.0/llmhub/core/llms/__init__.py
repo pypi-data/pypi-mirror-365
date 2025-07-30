f9f88e6db66443e88524bb3593e46abd570a4658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:11:54 +0000

    Add package coec-llmhub/0.1.0/coec_llmhub-0.1.0.tar.gz

---
+++ packages/coec-llmhub/coec_llmhub-0.1.0.tar.gz/coec_llmhub-0.1.0/llmhub/core/llms/__init__.py
@@ -0,0 +1,22 @@
+from typing import List, Literal
+
+from .gemini import GeminiClientAsync
+from .template.response import BaseGenerationModel
+
+CLIENT_FACTORY_ASYNC = {"gemini": GeminiClientAsync}
+
+
+class LLMClientAsync:
+    def __init__(self, providers: List[Literal["gemini"]] = ["gemini"]):
+        assert isinstance(providers, list)
+        self.clients = {}
+        for provider in providers:
+            if provider in CLIENT_FACTORY_ASYNC.keys():
+                self.clients[provider] = CLIENT_FACTORY_ASYNC[provider]()
+            else:
+                pass
+                # TODO Warn user here
+
+    async def create_generation(self, input_model: BaseGenerationModel, provider: str):
+        assert provider in self.clients.keys()
+        return await self.clients[provider].create_generation(input_model)
