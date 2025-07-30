5dbe7e4a161898e0acd7cbbee0f3557c64df75bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:29 +0000

    Add package pygpt-net/2.5.74/pygpt_net-2.5.74.tar.gz

---
+++ packages/pygpt-net/pygpt_net-2.5.74.tar.gz/pygpt_net-2.5.74/src/pygpt_net/provider/agents/base.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ================================================== #
+# This file is a part of PYGPT package               #
+# Website: https://pygpt.net                         #
+# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
+# MIT License                                        #
+# Created By  : Marcin Szczygliński                  #
+# Updated Date: 2024.12.14 22:00:00                  #
+# ================================================== #
+
+from typing import Dict, Any
+
+
+class BaseAgent:
+    def __init__(self, *args, **kwargs):
+        self.id = ""
+        self.mode = ""
+        self.name = ""
+
+    def get_mode(self) -> str:
+        """
+        Return Agent mode
+
+        :return: Agent mode
+        """
+        return self.mode
+
+    def get_agent(self, window, kwargs: Dict[str, Any]):
+        """
+        Return Agent provider instance
+
+        :param window: window instance
+        :param kwargs: keyword arguments
+        :return: Agent provider instance
+        """
+        pass
