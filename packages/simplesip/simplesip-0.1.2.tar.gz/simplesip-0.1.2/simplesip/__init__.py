6dd08862c7b8e149b180c6bf8bffa2808673d3e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 22:05:46 +0000

    Add package simplesip/0.1.2/simplesip-0.1.2.tar.gz

---
+++ packages/simplesip/simplesip-0.1.2.tar.gz/simplesip-0.1.2/simplesip/__init__.py
@@ -0,0 +1,13 @@
+"""
+Simple SIP Client Library (simplesip)
+
+A Python library for SIP (Session Initiation Protocol) communication with RTP audio streaming capabilities.
+"""
+
+__version__ = "0.1.2"
+__author__ = "Awais Khan"
+__email__ = "contact@awaiskhan.com.pk"
+
+from .client import SimpleSIPClient
+
+__all__ = ["SimpleSIPClient"]
