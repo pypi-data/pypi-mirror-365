98d7300060b2a05add11a1bf3fe23b3f8de269b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 22:05:29 +0000

    Add package yak-server/0.55.0/yak_server-0.55.0.tar.gz

---
+++ packages/yak-server/yak_server-0.55.0.tar.gz/yak_server-0.55.0/yak_server/v1/models/users.py
@@ -0,0 +1,62 @@
+from pydantic import UUID4, BaseModel, ConfigDict
+
+
+class SignupIn(BaseModel):
+    name: str
+    first_name: str
+    last_name: str
+    password: str
+
+    model_config = ConfigDict(extra="forbid")
+
+
+class SignupOut(BaseModel):
+    id: UUID4
+    name: str
+    access_token: str
+
+    model_config = ConfigDict(extra="forbid")
+
+
+class PasswordRequirementsOut(BaseModel):
+    minimum_length: int
+    uppercase: bool
+    lowercase: bool
+    digit: bool
+    no_space: bool
+
+    model_config = ConfigDict(extra="forbid")
+
+
+class LoginIn(BaseModel):
+    name: str
+    password: str
+
+    model_config = ConfigDict(extra="forbid")
+
+
+class LoginOut(BaseModel):
+    id: UUID4
+    name: str
+    access_token: str
+
+    model_config = ConfigDict(extra="forbid")
+
+
+class RefreshTokenOut(BaseModel):
+    access_token: str
+
+    model_config = ConfigDict(extra="forbid")
+
+
+class CurrentUserOut(BaseModel):
+    id: UUID4
+    name: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class ModifyUserIn(BaseModel):
+    password: str
+
+    model_config = ConfigDict(extra="forbid")
