64a84d8a554b3b9890932e951ad8121758d451ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 22:05:32 +0000

    Add package django-sti-models/0.1.4/django_sti_models-0.1.4-py3-none-any.whl

---
+++ packages/django-sti-models/django_sti_models-0.1.4-py3-none-any.whl/django_sti_models/__init__.py
@@ -0,0 +1,62 @@
+"""
+Django STI Models - Improved Single Table Inheritance for Django
+
+A modern, type-safe implementation of Single Table Inheritance (STI) for Django
+with improved monkey patching and better performance.
+"""
+
+__version__ = "0.1.4"
+__author__ = "Konrad Beck"
+__email__ = "konrad.beck@gmail.com"
+
+from .admin import (
+    TypedModelAdmin,
+    TypedModelAdminMixin,
+    TypedModelForm,
+    TypeFilter,
+    create_typed_admin_class,
+    register_typed_models,
+)
+from .exceptions import STIException
+from .fields import TypeField
+from .models import TypedModel, TypedModelManager
+from .utils import (
+    create_typed_instance,
+    filter_by_type,
+    get_type_field_value,
+    get_type_hierarchy,
+    get_type_statistics,
+    get_typed_model_classes,
+    get_typed_queryset,
+    is_typed_instance,
+    migrate_type_field,
+    validate_type_consistency,
+    validate_type_registration,
+)
+
+__all__ = [
+    # Core classes
+    "TypedModel",
+    "TypedModelManager",
+    "TypeField",
+    "STIException",
+    # Utility functions
+    "get_typed_queryset",
+    "create_typed_instance",
+    "get_type_hierarchy",
+    "validate_type_registration",
+    "get_type_field_value",
+    "is_typed_instance",
+    "get_typed_model_classes",
+    "filter_by_type",
+    "get_type_statistics",
+    "migrate_type_field",
+    "validate_type_consistency",
+    # Admin integration
+    "TypedModelAdmin",
+    "TypedModelAdminMixin",
+    "TypedModelForm",
+    "TypeFilter",
+    "register_typed_models",
+    "create_typed_admin_class",
+]
