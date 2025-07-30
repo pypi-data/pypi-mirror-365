afb9522ab2e9281b61f606feb828fca60c87b91e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:05:03 +0000

    Add package nested-mapping/0.10.2/nested_mapping-0.10.2-py3-none-any.whl

---
+++ packages/nested-mapping/nested_mapping-0.10.2-py3-none-any.whl/nested_mapping/typing.py
@@ -0,0 +1,41 @@
+from collections.abc import Sequence
+
+from ordered_set import OrderedSet
+
+TupleKey = tuple[str, ...]
+Key = str | tuple[str, ...]
+KeyLike = str | Sequence[str]
+
+
+def properkey(key: KeyLike, *, sep: str | bool | None = None) -> TupleKey:
+    if isinstance(key, str):
+        if isinstance(sep, str):
+            return tuple(key.split(sep))
+        elif sep is True:
+            return tuple(key.split("."))
+        return (key,)
+    if isinstance(key, tuple):
+        return key
+
+    return tuple(key)
+
+
+def orderedsetkey(key: KeyLike) -> OrderedSet[str]:
+    if isinstance(key, str):
+        return OrderedSet((key,))
+
+    return OrderedSet(key)
+
+
+def setkey(key: KeyLike) -> set[str]:
+    if isinstance(key, str):
+        return set((key,))
+
+    return set(key)
+
+
+def strkey(key: KeyLike, *, sep=".") -> str:
+    if isinstance(key, str):
+        return key
+
+    return sep.join(key)
