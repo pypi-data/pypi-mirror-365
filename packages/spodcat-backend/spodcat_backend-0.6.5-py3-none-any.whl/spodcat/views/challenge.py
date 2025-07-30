7058d9a8bd38632339fe4fc33c96b38b4824c54d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:14 +0000

    Add package spodcat-backend/0.6.5/spodcat_backend-0.6.5-py3-none-any.whl

---
+++ packages/spodcat-backend/spodcat_backend-0.6.5-py3-none-any.whl/spodcat/views/challenge.py
@@ -0,0 +1,19 @@
+from rest_framework.mixins import (
+    CreateModelMixin,
+    DestroyModelMixin,
+    RetrieveModelMixin,
+)
+from rest_framework.viewsets import GenericViewSet
+
+from spodcat import serializers
+from spodcat.models import Challenge
+
+
+class ChallengeViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet[Challenge]):
+    select_for_includes = {
+        "__all__": ["podcast"],
+    }
+    serializer_class = serializers.ChallengeSerializer
+
+    def get_queryset(self, *args, **kwargs):
+        return Challenge.objects.all()
