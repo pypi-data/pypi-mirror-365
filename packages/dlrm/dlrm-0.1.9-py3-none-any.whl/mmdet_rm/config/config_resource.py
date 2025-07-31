616c2d96e231ac843ebaa58f9653fabc74a597e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 03:34:36 +0000

    Add package dlrm/0.1.9/dlrm-0.1.9-py3-none-any.whl

---
+++ packages/dlrm/dlrm-0.1.9-py3-none-any.whl/mmdet_rm/config/config_resource.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass, field
+from functools import cached_property
+from pathlib import Path
+from typing import Type
+
+from mmdet_rm.settings import get_settings
+from rm import PropertyManager, DBView, ResourceDBFactory, ResourceRecord, ResourceDB, ID, NAME
+from rm.resource_db.config_manager import PathHandling_PropertyManager
+
+@dataclass
+class MainConfig_PropertyKey:
+    MAIN_CONFIG_FILE_PATH:str = "main_config_file_path"
+
+@dataclass
+class MainConfig_PropertyManager(PathHandling_PropertyManager):
+    # 데이터 셋 리소스에 대한 config를 관리하는 객체
+
+    @property
+    def config_file_path(self)->Path:
+        return self.get_as_absolute_path(MainConfig_PropertyKey.MAIN_CONFIG_FILE_PATH)
+
+    @config_file_path.setter
+    def config_file_path(self, value:Path)->None:
+        self.set_as_absolute_path(MainConfig_PropertyKey.MAIN_CONFIG_FILE_PATH, value)
+
+
+
+class MainConfigRecord(ResourceRecord[MainConfig_PropertyManager]):
+    pass
+
+
+class MainConfigDB(ResourceDB[MainConfigRecord]):
+    
+    def create(self, name:NAME)->MainConfigRecord:
+        # 실제로 
+        record = super().create(name)
+        record.property_manager.set(MainConfig_PropertyKey.MAIN_CONFIG_FILE_PATH, (record.dir_path / "main_config.py").as_posix())
+        return record
+
+class MainConfigDBView(DBView):
+    db:MainConfigDB
+
+
+@dataclass
+class MainConfig_ResourceFactory(ResourceDBFactory[MainConfig_PropertyManager, MainConfigRecord, MainConfigDB, MainConfigDBView]):
+    dir_path:Path = field(default_factory=lambda : get_settings().config_dir)
+    
+    CONFIG_MANAGER_CLASS:Type[PropertyManager] = MainConfig_PropertyManager
+    RECORD_CLASS:Type[ResourceRecord] = MainConfigRecord
+    DB_CLASS:Type[ResourceDB] = MainConfigDB
+    VIEW_CLASS:Type[MainConfigDBView] = MainConfigDBView
+
+    CONFIG_NAME:str = field(default="properties")
