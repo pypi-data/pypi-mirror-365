c011d90c8f493eb852a2a0fb16a5f73f9ef32f9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 03:34:31 +0000

    Add package huibiao-framework/0.1.38/huibiao_framework-0.1.38-py3-none-any.whl

---
+++ packages/huibiao-framework/huibiao_framework-0.1.38-py3-none-any.whl/huibiao_framework/client/__init__.py
@@ -0,0 +1,5 @@
+from .huize_qwen32b_awq_client import HuiZeQwen32bQwqClient
+from .minio_client import MinIOClient
+from .embed_client import EmbedClient
+
+__all__ = ["MinIOClient", "HuiZeQwen32bQwqClient", "EmbedClient"]
