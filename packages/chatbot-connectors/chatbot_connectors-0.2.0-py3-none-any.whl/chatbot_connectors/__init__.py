bc45e7b986c6b4f8354771731d8945f77d65cdfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 03:35:00 +0000

    Add package chatbot-connectors/0.2.0/chatbot_connectors-0.2.0-py3-none-any.whl

---
+++ packages/chatbot-connectors/chatbot_connectors-0.2.0-py3-none-any.whl/chatbot_connectors/__init__.py
@@ -0,0 +1,45 @@
+"""Chatbot Connectors Library.
+
+A library for connecting to various chatbot APIs.
+"""
+
+# Import registry to auto-register chatbots
+from chatbot_connectors import registry
+from chatbot_connectors.core import (
+    Chatbot,
+    ChatbotConfig,
+    ChatbotResponse,
+    EndpointConfig,
+    Parameter,
+    RequestMethod,
+    ResponseProcessor,
+    SimpleTextProcessor,
+)
+from chatbot_connectors.exceptions import (
+    ConnectorAuthenticationError,
+    ConnectorConfigurationError,
+    ConnectorConnectionError,
+    ConnectorError,
+    ConnectorResponseError,
+)
+from chatbot_connectors.factory import ChatbotFactory
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "Chatbot",
+    "ChatbotConfig",
+    "ChatbotFactory",
+    "ChatbotResponse",
+    "ConnectorAuthenticationError",
+    "ConnectorConfigurationError",
+    "ConnectorConnectionError",
+    "ConnectorError",
+    "ConnectorResponseError",
+    "EndpointConfig",
+    "Parameter",
+    "RequestMethod",
+    "ResponseProcessor",
+    "SimpleTextProcessor",
+    "registry",
+]
