70374e0da2564abfcd516a20d99de03ac17ff59d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 04:20:08 +0000

    Add package st-conceptmap-cs/1.0.2.1/st_conceptmap_cs-1.0.2.1.tar.gz

---
+++ packages/st-conceptmap-cs/st_conceptmap_cs-1.0.2.1.tar.gz/st_conceptmap_cs-1.0.2.1/conceptmap_component/app.py
@@ -0,0 +1,28 @@
+import streamlit as st 
+from conceptmap_component import conceptmap_component, parse_conceptmap
+
+cm_data = {
+    "elements": [
+        { "data": { "id": "a", "label": "Node A" } },
+        { "data": { "id": "b", "label": "Node B" } },
+        { "data": { "source": "a", "target": "b", "label": "A to B" } }
+    ]
+}
+
+st.header("Scaffolding")
+
+st.subheader("Task 1")
+st.write("Create a concept map!")
+
+submit_requested = st.button("Submit")
+
+cm_response = conceptmap_component(cm_data=cm_data, submit_request=submit_requested, key="task_1")
+
+if cm_response:
+    st.write(parse_conceptmap(cm_response))
+    st.markdown("---")
+
+    st.subheader("Task 2")
+    st.write("Response: ")
+    st.write("Make changes in your concept map based on the response")
+    conceptmap_component(cm_data=cm_response, key="task_2")
