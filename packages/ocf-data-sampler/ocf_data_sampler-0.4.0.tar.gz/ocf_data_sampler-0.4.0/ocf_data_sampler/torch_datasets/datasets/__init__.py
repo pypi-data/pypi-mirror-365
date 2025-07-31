df9d932e20711d2cd0ccdb0b8426ba46b898475b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 04:20:07 +0000

    Add package ocf-data-sampler/0.4.0/ocf_data_sampler-0.4.0.tar.gz

---
+++ packages/ocf-data-sampler/ocf_data_sampler-0.4.0.tar.gz/ocf_data_sampler-0.4.0/ocf_data_sampler/torch_datasets/datasets/__init__.py
@@ -0,0 +1,6 @@
+from .pvnet_uk import PVNetUKRegionalDataset, PVNetUKConcurrentDataset
+
+from .site import (
+    convert_netcdf_to_numpy_sample,
+    SitesDataset
+)
---
+++ packages/ocf-data-sampler/ocf_data_sampler-0.4.0.tar.gz/ocf_data_sampler-0.4.0/ocf_data_sampler/torch_datasets/sample/site.py
@@ -0,0 +1,39 @@
+"""PVNet Site sample implementation for netCDF data handling and conversion."""
+
+import xarray as xr
+from typing_extensions import override
+
+from ocf_data_sampler.numpy_sample.common_types import NumpySample
+from ocf_data_sampler.torch_datasets.datasets.site import convert_netcdf_to_numpy_sample
+
+from .base import SampleBase
+
+
+class SiteSample(SampleBase):
+    """Handles PVNet site specific netCDF operations."""
+
+    def __init__(self, data: xr.Dataset) -> None:
+        """Initializes the SiteSample object with the given xarray Dataset."""
+        if not isinstance(data, xr.Dataset):
+            raise TypeError(f"Data must be xarray Dataset - Found type {type(data)}")
+        self._data = data
+
+    @override
+    def to_numpy(self) -> NumpySample:
+        return convert_netcdf_to_numpy_sample(self._data)
+
+    @override
+    def save(self, path: str) -> None:
+        # Saves as NetCDF
+        self._data.to_netcdf(path, mode="w", engine="h5netcdf")
+
+    @classmethod
+    @override
+    def load(cls, path: str) -> "SiteSample":
+        # Loads from NetCDF
+        return cls(xr.open_dataset(path, decode_timedelta=False))
+
+    @override
+    def plot(self) -> None:
+        # TODO - placeholder for now
+        raise NotImplementedError("Plotting not yet implemented for SiteSample")
