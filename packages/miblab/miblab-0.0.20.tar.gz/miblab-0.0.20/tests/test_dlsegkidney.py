e4413e1449390d1ffa41c13263b895162dd0dcbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 04:20:53 +0000

    Add package miblab/0.0.20/miblab-0.0.20.tar.gz

---
+++ packages/miblab/miblab-0.0.20.tar.gz/miblab-0.0.20/tests/test_dlsegkidney.py
@@ -0,0 +1,45 @@
+import os
+import shutil
+
+import dbdicom as db
+import numpy as np
+
+from miblab import kidney_pc_dixon
+from miblab import zenodo_fetch
+
+
+
+def test_kidney_pc_dixon():
+    
+    tmp_dir = os.path.join(os.getcwd(), 'tmp')
+    os.makedirs(tmp_dir, exist_ok=True)
+
+    testdata = 'test_data_post_contrast_dixon.zip'
+    testdatadoi = '15489381'
+
+    # Download ZIP file to temp directory
+    folder = zenodo_fetch(testdata, tmp_dir, testdatadoi, extract=True)
+
+    # Read DICOM data
+    study = db.studies(folder)[0]
+    series = [
+        'Dixon_post_contrast_out_phase', 
+        'Dixon_post_contrast_in_phase', 
+        'Dixon_post_contrast_water',
+        'Dixon_post_contrast_fat',
+    ]
+    arrays = [db.pixel_data(study + [s]) for s in series]
+    array = np.stack(arrays, axis=-1)
+
+    mask = kidney_pc_dixon(array, model='unetr', verbose=True)
+    assert np.sum(mask['kidney_left']) == 62284
+
+    mask = kidney_pc_dixon(array, model='nnunet', verbose=True)
+    assert np.sum(mask['kidney_left']) == 79285
+
+    shutil.rmtree(tmp_dir)
+
+
+if __name__=='__main__':
+    test_kidney_pc_dixon()
+    print('kidney_pc_dixon passed all tests..')
