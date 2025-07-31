ac9a397f253e34e6c44b572495a6a063e01539a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 04:21:08 +0000

    Add package eggai/0.1.48/eggai-0.1.48.tar.gz

---
+++ packages/eggai/eggai-0.1.48.tar.gz/eggai-0.1.48/eggai/__init__.py
@@ -0,0 +1,3 @@
+from .agent import Agent
+from .channel import Channel
+from .hooks import eggai_cleanup, eggai_register_stop, eggai_main, EggaiRunner
---
+++ packages/eggai/eggai-0.1.48.tar.gz/eggai-0.1.48/eggai/transport/__init__.py
@@ -0,0 +1,4 @@
+from .base import Transport
+from .defaults import eggai_set_default_transport, get_default_transport
+from .inmemory import InMemoryTransport
+from .kafka import KafkaTransport
