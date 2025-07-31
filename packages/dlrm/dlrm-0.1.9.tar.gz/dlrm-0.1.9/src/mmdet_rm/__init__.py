c3b16f8f81a4758ef548e22b2d0e8dc5c47adf74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-31 03:34:36 +0000

    Add package dlrm/0.1.9/dlrm-0.1.9.tar.gz

---
+++ packages/dlrm/dlrm-0.1.9.tar.gz/dlrm-0.1.9/src/mmdet_rm/__init__.py
@@ -0,0 +1,5 @@
+from .factory import MMDetection_RM_Factory
+from .settings import MMDetection_RM_Settings
+from .settings import get_settings, set_settings
+
+__all__ = ["MMDetection_RM_Factory", "MMDetection_RM_Settings", "get_settings", "set_settings"]
